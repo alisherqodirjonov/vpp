alisherqodirjonov/vpp

commit 25439ef3b2df3ae82f8c6c73c5ea554cff8a6c39

Unpack into temporary directory with longer name.
JIRA VPP-498

This commit is a fix so downstream builds in Centos and
RHEL mock environment will work.  This is a workaround
for a bug in the debugedit code in rpm-build package 4.13
and earlier.

Change-Id: Ia6361f7487c5afe9948a2d2fe929942ba5b9c0f1
Signed-off-by: Thomas F Herbert <therbert@redhat.com>

extras/rpm/vpp.spec
@@ -3,7 +3,8 @@
 %define _unitdir         /lib/systemd/system
 %define _topdir          %(pwd)
 %define _builddir        %{_topdir}
-%define _mu_build_dir    %{_topdir}/%{name}-%{_version}/build-root
+%define _tmp_build_dir   %{name}-%{_version}.0
+%define _mu_build_dir    %{_topdir}/%{_tmp_build_dir}/build-root
 %define _vpp_tag	 %{getenv:TAG}
 %if "%{_vpp_tag}" == ""
 %define _vpp_tag	 vpp
@@ -119,13 +120,20 @@ Requires: vpp = %{_version}-%{_release}, vpp-lib = %{_version}-%{_release}, pyth
 This package contains the python bindings for the vpp api
 
 %prep
-%setup -q -n %{name}-%{_version}
+# Unpack into dir with longer name as work around of debugedit bug in in rpm-build 4.13
+rm -rf %{name}-%{_version}
+rm -rf %{_tmpbuild_dir}
+/usr/bin/xz -dc '%{_sourcedir}/%{name}-%{_version}-%{_release}.tar.xz' | /usr/bin/tar -xf -
+mv %{name}-%{_version} %{_tmp_build_dir}
+cd '%{_tmp_build_dir}'
+/usr/bin/chmod -Rf a+rX,u+w,g-w,o-w .
 
 %pre
 # Add the vpp group
 groupadd -f -r vpp
 
 %build
+cd '%{_tmp_build_dir}'
 %if %{with aesni}
     make bootstrap
     make -C build-root PLATFORM=vpp TAG=%{_vpp_tag} install-packages

commit 9c80eb3211833942b6d0a96d989bbf0b2170d959

Fix bug with temporary directory when building rpms.
Fixes bug introduced in commit 25439ef3.

JIRA: VPP-1014

Change-Id: Ia18f4c6f5f1124306cce790a36f6de970d186687
Signed-off-by: Thomas F Herbert <therbert@redhat.com>
(cherry picked from commit 8c5bfbcc09f44f3ed2b87b0b5c257fbb7b735f97)

extras/rpm/vpp.spec
@@ -126,7 +126,7 @@ This package contains the python bindings for the vpp api
 %prep
 # Unpack into dir with longer name as work around of debugedit bug in in rpm-build 4.13
 rm -rf %{name}-%{_version}
-rm -rf %{_tmpbuild_dir}
+rm -rf %{_tmp_build_dir}
 /usr/bin/xz -dc '%{_sourcedir}/%{name}-%{_version}-%{_release}.tar.xz' | /usr/bin/tar -xf -
 mv %{name}-%{_version} %{_tmp_build_dir}
 cd '%{_tmp_build_dir}'