alisherqodirjonov/vpp

commit 2620ecc2bc251262a09b3513befef9c82cfd3e6b

IPSEC: punt reasons; SPI=0, no-tunnel
Change-Id: If76992e283a27fa193a6865257ab3aa764066e48
Signed-off-by: Neale Ranns <nranns@cisco.com>

src/vnet/ipsec/ipsec_if_in.c
@@ -22,12 +22,14 @@
 #include <vnet/ipsec/ipsec.h>
 #include <vnet/ipsec/esp.h>
 #include <vnet/ipsec/ipsec_io.h>
+#include <vnet/ipsec/ipsec_punt.h>
 
 /* Statistics (not really errors) */
 #define foreach_ipsec_if_input_error				  \
 _(RX, "good packets received")					  \
 _(DISABLED, "ipsec packets received on disabled interface")       \
-_(NO_TUNNEL, "no matching tunnel")
+_(NO_TUNNEL, "no matching tunnel")                                \
+_(SPI_0, "SPI 0")
 
 static char *ipsec_if_input_error_strings[] = {
 #define _(sym,string) string,
@@ -61,6 +63,46 @@ format_ipsec_if_input_trace (u8 * s, va_list * args)
   return s;
 }
 
+always_inline u16
+ipsec_ip4_if_no_tunnel (vlib_node_runtime_t * node,
+			vlib_buffer_t * b,
+			const esp_header_t * esp,
+			const ip4_header_t * ip4, u16 offset)
+{
+  if (PREDICT_FALSE (0 == esp->spi))
+    {
+      b->error = node->errors[IPSEC_IF_INPUT_ERROR_SPI_0];
+      b->punt_reason =
+	ipsec_punt_reason[(ip4->protocol == IP_PROTOCOL_UDP ?
+			   IPSEC_PUNT_IP4_SPI_UDP_0 : IPSEC_PUNT_IP4_SPI_0)];
+    }
+  else
+    {
+      b->error = node->errors[IPSEC_IF_INPUT_ERROR_NO_TUNNEL];
+      b->punt_reason = ipsec_punt_reason[IPSEC_PUNT_IP4_NO_SUCH_TUNNEL];
+    }
+  vlib_buffer_advance (b, -offset);
+  return IPSEC_INPUT_NEXT_PUNT;
+}
+
+always_inline u16
+ipsec_ip6_if_no_tunnel (vlib_node_runtime_t * node,
+			vlib_buffer_t * b,
+			const esp_header_t * esp, u16 offset)
+{
+  if (PREDICT_FALSE (0 == esp->spi))
+    {
+      b->error = node->errors[IPSEC_IF_INPUT_ERROR_NO_TUNNEL];
+      b->punt_reason = ipsec_punt_reason[IPSEC_PUNT_IP6_SPI_0];
+    }
+  else
+    {
+      b->error = node->errors[IPSEC_IF_INPUT_ERROR_NO_TUNNEL];
+      b->punt_reason = ipsec_punt_reason[IPSEC_PUNT_IP6_NO_SUCH_TUNNEL];
+    }
+  vlib_buffer_advance (b, -offset);
+  return (IPSEC_INPUT_NEXT_PUNT);
+}
 
 always_inline uword
 ipsec_if_input_inline (vlib_main_t * vm, vlib_node_runtime_t * node,
@@ -197,9 +239,9 @@ ipsec_if_input_inline (vlib_main_t * vm, vlib_node_runtime_t * node,
 		}
 	      else
 		{
-		  b[0]->error = node->errors[IPSEC_IF_INPUT_ERROR_NO_TUNNEL];
+		  next[0] =
+		    ipsec_ip6_if_no_tunnel (node, b[0], esp0, buf_adv0);
 		  n_no_tunnel++;
-		  next[0] = IPSEC_INPUT_NEXT_DROP;
 		  goto pkt1;
 		}
 	    }
@@ -225,9 +267,9 @@ ipsec_if_input_inline (vlib_main_t * vm, vlib_node_runtime_t * node,
 		}
 	      else
 		{
-		  b[0]->error = node->errors[IPSEC_IF_INPUT_ERROR_NO_TUNNEL];
+		  next[0] =
+		    ipsec_ip4_if_no_tunnel (node, b[0], esp0, ip40, buf_adv0);
 		  n_no_tunnel++;
-		  next[0] = IPSEC_INPUT_NEXT_DROP;
 		  goto pkt1;
 		}
 	    }
@@ -293,9 +335,9 @@ ipsec_if_input_inline (vlib_main_t * vm, vlib_node_runtime_t * node,
 		}
 	      else
 		{
-		  b[1]->error = node->errors[IPSEC_IF_INPUT_ERROR_NO_TUNNEL];
+		  next[1] =
+		    ipsec_ip6_if_no_tunnel (node, b[1], esp1, buf_adv1);
 		  n_no_tunnel++;
-		  next[1] = IPSEC_INPUT_NEXT_DROP;
 		  goto trace1;
 		}
 	    }
@@ -321,9 +363,9 @@ ipsec_if_input_inline (vlib_main_t * vm, vlib_node_runtime_t * node,
 		}
 	      else
 		{
-		  b[1]->error = node->errors[IPSEC_IF_INPUT_ERROR_NO_TUNNEL];
+		  next[1] =
+		    ipsec_ip4_if_no_tunnel (node, b[1], esp1, ip41, buf_adv1);
 		  n_no_tunnel++;
-		  next[1] = IPSEC_INPUT_NEXT_DROP;
 		  goto trace1;
 		}
 	    }
@@ -456,9 +498,9 @@ ipsec_if_input_inline (vlib_main_t * vm, vlib_node_runtime_t * node,
 		}
 	      else
 		{
-		  b[0]->error = node->errors[IPSEC_IF_INPUT_ERROR_NO_TUNNEL];
+		  next[0] =
+		    ipsec_ip6_if_no_tunnel (node, b[0], esp0, buf_adv0);
 		  n_no_tunnel++;
-		  next[0] = IPSEC_INPUT_NEXT_DROP;
 		  goto trace00;
 		}
 	    }
@@ -484,9 +526,9 @@ ipsec_if_input_inline (vlib_main_t * vm, vlib_node_runtime_t * node,
 		}
 	      else
 		{
-		  b[0]->error = node->errors[IPSEC_IF_INPUT_ERROR_NO_TUNNEL];
+		  next[0] =
+		    ipsec_ip4_if_no_tunnel (node, b[0], esp0, ip40, buf_adv0);
 		  n_no_tunnel++;
-		  next[0] = IPSEC_INPUT_NEXT_DROP;
 		  goto trace00;
 		}
 	    }

test/test_punt.py
@@ -760,8 +760,8 @@ class TestPunt(VppTestCase):
         self.assertEqual(stats, 130)
 
         stats = self.statistics.get_counter("/net/punt")
-        self.assertEqual(stats[0][2]['packets'], 65)
-        self.assertEqual(stats[0][3]['packets'], 65)
+        self.assertEqual(stats[0][7]['packets'], 65)
+        self.assertEqual(stats[0][8]['packets'], 65)
 
         #
         # use the test CLI to test a client that punts exception
@@ -788,8 +788,8 @@ class TestPunt(VppTestCase):
             self.assertEqual(p6[IPv6].hlim, rx[IPv6].hlim)
 
         stats = self.statistics.get_counter("/net/punt")
-        self.assertEqual(stats[0][2]['packets'], 2*65)
-        self.assertEqual(stats[0][3]['packets'], 2*65)
+        self.assertEqual(stats[0][7]['packets'], 2*65)
+        self.assertEqual(stats[0][8]['packets'], 2*65)
 
         #
         # add another registration for the same reason to send packets
@@ -835,8 +835,8 @@ class TestPunt(VppTestCase):
             self.assertEqual(p6[IPv6].hlim, rx[IPv6].hlim)
 
         stats = self.statistics.get_counter("/net/punt")
-        self.assertEqual(stats[0][2]['packets'], 3*65)
-        self.assertEqual(stats[0][3]['packets'], 3*65)
+        self.assertEqual(stats[0][7]['packets'], 3*65)
+        self.assertEqual(stats[0][8]['packets'], 3*65)
 
         self.logger.info(self.vapi.cli("show vlib graph punt-dispatch"))
         self.logger.info(self.vapi.cli("show punt client"))

src/vnet/CMakeLists.txt
@@ -556,6 +556,7 @@ list(APPEND VNET_SOURCES
   ipsec/ipsec_input.c
   ipsec/ipsec_if.c
   ipsec/ipsec_if_in.c
+  ipsec/ipsec_punt.c
   ipsec/ipsec_sa.c
   ipsec/ipsec_spd.c
   ipsec/ipsec_spd_policy.c

src/vnet/ipsec/ipsec_punt.c
@@ -1,4 +1,6 @@
 /*
+ * esp_decrypt.c : IPSec ESP decrypt node
+ *
  * Copyright (c) 2015 Cisco and/or its affiliates.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,44 +14,33 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#ifndef __IPSEC_IO_H__
-#define __IPSEC_IO_H__
 
-#define foreach_ipsec_output_next  \
-  _ (DROP, "error-drop")
+#include <vnet/ipsec/ipsec.h>
+#include <vnet/ipsec/ipsec_punt.h>
 
-#define _(v, s) IPSEC_OUTPUT_NEXT_##v,
-typedef enum
+static vlib_punt_hdl_t punt_hdl;
+
+vlib_punt_reason_t ipsec_punt_reason[IPSEC_PUNT_N_REASONS];
+
+static clib_error_t *
+ipsec_punt_init (vlib_main_t * vm)
 {
-  foreach_ipsec_output_next
+  clib_error_t *error;
+
+  if ((error = vlib_call_init_function (vm, punt_init)))
+    return (error);
+
+  punt_hdl = vlib_punt_client_register ("ipsec");
+
+#define _(s,v)  vlib_punt_reason_alloc (punt_hdl, v,                    \
+                                        &ipsec_punt_reason[IPSEC_PUNT_##s]);
+  foreach_ipsec_punt_reason
 #undef _
-    IPSEC_OUTPUT_N_NEXT,
-} ipsec_output_next_t;
+    return (error);
+}
 
-#define foreach_ipsec_input_next   \
-  _ (PUNT, "punt-dispatch")        \
-  _ (DROP, "error-drop")
+VLIB_INIT_FUNCTION (ipsec_punt_init);
 
-#define _(v, s) IPSEC_INPUT_NEXT_##v,
-typedef enum
-{
-  foreach_ipsec_input_next
-#undef _
-    IPSEC_INPUT_N_NEXT,
-} ipsec_input_next_t;
-
-
-typedef struct
-{
-  u32 spd_index;
-} ip4_ipsec_config_t;
-
-typedef struct
-{
-  u32 spd_index;
-} ip6_ipsec_config_t;
-
-#endif /* __IPSEC_IO_H__ */
 
 /*
  * fd.io coding-style-patch-verification: ON

src/vnet/ipsec/ipsec_punt.h
@@ -1,0 +1,47 @@
+/*
+ * Copyright (c) 2019 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef __IPSEC_PUNT_H__
+#define __IPSEC_PUNT_H__
+
+#include <vlib/vlib.h>
+
+#define foreach_ipsec_punt_reason                             \
+  _(IP4_SPI_0, "ip4-spi-0")                                   \
+  _(IP6_SPI_0, "ip6-spi-0")                                   \
+  _(IP4_SPI_UDP_0, "ip4-spi-o-udp-0")                         \
+  _(IP4_NO_SUCH_TUNNEL, "ip4-no-such-tunnel")                 \
+  _(IP6_NO_SUCH_TUNNEL, "ip6-no-such-tunnel")
+
+typedef enum ipsec_punt_reason_t_
+{
+#define _(s,v) IPSEC_PUNT_##s,
+  foreach_ipsec_punt_reason
+#undef _
+    IPSEC_PUNT_N_REASONS,
+} ipsec_punt_reason_type_t;
+
+extern u8 *format_ipsec_punt_reason (u8 * s, va_list * args);
+
+extern vlib_punt_reason_t ipsec_punt_reason[IPSEC_PUNT_N_REASONS];
+
+#endif /* __IPSEC_SPD_H__ */
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */

commit 934eb7a529b287da6063d045f8fb7539f094728c

ipsec: Correctly rewind the buffer to the IP header when punting UDP encapped SPI-0 packets
Type: fix
Fixes: 2620ecc2bc

Change-Id: I2d81b373f7659e702759939c096b315afa36f621
Signed-off-by: Neale Ranns <nranns@cisco.com>

src/vnet/ipsec/ipsec_if_in.c
@@ -440,7 +440,7 @@ ipsec_if_input_inline (vlib_main_t * vm, vlib_node_runtime_t * node,
       ip6_header_t *ip60;
       esp_header_t *esp0;
       u32 len0;
-      u16 buf_adv0;
+      u16 buf_adv0, buf_rewind0;
       u32 tid0;
       ipsec_tunnel_if_t *t0;
       ipsec4_tunnel_key_t key40;
@@ -464,11 +464,12 @@ ipsec_if_input_inline (vlib_main_t * vm, vlib_node_runtime_t * node,
 		(esp_header_t *) ((u8 *) ip40 + ip4_header_bytes (ip40) +
 				  sizeof (udp_header_t));
 	      buf_adv0 = 0;
+	      buf_rewind0 = ip4_header_bytes (ip40) + sizeof (udp_header_t);
 	    }
 	  else
 	    {
 	      esp0 = (esp_header_t *) ((u8 *) ip40 + ip4_header_bytes (ip40));
-	      buf_adv0 = ip4_header_bytes (ip40);
+	      buf_rewind0 = buf_adv0 = ip4_header_bytes (ip40);
 	    }
 	}
 
@@ -527,7 +528,8 @@ ipsec_if_input_inline (vlib_main_t * vm, vlib_node_runtime_t * node,
 	      else
 		{
 		  next[0] =
-		    ipsec_ip4_if_no_tunnel (node, b[0], esp0, ip40, buf_adv0);
+		    ipsec_ip4_if_no_tunnel (node, b[0], esp0, ip40,
+					    buf_rewind0);
 		  n_no_tunnel++;
 		  goto trace00;
 		}

test/test_punt.py
@@ -141,6 +141,7 @@ class TestPuntSocket(VppTestCase):
     def verify_udp_pkts(self, rxs, n_rx, port):
         n_match = 0
         for rx in rxs:
+            rx.show()
             self.assertTrue(rx.haslayer(UDP))
             if rx[UDP].dport == port:
                 n_match += 1
@@ -772,11 +773,14 @@ class TestExceptionPuntSocket(TestPuntSocket):
         punts = self.vapi.punt_socket_dump(type=pt_ex)
         self.assertEqual(len(punts), 0)
 
-    def verify_esp_pkts(self, rxs, n_sent, spi):
+    def verify_esp_pkts(self, rxs, n_sent, spi, has_udp):
         self.assertEqual(len(rxs), n_sent)
         for rx in rxs:
+            self.assertTrue(rx.haslayer(IP))
             self.assertTrue(rx.haslayer(ESP))
             self.assertEqual(rx[ESP].spi, spi)
+            if has_udp:
+                self.assertTrue(rx.haslayer(UDP))
 
     def test_traffic(self):
         """ Punt socket traffic """
@@ -791,7 +795,7 @@ class TestExceptionPuntSocket(TestPuntSocket):
         }
 
         #
-        # we need an IPSec tunnel for this to work otherwise ESP gets dropped
+        # we need an IPSec tunnels for this to work otherwise ESP gets dropped
         # due to unknown IP proto
         #
         VppIpsecTunInterface(self, self.pg0, 1000, 1000,
@@ -803,14 +807,25 @@ class TestExceptionPuntSocket(TestPuntSocket):
                               IPSEC_API_INTEG_ALG_SHA1_96),
                              "0123456701234567",
                              "0123456701234567").add_vpp_config()
+        VppIpsecTunInterface(self, self.pg0, 1001, 1001,
+                             (VppEnum.vl_api_ipsec_crypto_alg_t.
+                              IPSEC_API_CRYPTO_ALG_AES_CBC_128),
+                             "0123456701234567",
+                             "0123456701234567",
+                             (VppEnum.vl_api_ipsec_integ_alg_t.
+                              IPSEC_API_INTEG_ALG_SHA1_96),
+                             "0123456701234567",
+                             "0123456701234567",
+                             udp_encap=True).add_vpp_config()
 
         #
         # we're dealing with IPSec tunnels punting for no-such-tunnel
         # adn SPI=0
         #
         cfgs = dict()
-        cfgs['ipsec4-no-such-tunnel'] = {'spi': 99}
-        cfgs['ipsec4-spi-0'] = {'spi': 0}
+        cfgs['ipsec4-no-such-tunnel'] = {'spi': 99, 'udp': False}
+        cfgs['ipsec4-spi-0'] = {'spi': 0, 'udp': False}
+        cfgs['ipsec4-spi-o-udp-0'] = {'spi': 0, 'udp': True}
 
         #
         # find the VPP ID for these punt exception reasin
@@ -826,22 +841,28 @@ class TestExceptionPuntSocket(TestPuntSocket):
                     break
 
         #
-        # create packet streams and configure a punt sockets
+        # configure punt sockets
         #
         for cfg in cfgs.values():
-            pkt = (Ether(src=self.pg0.remote_mac,
-                         dst=self.pg0.local_mac) /
-                   IP(src=self.pg0.remote_ip4, dst=self.pg0.local_ip4) /
-                   ESP(spi=cfg['spi'], seq=3) /
-                   Raw('\xa5' * 100))
-            cfg['pkts'] = pkt * self.nr_packets
-
             cfg['sock'] = self.socket_client_create(b"%s/socket_%d" % (
                 six.ensure_binary(self.tempdir), cfg['id']))
             self.vapi.punt_socket_register(
                 cfg['vpp'],
                 b"%s/socket_%d" % (six.ensure_binary(self.tempdir),
                                    cfg['id']))
+
+        #
+        # create packet streams for 'no-such-tunnel' exception
+        #
+        for cfg in cfgs.values():
+            pkt = (Ether(src=self.pg0.remote_mac,
+                         dst=self.pg0.local_mac) /
+                   IP(src=self.pg0.remote_ip4, dst=self.pg0.local_ip4))
+            if (cfg['udp']):
+                pkt = pkt / UDP(sport=666, dport=4500)
+            pkt = (pkt / ESP(spi=cfg['spi'], seq=3) /
+                   Raw('\xa5' * 100))
+            cfg['pkts'] = [pkt]
 
         #
         # send packets for each SPI we expect to be punted
@@ -854,7 +875,13 @@ class TestExceptionPuntSocket(TestPuntSocket):
         #
         for cfg in cfgs.values():
             rx = cfg['sock'].close()
-            self.verify_esp_pkts(rx, len(cfg['pkts']), cfg['spi'])
+            self.verify_esp_pkts(rx, len(cfg['pkts']),
+                                 cfg['spi'], cfg['udp'])
+
+        #
+        # socket deregister
+        #
+        for cfg in cfgs.values():
             self.vapi.punt_socket_deregister(cfg['vpp'])
 
 

test/vpp_ipsec_tun_interface.py
@@ -9,6 +9,7 @@ class VppIpsecTunInterface(VppTunnelInterface):
     def __init__(self, test, parent_if, local_spi,
                  remote_spi, crypto_alg, local_crypto_key, remote_crypto_key,
                  integ_alg, local_integ_key, remote_integ_key, salt=0,
+                 udp_encap=False,
                  is_ip6=False):
         super(VppIpsecTunInterface, self).__init__(test, parent_if)
         self.local_spi = local_spi
@@ -26,6 +27,7 @@ class VppIpsecTunInterface(VppTunnelInterface):
         else:
             self.local_ip = self.parent_if.local_ip4
             self.remote_ip = self.parent_if.remote_ip4
+        self.udp_encap = False
 
     def add_vpp_config(self):
         r = self.test.vapi.ipsec_tunnel_if_add_del(
@@ -33,7 +35,8 @@ class VppIpsecTunInterface(VppTunnelInterface):
             self.remote_spi, self.local_spi,
             self.crypto_alg, self.local_crypto_key, self.remote_crypto_key,
             self.integ_alg, self.local_integ_key, self.remote_integ_key,
-            salt=self.salt)
+            salt=self.salt,
+            udp_encap=self.udp_encap)
         self.set_sw_if_index(r.sw_if_index)
         self.generate_remote_hosts()
         self.test.registry.register(self, self.test.logger)

test/vpp_papi_provider.py
@@ -1889,7 +1889,8 @@ class VppPapiProvider(object):
                                 remote_spi, crypto_alg, local_crypto_key,
                                 remote_crypto_key, integ_alg, local_integ_key,
                                 remote_integ_key, is_add=1, esn=0, salt=0,
-                                anti_replay=1, renumber=0, show_instance=0):
+                                anti_replay=1, renumber=0,
+                                udp_encap=0, show_instance=0):
         return self.api(
             self.papi.ipsec_tunnel_if_add_del,
             {
@@ -1912,6 +1913,7 @@ class VppPapiProvider(object):
                 'anti_replay': anti_replay,
                 'renumber': renumber,
                 'show_instance': show_instance,
+                'udp_encap': udp_encap,
                 'salt': salt
             })
 