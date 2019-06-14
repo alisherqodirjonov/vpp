alisherqodirjonov/vpp

commit 9b4514c34146160059c67b01d87e63e1b3ccb92d

IPSEC: no second lookup after tunnel encap
in the same maaner as with other tunnel tyeps we use
the FIB to cache and track the destination used to reach
the tunnel endpoint. Post encap we can then ship the packet
straight to this adjacency and thus elide the costly second
lookup.

- SA add and del function so they can be used both directly
  from the API and for tunnels.
- API change for the SA dump to use the SA type
- ipsec_key_t type for convenience (copying, [un]formating)
- no matching tunnel counters in ipsec-if-input

Change-Id: I9d144a59667f7bf96442f4ca66bef5c1d3c7f1ea
Signed-off-by: Neale Ranns <nranns@cisco.com>

src/vnet/ipsec/ipsec_cli.c
@@ -73,87 +73,62 @@ ipsec_sa_add_del_command_fn (vlib_main_t * vm,
 			     unformat_input_t * input,
 			     vlib_cli_command_t * cmd)
 {
-  ipsec_main_t *im = &ipsec_main;
   unformat_input_t _line_input, *line_input = &_line_input;
-  ipsec_sa_t sa;
-  int is_add = ~0;
-  u8 *ck = 0, *ik = 0;
-  clib_error_t *error = NULL;
+  ip46_address_t tun_src = { }, tun_dst =
+  {
+  };
+  ipsec_crypto_alg_t crypto_alg;
+  ipsec_integ_alg_t integ_alg;
+  ipsec_protocol_t proto;
+  ipsec_sa_flags_t flags;
+  clib_error_t *error;
+  ipsec_key_t ck, ik;
+  int is_add, rv;
+  u32 id, spi;
 
-  clib_memset (&sa, 0, sizeof (sa));
-  sa.tx_fib_index = ~((u32) 0);	/* Only supported for ipsec interfaces */
+  error = NULL;
+  is_add = 0;
+  flags = IPSEC_SA_FLAG_NONE;
+  proto = IPSEC_PROTOCOL_ESP;
 
   if (!unformat_user (input, unformat_line_input, line_input))
     return 0;
 
   while (unformat_check_input (line_input) != UNFORMAT_END_OF_INPUT)
     {
-      if (unformat (line_input, "add %u", &sa.id))
+      if (unformat (line_input, "add %u", &id))
 	is_add = 1;
-      else if (unformat (line_input, "del %u", &sa.id))
+      else if (unformat (line_input, "del %u", &id))
 	is_add = 0;
-      else if (unformat (line_input, "spi %u", &sa.spi))
+      else if (unformat (line_input, "spi %u", &spi))
 	;
       else if (unformat (line_input, "esp"))
-	sa.protocol = IPSEC_PROTOCOL_ESP;
+	proto = IPSEC_PROTOCOL_ESP;
       else if (unformat (line_input, "ah"))
-	{
-	  sa.protocol = IPSEC_PROTOCOL_AH;
-	}
-      else
-	if (unformat (line_input, "crypto-key %U", unformat_hex_string, &ck))
-	sa.crypto_key_len = vec_len (ck);
-      else
-	if (unformat
-	    (line_input, "crypto-alg %U", unformat_ipsec_crypto_alg,
-	     &sa.crypto_alg))
-	{
-	  if (sa.crypto_alg < IPSEC_CRYPTO_ALG_NONE ||
-	      sa.crypto_alg >= IPSEC_CRYPTO_N_ALG)
-	    {
-	      error = clib_error_return (0, "unsupported crypto-alg: '%U'",
-					 format_ipsec_crypto_alg,
-					 sa.crypto_alg);
-	      goto done;
-	    }
-	}
-      else
-	if (unformat (line_input, "integ-key %U", unformat_hex_string, &ik))
-	sa.integ_key_len = vec_len (ik);
-      else if (unformat (line_input, "integ-alg %U", unformat_ipsec_integ_alg,
-			 &sa.integ_alg))
-	{
-	  if (sa.integ_alg < IPSEC_INTEG_ALG_NONE ||
-	      sa.integ_alg >= IPSEC_INTEG_N_ALG)
-	    {
-	      error = clib_error_return (0, "unsupported integ-alg: '%U'",
-					 format_ipsec_integ_alg,
-					 sa.integ_alg);
-	      goto done;
-	    }
-	}
+	proto = IPSEC_PROTOCOL_AH;
+      else if (unformat (line_input, "crypto-key %U",
+			 unformat_ipsec_key, &ck))
+	;
+      else if (unformat (line_input, "crypto-alg %U",
+			 unformat_ipsec_crypto_alg, &crypto_alg))
+	;
+      else if (unformat (line_input, "integ-key %U", unformat_ipsec_key, &ik))
+	;
+      else if (unformat (line_input, "integ-alg %U",
+			 unformat_ipsec_integ_alg, &integ_alg))
+	;
       else if (unformat (line_input, "tunnel-src %U",
-			 unformat_ip4_address, &sa.tunnel_src_addr.ip4))
-	sa.is_tunnel = 1;
-      else if (unformat (line_input, "tunnel-dst %U",
-			 unformat_ip4_address, &sa.tunnel_dst_addr.ip4))
-	sa.is_tunnel = 1;
-      else if (unformat (line_input, "tunnel-src %U",
-			 unformat_ip6_address, &sa.tunnel_src_addr.ip6))
+			 unformat_ip46_address, &tun_src))
 	{
-	  sa.is_tunnel = 1;
-	  sa.is_tunnel_ip6 = 1;
+	  flags |= IPSEC_SA_FLAG_IS_TUNNEL;
+	  if (!ip46_address_is_ip4 (&tun_src))
+	    flags |= IPSEC_SA_FLAG_IS_TUNNEL_V6;
 	}
       else if (unformat (line_input, "tunnel-dst %U",
-			 unformat_ip6_address, &sa.tunnel_dst_addr.ip6))
-	{
-	  sa.is_tunnel = 1;
-	  sa.is_tunnel_ip6 = 1;
-	}
+			 unformat_ip46_address, &tun_dst))
+	;
       else if (unformat (line_input, "udp-encap"))
-	{
-	  sa.udp_encap = 1;
-	}
+	flags |= IPSEC_SA_FLAG_UDP_ENCAP;
       else
 	{
 	  error = clib_error_return (0, "parse error: '%U'",
@@ -162,26 +137,15 @@ ipsec_sa_add_del_command_fn (vlib_main_t * vm,
 	}
     }
 
-  if (sa.crypto_key_len > sizeof (sa.crypto_key))
-    sa.crypto_key_len = sizeof (sa.crypto_key);
-
-  if (sa.integ_key_len > sizeof (sa.integ_key))
-    sa.integ_key_len = sizeof (sa.integ_key);
-
-  if (ck)
-    memcpy (sa.crypto_key, ck, sa.crypto_key_len);
-
-  if (ik)
-    memcpy (sa.integ_key, ik, sa.integ_key_len);
-
   if (is_add)
-    {
-      error = ipsec_check_support_cb (im, &sa);
-      if (error)
-	goto done;
-    }
+    rv = ipsec_sa_add (id, spi, proto, crypto_alg,
+		       &ck, integ_alg, &ik, flags,
+		       0, &tun_src, &tun_dst, NULL);
+  else
+    rv = ipsec_sa_del (id);
 
-  ipsec_add_del_sa (vm, &sa, is_add);
+  if (rv)
+    clib_error_return (0, "failed");
 
 done:
   unformat_free (line_input);
@@ -388,25 +352,22 @@ set_ipsec_sa_key_command_fn (vlib_main_t * vm,
 			     vlib_cli_command_t * cmd)
 {
   unformat_input_t _line_input, *line_input = &_line_input;
-  ipsec_sa_t sa;
-  u8 *ck = 0, *ik = 0;
   clib_error_t *error = NULL;
-
-  clib_memset (&sa, 0, sizeof (sa));
+  ipsec_key_t ck, ik;
+  u32 id;
 
   if (!unformat_user (input, unformat_line_input, line_input))
     return 0;
 
   while (unformat_check_input (line_input) != UNFORMAT_END_OF_INPUT)
     {
-      if (unformat (line_input, "%u", &sa.id))
+      if (unformat (line_input, "%u", &id))
 	;
       else
-	if (unformat (line_input, "crypto-key %U", unformat_hex_string, &ck))
-	sa.crypto_key_len = vec_len (ck);
-      else
-	if (unformat (line_input, "integ-key %U", unformat_hex_string, &ik))
-	sa.integ_key_len = vec_len (ik);
+	if (unformat (line_input, "crypto-key %U", unformat_ipsec_key, &ck))
+	;
+      else if (unformat (line_input, "integ-key %U", unformat_ipsec_key, &ik))
+	;
       else
 	{
 	  error = clib_error_return (0, "parse error: '%U'",
@@ -415,19 +376,7 @@ set_ipsec_sa_key_command_fn (vlib_main_t * vm,
 	}
     }
 
-  if (sa.crypto_key_len > sizeof (sa.crypto_key))
-    sa.crypto_key_len = sizeof (sa.crypto_key);
-
-  if (sa.integ_key_len > sizeof (sa.integ_key))
-    sa.integ_key_len = sizeof (sa.integ_key);
-
-  if (ck)
-    strncpy ((char *) sa.crypto_key, (char *) ck, sa.crypto_key_len);
-
-  if (ik)
-    strncpy ((char *) sa.integ_key, (char *) ik, sa.integ_key_len);
-
-  ipsec_set_sa_key (vm, &sa);
+  ipsec_set_sa_key (id, &ck, &ik);
 
 done:
   unformat_free (line_input);
@@ -448,61 +397,30 @@ static clib_error_t *
 show_ipsec_command_fn (vlib_main_t * vm,
 		       unformat_input_t * input, vlib_cli_command_t * cmd)
 {
-  ipsec_sa_t *sa;
   ipsec_main_t *im = &ipsec_main;
-  u32 i;
-  ipsec_tunnel_if_t *t;
+  u32 spd_id, sw_if_index, sai;
   vnet_hw_interface_t *hi;
+  ipsec_tunnel_if_t *t;
   u8 *protocol = NULL;
   u8 *policy = NULL;
-  u32 tx_table_id, spd_id, sw_if_index;
+  u32 i;
 
   /* *INDENT-OFF* */
-  pool_foreach (sa, im->sad, ({
-    if (sa->id) {
-      vlib_cli_output(vm, "sa %u spi %u mode %s protocol %s%s%s%s", sa->id, sa->spi,
-                      sa->is_tunnel ? "tunnel" : "transport",
-                      sa->protocol ? "esp" : "ah",
-		      sa->udp_encap ? " udp-encap-enabled" : "",
-                      sa->use_anti_replay ? " anti-replay" : "",
-                      sa->use_esn ? " extended-sequence-number" : "");
-      if (sa->protocol == IPSEC_PROTOCOL_ESP) {
-        vlib_cli_output(vm, "  crypto alg %U%s%U integrity alg %U%s%U",
-                        format_ipsec_crypto_alg, sa->crypto_alg,
-                        sa->crypto_alg ? " key " : "",
-                        format_hex_bytes, sa->crypto_key, sa->crypto_key_len,
-                        format_ipsec_integ_alg, sa->integ_alg,
-                        sa->integ_alg ? " key " : "",
-                        format_hex_bytes, sa->integ_key, sa->integ_key_len);
-      }
-      if (sa->is_tunnel && sa->is_tunnel_ip6) {
-        vlib_cli_output(vm, "  tunnel src %U dst %U",
-                        format_ip6_address, &sa->tunnel_src_addr.ip6,
-                        format_ip6_address, &sa->tunnel_dst_addr.ip6);
-      } else if (sa->is_tunnel) {
-        vlib_cli_output(vm, "  tunnel src %U dst %U",
-                        format_ip4_address, &sa->tunnel_src_addr.ip4,
-                        format_ip4_address, &sa->tunnel_dst_addr.ip4);
-      }
-    }
+  pool_foreach_index (sai, im->sad, ({
+     vlib_cli_output(vm, "%U", format_ipsec_sa, sai);
   }));
-  /* *INDENT-ON* */
-
-  /* *INDENT-OFF* */
   pool_foreach_index (i, im->spds, ({
     vlib_cli_output(vm, "%U", format_ipsec_spd, i);
   }));
-  /* *INDENT-ON* */
 
   vlib_cli_output (vm, "SPD Bindings:");
-  /* *INDENT-OFF* */
+
   hash_foreach(sw_if_index, spd_id, im->spd_index_by_sw_if_index, ({
-        vlib_cli_output (vm, "  %d -> %U", spd_id,
-                         format_vnet_sw_if_index_name, im->vnet_main,
-                         sw_if_index);
+    vlib_cli_output (vm, "  %d -> %U", spd_id,
+                     format_vnet_sw_if_index_name, im->vnet_main,
+                     sw_if_index);
   }));
   /* *INDENT-ON* */
-
 
   vlib_cli_output (vm, "tunnel interfaces");
   /* *INDENT-OFF* */
@@ -510,34 +428,14 @@ show_ipsec_command_fn (vlib_main_t * vm,
     if (t->hw_if_index == ~0)
       continue;
     hi = vnet_get_hw_interface (im->vnet_main, t->hw_if_index);
-    vlib_cli_output(vm, "  %s seq", hi->name);
-    sa = pool_elt_at_index(im->sad, t->output_sa_index);
 
-    tx_table_id = fib_table_get_table_id(sa->tx_fib_index, FIB_PROTOCOL_IP4);
+    vlib_cli_output(vm, "  %s", hi->name);
 
-    vlib_cli_output(vm, "   seq %u seq-hi %u esn %u anti-replay %u udp-encap %u tx-table %u",
-                    sa->seq, sa->seq_hi, sa->use_esn, sa->use_anti_replay, sa->udp_encap, tx_table_id);
-    vlib_cli_output(vm, "   local-spi %u local-ip %U", sa->spi,
-                    format_ip4_address, &sa->tunnel_src_addr.ip4);
-    vlib_cli_output(vm, "   local-crypto %U %U",
-                    format_ipsec_crypto_alg, sa->crypto_alg,
-                    format_hex_bytes, sa->crypto_key, sa->crypto_key_len);
-    vlib_cli_output(vm, "   local-integrity %U %U",
-                    format_ipsec_integ_alg, sa->integ_alg,
-                    format_hex_bytes, sa->integ_key, sa->integ_key_len);
-    sa = pool_elt_at_index(im->sad, t->input_sa_index);
-    vlib_cli_output(vm, "   last-seq %u last-seq-hi %u esn %u anti-replay %u window %U",
-                    sa->last_seq, sa->last_seq_hi, sa->use_esn,
-                    sa->use_anti_replay,
-                    format_ipsec_replay_window, sa->replay_window);
-    vlib_cli_output(vm, "   remote-spi %u remote-ip %U", sa->spi,
-                    format_ip4_address, &sa->tunnel_src_addr.ip4);
-    vlib_cli_output(vm, "   remote-crypto %U %U",
-                    format_ipsec_crypto_alg, sa->crypto_alg,
-                    format_hex_bytes, sa->crypto_key, sa->crypto_key_len);
-    vlib_cli_output(vm, "   remote-integrity %U %U",
-                    format_ipsec_integ_alg, sa->integ_alg,
-                    format_hex_bytes, sa->integ_key, sa->integ_key_len);
+    vlib_cli_output(vm, "  out-bound sa");
+    vlib_cli_output(vm, "   %U", format_ipsec_sa, t->output_sa_index);
+
+    vlib_cli_output(vm, "  in-bound sa");
+    vlib_cli_output(vm, "   %U", format_ipsec_sa, t->input_sa_index);
   }));
   vec_free(policy);
   vec_free(protocol);

src/plugins/dpdk/ipsec/ipsec.c
@@ -252,7 +252,7 @@ crypto_set_aead_xform (struct rte_crypto_sym_xform *xform,
 
   xform->type = RTE_CRYPTO_SYM_XFORM_AEAD;
   xform->aead.algo = c->alg;
-  xform->aead.key.data = sa->crypto_key;
+  xform->aead.key.data = sa->crypto_key.data;
   xform->aead.key.length = c->key_len;
   xform->aead.iv.offset =
     crypto_op_get_priv_offset () + offsetof (dpdk_op_priv_t, cb);
@@ -280,7 +280,7 @@ crypto_set_cipher_xform (struct rte_crypto_sym_xform *xform,
 
   xform->type = RTE_CRYPTO_SYM_XFORM_CIPHER;
   xform->cipher.algo = c->alg;
-  xform->cipher.key.data = sa->crypto_key;
+  xform->cipher.key.data = sa->crypto_key.data;
   xform->cipher.key.length = c->key_len;
   xform->cipher.iv.offset =
     crypto_op_get_priv_offset () + offsetof (dpdk_op_priv_t, cb);
@@ -306,7 +306,7 @@ crypto_set_auth_xform (struct rte_crypto_sym_xform *xform,
 
   xform->type = RTE_CRYPTO_SYM_XFORM_AUTH;
   xform->auth.algo = a->alg;
-  xform->auth.key.data = sa->integ_key;
+  xform->auth.key.data = sa->integ_key.data;
   xform->auth.key.length = a->key_len;
   xform->auth.digest_length = a->trunc_size;
   xform->next = NULL;
@@ -511,7 +511,8 @@ add_del_sa_session (u32 sa_index, u8 is_add)
 	case IPSEC_CRYPTO_ALG_AES_GCM_128:
 	case IPSEC_CRYPTO_ALG_AES_GCM_192:
 	case IPSEC_CRYPTO_ALG_AES_GCM_256:
-	  clib_memcpy (&sa->salt, &sa->crypto_key[sa->crypto_key_len - 4], 4);
+	  clib_memcpy (&sa->salt,
+		       &sa->crypto_key.data[sa->crypto_key.len - 4], 4);
 	  break;
 	default:
 	  seed = (u32) clib_cpu_time_now ();

src/vat/api_format.c
@@ -15215,21 +15215,21 @@ vl_api_ipsec_sa_details_t_handler (vl_api_ipsec_sa_details_t * mp)
   vat_main_t *vam = &vat_main;
 
   print (vam->ofp, "sa_id %u sw_if_index %u spi %u proto %u crypto_alg %u "
-	 "crypto_key %U integ_alg %u integ_key %U use_esn %u "
-	 "use_anti_replay %u is_tunnel %u is_tunnel_ip6 %u "
+	 "crypto_key %U integ_alg %u integ_key %U flags %x "
 	 "tunnel_src_addr %U tunnel_dst_addr %U "
 	 "salt %u seq_outbound %lu last_seq_inbound %lu "
 	 "replay_window %lu total_data_size %lu\n",
-	 ntohl (mp->sa_id), ntohl (mp->sw_if_index), ntohl (mp->spi),
-	 mp->protocol,
-	 mp->crypto_alg, format_hex_bytes, mp->crypto_key, mp->crypto_key_len,
-	 mp->integ_alg, format_hex_bytes, mp->integ_key, mp->integ_key_len,
-	 mp->use_esn, mp->use_anti_replay, mp->is_tunnel, mp->is_tunnel_ip6,
-	 (mp->is_tunnel_ip6) ? format_ip6_address : format_ip4_address,
-	 mp->tunnel_src_addr,
-	 (mp->is_tunnel_ip6) ? format_ip6_address : format_ip4_address,
-	 mp->tunnel_dst_addr,
-	 ntohl (mp->salt),
+	 ntohl (mp->entry.sad_id),
+	 ntohl (mp->sw_if_index),
+	 ntohl (mp->entry.spi),
+	 ntohl (mp->entry.protocol),
+	 ntohl (mp->entry.crypto_algorithm),
+	 format_hex_bytes, mp->entry.crypto_key.data,
+	 mp->entry.crypto_key.length, ntohl (mp->entry.integrity_algorithm),
+	 format_hex_bytes, mp->entry.integrity_key.data,
+	 mp->entry.integrity_key.length, ntohl (mp->entry.flags),
+	 format_vl_api_address, &mp->entry.tunnel_src, format_vl_api_address,
+	 &mp->entry.tunnel_dst, ntohl (mp->salt),
 	 clib_net_to_host_u64 (mp->seq_outbound),
 	 clib_net_to_host_u64 (mp->last_seq_inbound),
 	 clib_net_to_host_u64 (mp->replay_window),
@@ -15239,13 +15239,32 @@ vl_api_ipsec_sa_details_t_handler (vl_api_ipsec_sa_details_t * mp)
 #define vl_api_ipsec_sa_details_t_endian vl_noop_handler
 #define vl_api_ipsec_sa_details_t_print vl_noop_handler
 
+static void
+vat_json_object_add_address (vat_json_node_t * node,
+			     const vl_api_address_t * addr)
+{
+  if (ADDRESS_IP6 == addr->af)
+    {
+      struct in6_addr ip6;
+
+      clib_memcpy (&ip6, &addr->un.ip6, sizeof (ip6));
+      vat_json_object_add_ip6 (node, "ip_address", ip6);
+    }
+  else
+    {
+      struct in_addr ip4;
+
+      clib_memcpy (&ip4, &addr->un.ip4, sizeof (ip4));
+      vat_json_object_add_ip4 (node, "ip_address", ip4);
+    }
+}
+
 static void vl_api_ipsec_sa_details_t_handler_json
   (vl_api_ipsec_sa_details_t * mp)
 {
   vat_main_t *vam = &vat_main;
   vat_json_node_t *node = NULL;
-  struct in_addr src_ip4, dst_ip4;
-  struct in6_addr src_ip6, dst_ip6;
+  vl_api_ipsec_sad_flags_t flags;
 
   if (VAT_JSON_ARRAY != vam->json_tree.type)
     {
@@ -15255,39 +15274,36 @@ static void vl_api_ipsec_sa_details_t_handler_json
   node = vat_json_array_add (&vam->json_tree);
 
   vat_json_init_object (node);
-  vat_json_object_add_uint (node, "sa_id", ntohl (mp->sa_id));
+  vat_json_object_add_uint (node, "sa_id", ntohl (mp->entry.sad_id));
   vat_json_object_add_uint (node, "sw_if_index", ntohl (mp->sw_if_index));
-  vat_json_object_add_uint (node, "spi", ntohl (mp->spi));
-  vat_json_object_add_uint (node, "proto", mp->protocol);
-  vat_json_object_add_uint (node, "crypto_alg", mp->crypto_alg);
-  vat_json_object_add_uint (node, "integ_alg", mp->integ_alg);
-  vat_json_object_add_uint (node, "use_esn", mp->use_esn);
-  vat_json_object_add_uint (node, "use_anti_replay", mp->use_anti_replay);
-  vat_json_object_add_uint (node, "is_tunnel", mp->is_tunnel);
-  vat_json_object_add_uint (node, "is_tunnel_ip6", mp->is_tunnel_ip6);
-  vat_json_object_add_bytes (node, "crypto_key", mp->crypto_key,
-			     mp->crypto_key_len);
-  vat_json_object_add_bytes (node, "integ_key", mp->integ_key,
-			     mp->integ_key_len);
-  if (mp->is_tunnel_ip6)
-    {
-      clib_memcpy (&src_ip6, mp->tunnel_src_addr, sizeof (src_ip6));
-      vat_json_object_add_ip6 (node, "tunnel_src_addr", src_ip6);
-      clib_memcpy (&dst_ip6, mp->tunnel_dst_addr, sizeof (dst_ip6));
-      vat_json_object_add_ip6 (node, "tunnel_dst_addr", dst_ip6);
-    }
-  else
-    {
-      clib_memcpy (&src_ip4, mp->tunnel_src_addr, sizeof (src_ip4));
-      vat_json_object_add_ip4 (node, "tunnel_src_addr", src_ip4);
-      clib_memcpy (&dst_ip4, mp->tunnel_dst_addr, sizeof (dst_ip4));
-      vat_json_object_add_ip4 (node, "tunnel_dst_addr", dst_ip4);
-    }
+  vat_json_object_add_uint (node, "spi", ntohl (mp->entry.spi));
+  vat_json_object_add_uint (node, "proto", ntohl (mp->entry.protocol));
+  vat_json_object_add_uint (node, "crypto_alg",
+			    ntohl (mp->entry.crypto_algorithm));
+  vat_json_object_add_uint (node, "integ_alg",
+			    ntohl (mp->entry.integrity_algorithm));
+  flags = ntohl (mp->entry.flags);
+  vat_json_object_add_uint (node, "use_esn",
+			    ! !(flags &
+				IPSEC_API_SAD_FLAG_USE_EXTENDED_SEQ_NUM));
+  vat_json_object_add_uint (node, "use_anti_replay",
+			    ! !(flags & IPSEC_API_SAD_FLAG_USE_ANTI_REPLAY));
+  vat_json_object_add_uint (node, "is_tunnel",
+			    ! !(flags & IPSEC_API_SAD_FLAG_IS_TUNNEL));
+  vat_json_object_add_uint (node, "is_tunnel_ip6",
+			    ! !(flags & IPSEC_API_SAD_FLAG_IS_TUNNEL_V6));
+  vat_json_object_add_uint (node, "udp_encap",
+			    ! !(flags & IPSEC_API_SAD_FLAG_UDP_ENCAP));
+  vat_json_object_add_bytes (node, "crypto_key", mp->entry.crypto_key.data,
+			     mp->entry.crypto_key.length);
+  vat_json_object_add_bytes (node, "integ_key", mp->entry.integrity_key.data,
+			     mp->entry.integrity_key.length);
+  vat_json_object_add_address (node, &mp->entry.tunnel_src);
+  vat_json_object_add_address (node, &mp->entry.tunnel_dst);
   vat_json_object_add_uint (node, "replay_window",
 			    clib_net_to_host_u64 (mp->replay_window));
   vat_json_object_add_uint (node, "total_data_size",
 			    clib_net_to_host_u64 (mp->total_data_size));
-
 }
 
 static int
@@ -20085,8 +20101,6 @@ static void vl_api_ip_neighbor_details_t_handler_json
 
   vat_main_t *vam = &vat_main;
   vat_json_node_t *node;
-  struct in_addr ip4;
-  struct in6_addr ip6;
 
   if (VAT_JSON_ARRAY != vam->json_tree.type)
     {
@@ -20104,17 +20118,7 @@ static void vl_api_ip_neighbor_details_t_handler_json
   vat_json_object_add_string_copy (node, "link_layer",
 				   format (0, "%U", format_vl_api_mac_address,
 					   &mp->neighbor.mac_address));
-
-  if (ADDRESS_IP6 == mp->neighbor.ip_address.af)
-    {
-      clib_memcpy (&ip6, &mp->neighbor.ip_address.un.ip6, sizeof (ip6));
-      vat_json_object_add_ip6 (node, "ip_address", ip6);
-    }
-  else
-    {
-      clib_memcpy (&ip4, &mp->neighbor.ip_address.un.ip4, sizeof (ip4));
-      vat_json_object_add_ip4 (node, "ip_address", ip4);
-    }
+  vat_json_object_add_address (node, &mp->neighbor.ip_address);
 }
 
 static int

src/vnet/fib/fib_node.h
@@ -48,6 +48,7 @@ typedef enum fib_node_type_t_ {
     FIB_NODE_TYPE_BIER_FMASK,
     FIB_NODE_TYPE_BIER_ENTRY,
     FIB_NODE_TYPE_VXLAN_GBP_TUNNEL,
+    FIB_NODE_TYPE_IPSEC_SA,
     /**
      * Marker. New types before this one. leave the test last.
      */
@@ -75,7 +76,8 @@ typedef enum fib_node_type_t_ {
     [FIB_NODE_TYPE_UDP_ENCAP] = "udp-encap",			\
     [FIB_NODE_TYPE_BIER_FMASK] = "bier-fmask",			\
     [FIB_NODE_TYPE_BIER_ENTRY] = "bier-entry",			\
-    [FIB_NODE_TYPE_VXLAN_GBP_TUNNEL] = "vxlan-gbp-tunnel"	\
+    [FIB_NODE_TYPE_VXLAN_GBP_TUNNEL] = "vxlan-gbp-tunnel",	\
+    [FIB_NODE_TYPE_IPSEC_SA] = "ipsec-sa"                       \
 }
 
 /**

src/vnet/ipsec/ah_decrypt.c
@@ -200,9 +200,9 @@ ah_decrypt_inline (vlib_main_t * vm,
 		  icv_padding_len =
 		    ah_calc_icv_padding_len (icv_size, 0 /* is_ipv6 */ );
 		}
-	      hmac_calc (sa0->integ_alg, sa0->integ_key, sa0->integ_key_len,
-			 (u8 *) ih4, i_b0->current_length, sig, sa0->use_esn,
-			 sa0->seq_hi);
+	      hmac_calc (sa0->integ_alg, sa0->integ_key.data,
+			 sa0->integ_key.len, (u8 *) ih4, i_b0->current_length,
+			 sig, sa0->use_esn, sa0->seq_hi);
 
 	      if (PREDICT_FALSE (memcmp (digest, sig, icv_size)))
 		{

src/vnet/ipsec/ah_encrypt.c
@@ -59,6 +59,7 @@ static char *ah_encrypt_error_strings[] = {
 
 typedef struct
 {
+  u32 sa_index;
   u32 spi;
   u32 seq;
   ipsec_integ_alg_t integ_alg;
@@ -72,8 +73,9 @@ format_ah_encrypt_trace (u8 * s, va_list * args)
   CLIB_UNUSED (vlib_node_t * node) = va_arg (*args, vlib_node_t *);
   ah_encrypt_trace_t *t = va_arg (*args, ah_encrypt_trace_t *);
 
-  s = format (s, "ah: spi %u seq %u integrity %U",
-	      t->spi, t->seq, format_ipsec_integ_alg, t->integ_alg);
+  s = format (s, "ah: sa-index %d spi %u seq %u integrity %U",
+	      t->sa_index, t->spi, t->seq,
+	      format_ipsec_integ_alg, t->integ_alg);
   return s;
 }
 
@@ -237,8 +239,9 @@ ah_encrypt_inline (vlib_main_t * vm,
 	      oh0->ip4.src_address.as_u32 = sa0->tunnel_src_addr.ip4.as_u32;
 	      oh0->ip4.dst_address.as_u32 = sa0->tunnel_dst_addr.ip4.as_u32;
 
-	      next0 = AH_ENCRYPT_NEXT_IP4_LOOKUP;
-	      vnet_buffer (i_b0)->sw_if_index[VLIB_TX] = (u32) ~ 0;
+	      next0 = sa0->dpo[IPSEC_PROTOCOL_AH].dpoi_next_node;
+	      vnet_buffer (i_b0)->ip.adj_index[VLIB_TX] =
+		sa0->dpo[IPSEC_PROTOCOL_AH].dpoi_index;
 	    }
 	  else if (is_ip6 && sa0->is_tunnel && sa0->is_tunnel_ip6)
 	    {
@@ -251,8 +254,9 @@ ah_encrypt_inline (vlib_main_t * vm,
 	      oh6_0->ip6.dst_address.as_u64[1] =
 		sa0->tunnel_dst_addr.ip6.as_u64[1];
 
-	      next0 = AH_ENCRYPT_NEXT_IP6_LOOKUP;
-	      vnet_buffer (i_b0)->sw_if_index[VLIB_TX] = (u32) ~ 0;
+	      next0 = sa0->dpo[IPSEC_PROTOCOL_AH].dpoi_next_node;
+	      vnet_buffer (i_b0)->ip.adj_index[VLIB_TX] =
+		sa0->dpo[IPSEC_PROTOCOL_AH].dpoi_index;
 	    }
 
 	  u8 sig[64];
@@ -262,8 +266,8 @@ ah_encrypt_inline (vlib_main_t * vm,
 	    sizeof (ah_header_t);
 	  clib_memset (digest, 0, icv_size);
 
-	  unsigned size = hmac_calc (sa0->integ_alg, sa0->integ_key,
-				     sa0->integ_key_len,
+	  unsigned size = hmac_calc (sa0->integ_alg, sa0->integ_key.data,
+				     sa0->integ_key.len,
 				     vlib_buffer_get_current (i_b0),
 				     i_b0->current_length, sig, sa0->use_esn,
 				     sa0->seq_hi);
@@ -297,6 +301,7 @@ ah_encrypt_inline (vlib_main_t * vm,
 	      tr->spi = sa0->spi;
 	      tr->seq = sa0->seq - 1;
 	      tr->integ_alg = sa0->integ_alg;
+	      tr->sa_index = sa_index0;
 	    }
 
 	  vlib_validate_buffer_enqueue_x1 (vm, node, next_index,

src/vnet/ipsec/esp_decrypt.c
@@ -206,8 +206,9 @@ esp_decrypt_inline (vlib_main_t * vm,
 		icv_size;
 	      i_b0->current_length -= icv_size;
 
-	      hmac_calc (sa0->integ_alg, sa0->integ_key, sa0->integ_key_len,
-			 (u8 *) esp0, i_b0->current_length, sig, sa0->use_esn,
+	      hmac_calc (sa0->integ_alg, sa0->integ_key.data,
+			 sa0->integ_key.len, (u8 *) esp0,
+			 i_b0->current_length, sig, sa0->use_esn,
 			 sa0->seq_hi);
 
 	      if (PREDICT_FALSE (memcmp (icv, sig, icv_size)))
@@ -299,7 +300,7 @@ esp_decrypt_inline (vlib_main_t * vm,
 			       esp0->data + IV_SIZE,
 			       (u8 *) vlib_buffer_get_current (o_b0) +
 			       ip_hdr_size, BLOCK_SIZE * blocks,
-			       sa0->crypto_key, esp0->data);
+			       sa0->crypto_key.data, esp0->data);
 
 	      o_b0->current_length = (blocks * BLOCK_SIZE) - 2 + ip_hdr_size;
 	      o_b0->flags = VLIB_BUFFER_TOTAL_LENGTH_VALID;

src/vnet/ipsec/esp_encrypt.c
@@ -62,6 +62,7 @@ static char *esp_encrypt_error_strings[] = {
 
 typedef struct
 {
+  u32 sa_index;
   u32 spi;
   u32 seq;
   u8 udp_encap;
@@ -77,8 +78,8 @@ format_esp_encrypt_trace (u8 * s, va_list * args)
   CLIB_UNUSED (vlib_node_t * node) = va_arg (*args, vlib_node_t *);
   esp_encrypt_trace_t *t = va_arg (*args, esp_encrypt_trace_t *);
 
-  s = format (s, "esp: spi %u seq %u crypto %U integrity %U%s",
-	      t->spi, t->seq,
+  s = format (s, "esp: sa-index %d spi %u seq %u crypto %U integrity %U%s",
+	      t->sa_index, t->spi, t->seq,
 	      format_ipsec_crypto_alg, t->crypto_alg,
 	      format_ipsec_integ_alg, t->integ_alg,
 	      t->udp_encap ? " udp-encap-enabled" : "");
@@ -279,7 +280,9 @@ esp_encrypt_inline (vlib_main_t * vm,
 	      oh0->ip4.src_address.as_u32 = sa0->tunnel_src_addr.ip4.as_u32;
 	      oh0->ip4.dst_address.as_u32 = sa0->tunnel_dst_addr.ip4.as_u32;
 
-	      vnet_buffer (o_b0)->sw_if_index[VLIB_TX] = sa0->tx_fib_index;
+	      next0 = sa0->dpo[IPSEC_PROTOCOL_ESP].dpoi_next_node;
+	      vnet_buffer (o_b0)->ip.adj_index[VLIB_TX] =
+		sa0->dpo[IPSEC_PROTOCOL_ESP].dpoi_index;
 	    }
 	  else if (is_ip6 && sa0->is_tunnel && sa0->is_tunnel_ip6)
 	    {
@@ -292,7 +295,9 @@ esp_encrypt_inline (vlib_main_t * vm,
 	      oh6_0->ip6.dst_address.as_u64[1] =
 		sa0->tunnel_dst_addr.ip6.as_u64[1];
 
-	      vnet_buffer (o_b0)->sw_if_index[VLIB_TX] = sa0->tx_fib_index;
+	      next0 = sa0->dpo[IPSEC_PROTOCOL_ESP].dpoi_next_node;
+	      vnet_buffer (o_b0)->ip.adj_index[VLIB_TX] =
+		sa0->dpo[IPSEC_PROTOCOL_ESP].dpoi_index;
 	    }
 	  else
 	    {
@@ -367,17 +372,15 @@ esp_encrypt_inline (vlib_main_t * vm,
 			       (u8 *) vlib_buffer_get_current (o_b0) +
 			       ip_udp_hdr_size + sizeof (esp_header_t) +
 			       IV_SIZE, BLOCK_SIZE * blocks,
-			       sa0->crypto_key, iv);
+			       sa0->crypto_key.data, iv);
 	    }
 
-	  o_b0->current_length += hmac_calc (sa0->integ_alg, sa0->integ_key,
-					     sa0->integ_key_len,
-					     (u8 *) o_esp0,
-					     o_b0->current_length -
-					     ip_udp_hdr_size,
-					     vlib_buffer_get_current (o_b0) +
-					     o_b0->current_length,
-					     sa0->use_esn, sa0->seq_hi);
+	  o_b0->current_length +=
+	    hmac_calc (sa0->integ_alg, sa0->integ_key.data,
+		       sa0->integ_key.len, (u8 *) o_esp0,
+		       o_b0->current_length - ip_udp_hdr_size,
+		       vlib_buffer_get_current (o_b0) + o_b0->current_length,
+		       sa0->use_esn, sa0->seq_hi);
 
 
 	  if (is_ip6)
@@ -412,6 +415,7 @@ esp_encrypt_inline (vlib_main_t * vm,
 		  o_b0->trace_index = i_b0->trace_index;
 		  esp_encrypt_trace_t *tr =
 		    vlib_add_trace (vm, node, o_b0, sizeof (*tr));
+		  tr->sa_index = sa_index0;
 		  tr->spi = sa0->spi;
 		  tr->seq = sa0->seq - 1;
 		  tr->udp_encap = sa0->udp_encap;

commit 236445b9e49334f5fc1c05793b8b5de783a85275

ipsec: Display user specified SPI ID not VPP's index
Type: fix
Fixes: 9b4514c341

Change-Id: Ia6de250f20200c17937d9d7b2aab17ccd81d7823
Signed-off-by: Neale Ranns <nranns@cisco.com>

src/vnet/ipsec/ipsec_cli.c
@@ -374,12 +374,14 @@ static void
 ipsec_spd_bindings_show_all (vlib_main_t * vm, ipsec_main_t * im)
 {
   u32 spd_id, sw_if_index;
+  ipsec_spd_t *spd;
 
   vlib_cli_output (vm, "SPD Bindings:");
 
   /* *INDENT-OFF* */
   hash_foreach(sw_if_index, spd_id, im->spd_index_by_sw_if_index, ({
-    vlib_cli_output (vm, "  %d -> %U", spd_id,
+    spd = pool_elt_at_index (im->spds, spd_id);
+    vlib_cli_output (vm, "  %d -> %U", spd->id,
                      format_vnet_sw_if_index_name, im->vnet_main,
                      sw_if_index);
   }));