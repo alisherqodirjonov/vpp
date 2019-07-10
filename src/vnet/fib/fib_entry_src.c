alisherqodirjonov/vpp

commit 98af8f40f73198d2233662cd1489df1008f330c2

fib: fib api updates
Enhance the route add/del APIs to take a set of paths rather than just one.
Most unicast routing protocols calcualte all the available paths in one
run of the algorithm so updating all the paths at once is beneficial for the client.
two knobs control the behaviour:
  is_multipath - if set the the set of paths passed will be added to those
                 that already exist, otherwise the set will replace them.
  is_add - add or remove the set

is_add=0, is_multipath=1 and an empty set, results in deleting the route.

It is also considerably faster to add multiple paths at once, than one at a time:

vat# ip_add_del_route 1.1.1.1/32 count 100000 multipath via 10.10.10.11
100000 routes in .572240 secs, 174751.80 routes/sec
vat# ip_add_del_route 1.1.1.1/32 count 100000 multipath via 10.10.10.12
100000 routes in .528383 secs, 189256.54 routes/sec
vat# ip_add_del_route 1.1.1.1/32 count 100000 multipath via 10.10.10.13
100000 routes in .757131 secs, 132077.52 routes/sec
vat# ip_add_del_route 1.1.1.1/32 count 100000 multipath via 10.10.10.14
100000 routes in .878317 secs, 113854.12 routes/sec

vat# ip_route_add_del 1.1.1.1/32 count 100000 multipath via 10.10.10.11 via 10.10.10.12 via 10.10.10.13 via 10.10.10.14
100000 routes in .900212 secs, 111084.93 routes/sec

Change-Id: I416b93f7684745099c1adb0b33edac58c9339c1a
Signed-off-by: Neale Ranns <neale.ranns@cisco.com>
Signed-off-by: Ole Troan <ot@cisco.com>
Signed-off-by: Paul Vinciguerra <pvinci@vinciconsulting.com>

src/vnet/fib/fib_entry_src.c
@@ -1490,34 +1490,39 @@ fib_entry_src_flags_2_path_list_flags (fib_entry_flag_t eflags)
 
 static void
 fib_entry_flags_update (const fib_entry_t *fib_entry,
-			const fib_route_path_t *rpath,
+			const fib_route_path_t *rpaths,
 			fib_path_list_flags_t *pl_flags,
 			fib_entry_src_t *esrc)
 {
-    if ((esrc->fes_src == FIB_SOURCE_API) ||
-	(esrc->fes_src == FIB_SOURCE_CLI))
+    const fib_route_path_t *rpath;
+
+    vec_foreach(rpath, rpaths)
     {
-	if (fib_path_is_attached(rpath))
-	{
-	    esrc->fes_entry_flags |= FIB_ENTRY_FLAG_ATTACHED;
-	}
-	else
-	{
-	    esrc->fes_entry_flags &= ~FIB_ENTRY_FLAG_ATTACHED;
-	}
-	if (rpath->frp_flags & FIB_ROUTE_PATH_DEAG)
-	{
-	    esrc->fes_entry_flags |= FIB_ENTRY_FLAG_LOOSE_URPF_EXEMPT;
-	}
-    }
-    if (fib_route_attached_cross_table(fib_entry, rpath) &&
-        !(esrc->fes_entry_flags & FIB_ENTRY_FLAG_NO_ATTACHED_EXPORT))
-    {
-	esrc->fes_entry_flags |= FIB_ENTRY_FLAG_IMPORT;
-    }
-    else
-    {
-	esrc->fes_entry_flags &= ~FIB_ENTRY_FLAG_IMPORT;
+        if ((esrc->fes_src == FIB_SOURCE_API) ||
+            (esrc->fes_src == FIB_SOURCE_CLI))
+        {
+            if (fib_path_is_attached(rpath))
+            {
+                esrc->fes_entry_flags |= FIB_ENTRY_FLAG_ATTACHED;
+            }
+            else
+            {
+                esrc->fes_entry_flags &= ~FIB_ENTRY_FLAG_ATTACHED;
+            }
+            if (rpath->frp_flags & FIB_ROUTE_PATH_DEAG)
+            {
+                esrc->fes_entry_flags |= FIB_ENTRY_FLAG_LOOSE_URPF_EXEMPT;
+            }
+        }
+        if (fib_route_attached_cross_table(fib_entry, rpath) &&
+            !(esrc->fes_entry_flags & FIB_ENTRY_FLAG_NO_ATTACHED_EXPORT))
+        {
+            esrc->fes_entry_flags |= FIB_ENTRY_FLAG_IMPORT;
+        }
+        else
+        {
+            esrc->fes_entry_flags &= ~FIB_ENTRY_FLAG_IMPORT;
+        }
     }
 }
 
@@ -1533,7 +1538,7 @@ fib_entry_t*
 fib_entry_src_action_path_add (fib_entry_t *fib_entry,
 			       fib_source_t source,
 			       fib_entry_flag_t flags,
-			       const fib_route_path_t *rpath)
+			       const fib_route_path_t *rpaths)
 {
     fib_node_index_t old_path_list, fib_entry_index;
     fib_path_list_flags_t pl_flags;
@@ -1550,7 +1555,7 @@ fib_entry_src_action_path_add (fib_entry_t *fib_entry,
 	const dpo_id_t *dpo;
 
 	if (flags == FIB_ENTRY_FLAG_EXCLUSIVE) {
-	    dpo = &rpath->dpo;
+	    dpo = &rpaths->dpo;
 	} else {
 	    dpo = drop_dpo_get(fib_entry_get_dpo_proto(fib_entry));
 	}
@@ -1574,10 +1579,10 @@ fib_entry_src_action_path_add (fib_entry_t *fib_entry,
     ASSERT(FIB_ENTRY_SRC_VFT_EXISTS(esrc, fesv_path_add));
 
     pl_flags = fib_entry_src_flags_2_path_list_flags(fib_entry_get_flags_i(fib_entry));
-    fib_entry_flags_update(fib_entry, rpath, &pl_flags, esrc);
+    fib_entry_flags_update(fib_entry, rpaths, &pl_flags, esrc);
 
     FIB_ENTRY_SRC_VFT_INVOKE(esrc, fesv_path_add,
-                             (esrc, fib_entry, pl_flags, rpath));
+                             (esrc, fib_entry, pl_flags, rpaths));
     fib_entry = fib_entry_get(fib_entry_index);
 
     fib_path_list_lock(esrc->fes_pl);
@@ -1670,7 +1675,7 @@ fib_entry_src_action_path_swap (fib_entry_t *fib_entry,
 fib_entry_src_flag_t
 fib_entry_src_action_path_remove (fib_entry_t *fib_entry,
 				  fib_source_t source,
-				  const fib_route_path_t *rpath)
+				  const fib_route_path_t *rpaths)
 {
     fib_path_list_flags_t pl_flags;
     fib_node_index_t old_path_list;
@@ -1692,10 +1697,10 @@ fib_entry_src_action_path_remove (fib_entry_t *fib_entry,
     ASSERT(FIB_ENTRY_SRC_VFT_EXISTS(esrc, fesv_path_remove));
 
     pl_flags = fib_entry_src_flags_2_path_list_flags(fib_entry_get_flags_i(fib_entry));
-    fib_entry_flags_update(fib_entry, rpath, &pl_flags, esrc);
+    fib_entry_flags_update(fib_entry, rpaths, &pl_flags, esrc);
 
     FIB_ENTRY_SRC_VFT_INVOKE(esrc, fesv_path_remove,
-                             (esrc, pl_flags, rpath));
+                             (esrc, pl_flags, rpaths));
 
     /*
      * lock the new path-list, unlock the old if it had one

commit 20c8d35ace3fb77bffd207d1b25dfc174bab4f9d

fib: fib_entry_flags_update takes a vector of paths
Type: fix
Fixes: 98af8f40

Change-Id: I690e31433b64f11399c08b4a0318762916c2c2f0
Signed-off-by: Neale Ranns <nranns@cisco.com>

src/vnet/fib/fib_entry_src.c
@@ -1608,7 +1608,6 @@ fib_entry_src_action_path_swap (fib_entry_t *fib_entry,
 {
     fib_node_index_t old_path_list, fib_entry_index;
     fib_path_list_flags_t pl_flags;
-    const fib_route_path_t *rpath;
     fib_entry_src_t *esrc;
 
     esrc = fib_entry_src_find(fib_entry, source);
@@ -1655,10 +1654,7 @@ fib_entry_src_action_path_swap (fib_entry_t *fib_entry,
 
     pl_flags = fib_entry_src_flags_2_path_list_flags(flags);
 
-    vec_foreach(rpath, rpaths)
-    {
-	fib_entry_flags_update(fib_entry, rpath, &pl_flags, esrc);
-    }
+    fib_entry_flags_update(fib_entry, rpaths, &pl_flags, esrc);
 
     FIB_ENTRY_SRC_VFT_INVOKE(esrc, fesv_path_swap,
                              (esrc, fib_entry,