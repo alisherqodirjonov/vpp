alisherqodirjonov/vpp

commit ee05795211c27cc46e9fd4ddfbe62d799e2162fa

api: refactor format_vl_api_prefix_t return keys
format_vl_api_prefix_t returns a dict with keys 'address' and 'address_length',
but other format_vl_api_prefix functions return a dict with 'prefix', and 'len'.

Refactor all format_vl_api_prefix_t to return consistent keys 'address' and 'len'.

Type: refactor

Change-Id: I5f9558fc2da8742a303266e011102f5b2db80aad
Signed-off-by: Paul Vinciguerra <pvinci@vinciconsulting.com>

src/vpp-api/python/vpp_papi/vpp_format.py
@@ -45,29 +45,29 @@ def format_vl_api_address_t(args):
 
 def format_vl_api_prefix_t(args):
     if isinstance(args, (ipaddress.IPv4Network, ipaddress.IPv6Network)):
-        return {'prefix': format_vl_api_address_t(
+        return {'address': format_vl_api_address_t(
             text_type(args.network_address)),
                 'len': int(args.prefixlen)}
     p, length = args.split('/')
     return {'address': format_vl_api_address_t(p),
-            'address_length': int(length)}
+            'len': int(length)}
 
 
 def format_vl_api_ip6_prefix_t(args):
     if isinstance(args, ipaddress.IPv6Network):
-        return {'prefix': args.network_address.packed,
+        return {'address': args.network_address.packed,
                 'len': int(args.prefixlen)}
     p, length = args.split('/')
-    return {'prefix': inet_pton(AF_INET6, p),
+    return {'address': inet_pton(AF_INET6, p),
             'len': int(length)}
 
 
 def format_vl_api_ip4_prefix_t(args):
     if isinstance(args, ipaddress.IPv4Network):
-        return {'prefix': args.network_address.packed,
+        return {'address': args.network_address.packed,
                 'len': int(args.prefixlen)}
     p, length = args.split('/')
-    return {'prefix': inet_pton(AF_INET, p),
+    return {'address': inet_pton(AF_INET, p),
             'len': int(length)}
 
 
@@ -84,13 +84,13 @@ conversion_table = {
     },
     'vl_api_ip6_prefix_t':
     {
-        'IPv6Network': lambda o: {'prefix': o.network_address.packed,
+        'IPv6Network': lambda o: {'address': o.network_address.packed,
                                   'len': o.prefixlen},
         'str': lambda s: format_vl_api_ip6_prefix_t(s)
     },
     'vl_api_ip4_prefix_t':
     {
-        'IPv4Network': lambda o: {'prefix': o.network_address.packed,
+        'IPv4Network': lambda o: {'address': o.network_address.packed,
                                   'len': o.prefixlen},
         'str': lambda s: format_vl_api_ip4_prefix_t(s)
     },
@@ -129,16 +129,16 @@ def unformat_api_address_t(o):
 
 def unformat_api_prefix_t(o):
     if isinstance(o.address, ipaddress.IPv4Address):
-        return ipaddress.IPv4Network((o.address, o.address_length), False)
+        return ipaddress.IPv4Network((o.address, o.len), False)
     if isinstance(o.address, ipaddress.IPv6Address):
-        return ipaddress.IPv6Network((o.address, o.address_length), False)
+        return ipaddress.IPv6Network((o.address, o.len), False)
 
 
 conversion_unpacker_table = {
     'vl_api_ip6_address_t': lambda o: ipaddress.IPv6Address(o),
-    'vl_api_ip6_prefix_t': lambda o: ipaddress.IPv6Network((o.prefix, o.len)),
+    'vl_api_ip6_prefix_t': lambda o: ipaddress.IPv6Network((o.address, o.len)),
     'vl_api_ip4_address_t': lambda o: ipaddress.IPv4Address(o),
-    'vl_api_ip4_prefix_t': lambda o: ipaddress.IPv4Network((o.prefix, o.len)),
+    'vl_api_ip4_prefix_t': lambda o: ipaddress.IPv4Network((o.address, o.len)),
     'vl_api_address_t': lambda o: unformat_api_address_t(o),
     'vl_api_prefix_t': lambda o: unformat_api_prefix_t(o),
     'vl_api_mac_address_t': lambda o: macaddress.MACAddress(o),

extras/vom/vom/api_types.cpp
@@ -192,7 +192,7 @@ from_api(const vapi_type_mac_address& v)
 route::prefix_t
 from_api(const vapi_type_prefix& v)
 {
-  return route::prefix_t(from_api(v.address), v.address_length);
+  return route::prefix_t(from_api(v.address), v.len);
 }
 
 vapi_type_prefix
@@ -200,7 +200,7 @@ to_api(const route::prefix_t& p)
 {
   vapi_type_prefix v;
   to_api(p.address(), v.address);
-  v.address_length = p.mask_width();
+  v.len = p.mask_width();
   return v;
 }
 

src/plugins/gbp/gbp_api_print.h
@@ -218,7 +218,7 @@ vl_api_gbp_subnet_add_del_t_print (vl_api_gbp_subnet_add_del_t * a,
   s = format (s, "type %d ", ntohl (a->subnet.type));
   s =
     format (s, "prefix %U/%d", format_vl_api_address,
-	    &a->subnet.prefix.address, a->subnet.prefix.address_length);
+	    &a->subnet.prefix.address, a->subnet.prefix.len);
 
   s = format (s, "\n");
 

src/plugins/map/map_api.c
@@ -56,11 +56,11 @@ vl_api_map_add_domain_t_handler (vl_api_map_add_domain_t * mp)
   u8 *vtag = 0;
   vtag = vl_api_from_api_to_vec (&mp->tag);
   rv =
-    map_create_domain ((ip4_address_t *) & mp->ip4_prefix.prefix,
+    map_create_domain ((ip4_address_t *) & mp->ip4_prefix.address,
 		       mp->ip4_prefix.len,
-		       (ip6_address_t *) & mp->ip6_prefix.prefix,
+		       (ip6_address_t *) & mp->ip6_prefix.address,
 		       mp->ip6_prefix.len,
-		       (ip6_address_t *) & mp->ip6_src.prefix,
+		       (ip6_address_t *) & mp->ip6_src.address,
 		       mp->ip6_src.len, mp->ea_bits_len, mp->psid_offset,
 		       mp->psid_length, &index, ntohs (mp->mtu), flags, vtag);
   vec_free (vtag);
@@ -128,9 +128,9 @@ vl_api_map_domain_dump_t_handler (vl_api_map_domain_dump_t * mp)
     rmp->_vl_msg_id = htons(VL_API_MAP_DOMAIN_DETAILS + mm->msg_id_base);
     rmp->context = mp->context;
     rmp->domain_index = htonl(map_domain_index);
-    clib_memcpy(&rmp->ip6_prefix.prefix, &d->ip6_prefix, sizeof(rmp->ip6_prefix.prefix));
-    clib_memcpy(&rmp->ip4_prefix.prefix, &d->ip4_prefix, sizeof(rmp->ip4_prefix.prefix));
-    clib_memcpy(&rmp->ip6_src.prefix, &d->ip6_src, sizeof(rmp->ip6_src.prefix));
+    clib_memcpy(&rmp->ip6_prefix.address, &d->ip6_prefix, sizeof(rmp->ip6_prefix.address));
+    clib_memcpy(&rmp->ip4_prefix.address, &d->ip4_prefix, sizeof(rmp->ip4_prefix.address));
+    clib_memcpy(&rmp->ip6_src.address, &d->ip6_src, sizeof(rmp->ip6_src.address));
     rmp->ip6_prefix.len = d->ip6_prefix_len;
     rmp->ip4_prefix.len = d->ip4_prefix_len;
     rmp->ip6_src.len = d->ip6_src_len;

src/plugins/nat/nat_api.c
@@ -3007,7 +3007,7 @@ vl_api_nat64_add_del_prefix_t_handler (vl_api_nat64_add_del_prefix_t * mp)
   ip6_address_t prefix;
   int rv = 0;
 
-  memcpy (&prefix.as_u8, mp->prefix.prefix, 16);
+  memcpy (&prefix.as_u8, mp->prefix.address, 16);
 
   rv =
     nat64_add_del_prefix (&prefix, mp->prefix.len,
@@ -3022,7 +3022,7 @@ vl_api_nat64_add_del_prefix_t_print (vl_api_nat64_add_del_prefix_t * mp,
   u8 *s;
 
   s = format (0, "SCRIPT: nat64_add_del_prefix %U/%u vrf_id %u %s\n",
-	      format_ip6_address, mp->prefix.prefix, mp->prefix.len,
+	      format_ip6_address, mp->prefix.address, mp->prefix.len,
 	      ntohl (mp->vrf_id), mp->is_add ? "" : "del");
 
   FINISH;
@@ -3038,7 +3038,7 @@ nat64_api_prefix_walk (nat64_prefix_t * p, void *arg)
   rmp = vl_msg_api_alloc (sizeof (*rmp));
   clib_memset (rmp, 0, sizeof (*rmp));
   rmp->_vl_msg_id = ntohs (VL_API_NAT64_PREFIX_DETAILS + sm->msg_id_base);
-  clib_memcpy (rmp->prefix.prefix, &(p->prefix), 16);
+  clib_memcpy (rmp->prefix.address, &(p->prefix), 16);
   rmp->prefix.len = p->plen;
   rmp->vrf_id = ntohl (p->vrf_id);
   rmp->context = ctx->context;

src/vat/api_format.c
@@ -788,8 +788,8 @@ static void
 vat_json_object_add_prefix (vat_json_node_t * node,
 			    const vl_api_prefix_t * prefix)
 {
-  vat_json_object_add_uint (node, "address_length", prefix->address_length);
-  vat_json_object_add_address (node, "prefix", &prefix->address);
+  vat_json_object_add_uint (node, "len", prefix->len);
+  vat_json_object_add_address (node, "address", &prefix->address);
 }
 
 static void vl_api_create_loopback_reply_t_handler
@@ -2663,7 +2663,7 @@ static void vl_api_ip_address_details_t_handler
   address = vec_elt_at_index (addresses, vec_len (addresses) - 1);
 
   clib_memcpy (&address->ip, &mp->prefix.address.un, sizeof (address->ip));
-  address->prefix_length = mp->prefix.address_length;
+  address->prefix_length = mp->prefix.len;
 #undef addresses
 }
 
@@ -19201,8 +19201,7 @@ vl_api_ip_route_details_t_handler (vl_api_ip_route_details_t * mp)
   print (vam->ofp,
 	 "table-id %d, prefix %U/%d",
 	 ntohl (mp->route.table_id),
-	 format_ip46_address,
-	 mp->route.prefix.address, mp->route.prefix.address_length);
+	 format_ip46_address, mp->route.prefix.address, mp->route.prefix.len);
   for (i = 0; i < count; i++)
     {
       fp = &mp->route.paths[i];
@@ -19242,8 +19241,7 @@ static void vl_api_ip_route_details_t_handler_json
       clib_memcpy (&ip4, &mp->route.prefix.address.un.ip4, sizeof (ip4));
       vat_json_object_add_ip4 (node, "prefix", ip4);
     }
-  vat_json_object_add_uint (node, "mask_length",
-			    mp->route.prefix.address_length);
+  vat_json_object_add_uint (node, "mask_length", mp->route.prefix.len);
   vat_json_object_add_uint (node, "path_count", count);
   for (i = 0; i < count; i++)
     {
@@ -21310,7 +21308,7 @@ api_ip_container_proxy_add_del (vat_main_t * vam)
       else
 	break;
     }
-  if (sw_if_index == ~0 || pfx.address_length == 0)
+  if (sw_if_index == ~0 || pfx.len == 0)
     {
       errmsg ("address and sw_if_index must be set");
       return -99;

src/vnet/ip/ip_types.api
@@ -41,7 +41,7 @@ typedef address {
 
 typedef prefix {
   vl_api_address_t address;
-  u8 address_length;
+  u8 len;
 };
 
 typedef mprefix {
@@ -52,11 +52,11 @@ typedef mprefix {
 };
 
 typedef ip6_prefix {
-  vl_api_ip6_address_t prefix;
+  vl_api_ip6_address_t address;
   u8 len;
 };
 
 typedef ip4_prefix {
-  vl_api_ip4_address_t prefix;
+  vl_api_ip4_address_t address;
   u8 len;
 };

src/vnet/ip/ip_types_api.c
@@ -196,7 +196,7 @@ ip_prefix_decode (const vl_api_prefix_t * in, fib_prefix_t * out)
       out->fp_proto = FIB_PROTOCOL_IP6;
       break;
     }
-  out->fp_len = in->address_length;
+  out->fp_len = in->len;
   out->___fp___pad = 0;
   ip_address_decode (&in->address, &out->fp_addr);
 }
@@ -204,7 +204,7 @@ ip_prefix_decode (const vl_api_prefix_t * in, fib_prefix_t * out)
 void
 ip_prefix_encode (const fib_prefix_t * in, vl_api_prefix_t * out)
 {
-  out->address_length = in->fp_len;
+  out->len = in->fp_len;
   ip_address_encode (&in->fp_addr,
 		     fib_proto_to_ip46 (in->fp_proto), &out->address);
 }

commit 5c471f8faff7a3e4b4576af405b65165caa1c856

papi: fix vpp_format from change in vl_api_prefix_t
cherry picking fix for bug introduced by https://gerrit.fd.io/r/#/c/20011/

- also fixes unit tests.

Type: fix
Fixes: ee05795211c27cc46e9fd4ddfbe62d799e2162fa

Change-Id: I8287385f094911ea70de4751a716a7e0e6521b64
Signed-off-by: Paul Vinciguerra <pvinci@vinciconsulting.com>

src/vpp-api/python/vpp_papi/vpp_format.py
@@ -102,11 +102,11 @@ conversion_table = {
     },
     'vl_api_prefix_t':
     {
-        'IPv4Network': lambda o: {'prefix':
+        'IPv4Network': lambda o: {'address':
                                   {'af': ADDRESS_IP4, 'un':
                                    {'ip4': o.network_address.packed}},
                                   'len': o.prefixlen},
-        'IPv6Network': lambda o: {'prefix':
+        'IPv6Network': lambda o: {'address':
                                   {'af': ADDRESS_IP6, 'un':
                                    {'ip6': o.network_address.packed}},
                                   'len': o.prefixlen},

src/vpp-api/python/vpp_papi/tests/test_vpp_format.py
@@ -15,6 +15,7 @@
 import ipaddress
 import socket
 import unittest
+
 try:
     text_type = unicode
 except NameError:
@@ -31,11 +32,11 @@ ip4_prefix = '%s/%s' % (ip4_addr, ip4_prefix_len)
 ipv4_network = ipaddress.IPv4Network(text_type(ip4_prefix))
 ip4_addr_format_vl_api_address_t = {'un': {'ip4': b'\x01\x02\x03\x04'},
                                     'af': 0}
-ip4_addr_format_vl_api_prefix_t = {'prefix':
+ip4_addr_format_vl_api_prefix_t = {'address':                                # noqa: E127,E501
                                        {'un': {'ip4': b'\x01\x02\x03\x04'},
                                         'af': 0},
                                    'len': ip4_prefix_len}
-ip4_addr_format_vl_api_prefix_packed_t = {'prefix': b'\x01\x02\x03\x04',
+ip4_addr_format_vl_api_prefix_packed_t = {'address': b'\x01\x02\x03\x04',
                                           'len': ip4_prefix_len}
 
 ip6_addr = 'dead::'
@@ -49,7 +50,7 @@ ip6_addr_format_vl_api_address_t = {'un': {'ip6': b'\xde\xad\x00\x00'
                                                   b'\x00\x00\x00\x00'
                                                   b'\x00\x00\x00\x00'},
                                     'af': 1}
-ip6_addr_format_vl_api_prefix_t = {'prefix':
+ip6_addr_format_vl_api_prefix_t = {'address':       # noqa: E127
                                        {'af': 1,
                                         'un': {
                                             'ip6': b'\xde\xad\x00\x00'
@@ -57,10 +58,10 @@ ip6_addr_format_vl_api_prefix_t = {'prefix':
                                                    b'\x00\x00\x00\x00'
                                                    b'\x00\x00\x00\x00'}},
                                    'len': ip6_prefix_len}
-ip6_addr_format_vl_api_prefix_packed_t = {'prefix': b'\xde\xad\x00\x00'
-                                                    b'\x00\x00\x00\x00'
-                                                    b'\x00\x00\x00\x00'
-                                                    b'\x00\x00\x00\x00',
+ip6_addr_format_vl_api_prefix_packed_t = {'address': b'\xde\xad\x00\x00'   # noqa: E127,E501
+                                                     b'\x00\x00\x00\x00'
+                                                     b'\x00\x00\x00\x00'
+                                                     b'\x00\x00\x00\x00',
                                           'len': ip6_prefix_len}
 
 
@@ -97,7 +98,6 @@ class TestVppFormat(unittest.TestCase):
                             ip6_addr_format_vl_api_prefix_t),
                            ])
     def test_format_vl_api_prefix_t(self, _, arg, expected):
-
         res = vpp_format.format_vl_api_prefix_t(arg)
         self.assertEqual(res, expected)
 
@@ -115,7 +115,6 @@ class TestVppFormat(unittest.TestCase):
         res = vpp_format.format_vl_api_ip4_prefix_t(ipv4_network)
         self.assertEqual(res, ip4_addr_format_vl_api_prefix_packed_t)
 
-
     def test_format_vl_api_ip6_prefix_t_raises(self):
         # PY2: raises socket.error
         # PY3: raises OSError
@@ -127,4 +126,3 @@ class TestVppFormat(unittest.TestCase):
         # PY3: raises OSError
         with self.assertRaises((socket.error, OSError)):
             res = vpp_format.format_vl_api_ip4_prefix_t(ip6_prefix)
-