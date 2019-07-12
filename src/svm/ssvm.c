alisherqodirjonov/vpp

commit cbfd165d86f6d9e1757468d0774726fc822aa555

Add config option to use dlmalloc instead of mheap
Configure w/ --enable-dlmalloc, see .../build-data/platforms/vpp.mk

src/vppinfra/dlmalloc.[ch] are slightly modified versions of the
well-known Doug Lea malloc. Main advantage: dlmalloc mspaces have no
inherent size limit.

Change-Id: I19b3f43f3c65bcfb82c1a265a97922d01912446e
Signed-off-by: Dave Barach <dave@barachs.net>

src/svm/ssvm.c
@@ -28,7 +28,10 @@ static delete_fn delete_fns[SSVM_N_SEGMENT_TYPES] =
 int
 ssvm_master_init_shm (ssvm_private_t * ssvm)
 {
-  int ssvm_fd, mh_flags = MHEAP_FLAG_DISABLE_VM | MHEAP_FLAG_THREAD_SAFE;
+  int ssvm_fd;
+#if USE_DLMALLOC == 0
+  int mh_flags = MHEAP_FLAG_DISABLE_VM | MHEAP_FLAG_THREAD_SAFE;
+#endif
   clib_mem_vm_map_t mapa = { 0 };
   u8 junk = 0, *ssvm_filename;
   ssvm_shared_header_t *sh;
@@ -100,8 +103,15 @@ ssvm_master_init_shm (ssvm_private_t * ssvm)
   sh->ssvm_size = ssvm->ssvm_size;
   sh->ssvm_va = pointer_to_uword (sh);
   sh->type = SSVM_SEGMENT_SHM;
+#if USE_DLMALLOC == 0
   sh->heap = mheap_alloc_with_flags (((u8 *) sh) + page_size,
 				     ssvm->ssvm_size - page_size, mh_flags);
+#else
+  sh->heap = create_mspace_with_base (((u8 *) sh) + page_size,
+				      ssvm->ssvm_size - page_size,
+				      1 /* locked */ );
+  mspace_disable_expand (sh->heap);
+#endif
 
   oldheap = ssvm_push_heap (sh);
   sh->name = format (0, "%s", ssvm->name, 0);
@@ -212,7 +222,7 @@ ssvm_delete_shm (ssvm_private_t * ssvm)
 int
 ssvm_master_init_memfd (ssvm_private_t * memfd)
 {
-  uword page_size, flags = MHEAP_FLAG_DISABLE_VM | MHEAP_FLAG_THREAD_SAFE;
+  uword page_size;
   ssvm_shared_header_t *sh;
   void *oldheap;
   clib_mem_vm_alloc_t alloc = { 0 };
@@ -244,9 +254,18 @@ ssvm_master_init_memfd (ssvm_private_t * memfd)
   sh->ssvm_size = memfd->ssvm_size;
   sh->ssvm_va = pointer_to_uword (sh);
   sh->type = SSVM_SEGMENT_MEMFD;
+
+#if USE_DLMALLOC == 0
+  uword flags = MHEAP_FLAG_DISABLE_VM | MHEAP_FLAG_THREAD_SAFE;
+
   sh->heap = mheap_alloc_with_flags (((u8 *) sh) + page_size,
 				     memfd->ssvm_size - page_size, flags);
-
+#else
+  sh->heap = create_mspace_with_base (((u8 *) sh) + page_size,
+				      memfd->ssvm_size - page_size,
+				      1 /* locked */ );
+  mspace_disable_expand (sh->heap);
+#endif
   oldheap = ssvm_push_heap (sh);
   sh->name = format (0, "%s", memfd->name, 0);
   ssvm_pop_heap (oldheap);
@@ -328,22 +347,30 @@ ssvm_delete_memfd (ssvm_private_t * memfd)
 int
 ssvm_master_init_private (ssvm_private_t * ssvm)
 {
-  u32 pagesize = clib_mem_get_page_size ();
   ssvm_shared_header_t *sh;
-  mheap_t *heap_header;
-  u64 rnd_size = 0;
+  u32 pagesize = clib_mem_get_page_size ();
+  u32 rnd_size = 0;
   u8 *heap;
 
   rnd_size = (ssvm->ssvm_size + (pagesize - 1)) & ~(pagesize - 1);
   rnd_size = clib_min (rnd_size, ((u64) 1 << 32) - pagesize);
-  heap = mheap_alloc (0, rnd_size);
-  if (heap == 0)
-    {
-      clib_unix_warning ("mheap alloc");
-      return -1;
-    }
-  heap_header = mheap_header (heap);
-  heap_header->flags |= MHEAP_FLAG_THREAD_SAFE;
+
+#if USE_DLMALLOC == 0
+  {
+    mheap_t *heap_header;
+
+    heap = mheap_alloc (0, rnd_size);
+    if (heap == 0)
+      {
+	clib_unix_warning ("mheap alloc");
+	return -1;
+      }
+    heap_header = mheap_header (heap);
+    heap_header->flags |= MHEAP_FLAG_THREAD_SAFE;
+  }
+#else
+  heap = create_mspace (rnd_size, 1 /* locked */ );
+#endif
 
   ssvm->ssvm_size = rnd_size;
   ssvm->i_am_master = 1;
@@ -372,7 +399,11 @@ void
 ssvm_delete_private (ssvm_private_t * ssvm)
 {
   vec_free (ssvm->name);
+#if USE_DLMALLOC == 0
   mheap_free (ssvm->sh->heap);
+#else
+  destroy_mspace (ssvm->sh->heap);
+#endif
   clib_mem_free (ssvm->sh);
 }
 

build-data/platforms/vpp.mk
@@ -37,7 +37,9 @@ vpp_root_packages = vpp vom
 # vpp_dpdk_shared_lib = yes
 
 # Use '--without-libnuma' for non-numa aware architecture
-vpp_configure_args_vpp =
+# Use '--enable-dlmalloc' to use dlmalloc instead of mheap
+vpp_configure_args_vpp = --enable-dlmalloc
+sample-plugin_configure_args_vpp = --enable-dlmalloc
 
 # load balancer plugin is not portable on 32 bit platform
 ifeq ($(MACHINE),i686)

src/Makefile.am
@@ -19,7 +19,7 @@ AUTOMAKE_OPTIONS = foreign subdir-objects
 ACLOCAL_AMFLAGS = -I m4
 AM_LIBTOOLFLAGS = --quiet
 
-AM_CFLAGS = @CPU_FLAGS@ -Wall
+AM_CFLAGS = @CPU_FLAGS@ @DLMALLOC@ -Wall
 AM_CXXFLAGS = -Wall -std=gnu++11
 
 SUBDIRS = .

src/configure.ac
@@ -190,6 +190,7 @@ ENABLE_ARG(tests,	[Enable unit tests])
 ENABLE_ARG(dpdk_shared,	[Enable unit tests])
 ENABLE_ARG(perftool,	[Enable perftool])
 ENABLE_ARG(g2,		[Enable g2])
+ENABLE_ARG(dlmalloc,    [Enable dlmalloc])
 
 # --disable-X
 DISABLE_ARG(vlib,	[Disable vlib and dependant libs and binaries])
@@ -231,6 +232,8 @@ AC_ARG_WITH(pre-data,
 	       256) ;;
                *) with_pre_data="pre-data-not-set" ;;
 	     esac], [with_pre_data=128])
+
+AM_CONDITIONAL(ENABLE_DLMALLOC, test "$enable_dlmalloc" = "yes")
 
 ###############################################################################
 # Target CPU flags
@@ -278,6 +281,7 @@ AS_IF([test "$cc_flag_check" = yes],
 AC_SUBST(PRE_DATA_SIZE,		[$with_pre_data])
 AC_SUBST(LOG2_CACHE_LINE_BYTES, [$with_log2_cache_line_bytes])
 AC_SUBST(APICLI,		[-DVPP_API_TEST_BUILTIN=${n_with_apicli}])
+AC_SUBST(DLMALLOC,              [-DUSE_DLMALLOC=${n_enable_dlmalloc}])
 
 AC_DEFINE_UNQUOTED(DPDK_SHARED_LIB,	[${n_enable_dpdk_shared}])
 AC_DEFINE_UNQUOTED(WITH_LIBSSL,		[${n_with_libssl}])
@@ -447,6 +451,7 @@ PRINT_VAL([CFLAGS], ${CFLAGS})
 PRINT_VAL([CPPFLAGS], ${CPPFLAGS})
 PRINT_VAL([LDFLAGS], ${LDFLAGS})
 PRINT_VAL([LOG2_CACHE_LINE_BYTES], ${with_log2_cache_line_bytes})
+PRINT_VAL([DLMALLOC], ${enable_dlmalloc})
 AM_COND_IF([ENABLE_JAPI],
 [
   PRINT_VAL([JAVA_VERSION], ${JAVA_VERSION})

src/examples/sample-plugin/Makefile.am
@@ -13,7 +13,7 @@
 
 AUTOMAKE_OPTIONS = foreign subdir-objects
 
-AM_CFLAGS = -Wall -I${top_srcdir} -I${top_builddir}
+AM_CFLAGS = -Wall -I${top_srcdir} -I${top_builddir} @DLMALLOC@
 AM_LDFLAGS = -module -shared -avoid-version
 AM_LIBTOOLFLAGS = --quiet
 SUFFIXES = .api.h .api .api.json

src/examples/sample-plugin/configure.ac
@@ -22,6 +22,19 @@ AC_DEFUN([CC_CHECK_FLAG],
   AC_LANG_POP([C])
 ])
 
+AC_DEFUN([ENABLE_ARG],
+[
+  AC_ARG_ENABLE($1,
+    AC_HELP_STRING(patsubst([--enable-$1],[_],[-]), $2),
+    [enable_$1=yes n_enable_$1=1],
+    [enable_$1=no n_enable_$1=0])
+  AM_CONDITIONAL(m4_toupper(ENABLE_$1), test "$enable_$1" = "yes")
+  m4_append([list_of_enabled], [$1], [, ])
+])
+
+ENABLE_ARG(dlmalloc,    [Enable dlmalloc])
+AC_SUBST(DLMALLOC,              [-DUSE_DLMALLOC=${n_enable_dlmalloc}])
+
 CC_CHECK_FLAG("-Wno-address-of-packed-member")
 AS_IF([test "$cc_flag_check" = yes],
 	[CFLAGS="${CFLAGS} -Wno-address-of-packed-member"], [])

src/plugins/Makefile.am
@@ -14,7 +14,7 @@
 
 AUTOMAKE_OPTIONS = foreign subdir-objects
 
-AM_CFLAGS = @CPU_FLAGS@ -Wall -I${top_srcdir} -I${top_builddir}
+AM_CFLAGS = @CPU_FLAGS@ @DLMALLOC@ -Wall -I${top_srcdir} -I${top_builddir}
 AM_LDFLAGS = -module -shared -avoid-version
 AM_LIBTOOLFLAGS = --quiet
 SUFFIXES = .api.h .api .api.json

src/plugins/acl/acl.c
@@ -139,15 +139,16 @@ acl_set_heap (acl_main_t * am)
 	  clib_warning ("ACL heap size requested: %lld, max possible %lld",
 			am->acl_mheap_size, max_possible);
 	}
-      am->acl_mheap = mheap_alloc (0 /* use VM */ , am->acl_mheap_size);
+
+      am->acl_mheap = mheap_alloc_with_lock (0 /* use VM */ ,
+					     am->acl_mheap_size,
+					     1 /* locked */ );
       if (0 == am->acl_mheap)
 	{
 	  clib_error
 	    ("ACL plugin failed to allocate main heap of %U bytes, abort",
 	     format_memory_size, am->acl_mheap_size);
 	}
-      mheap_t *h = mheap_header (am->acl_mheap);
-      h->flags |= MHEAP_FLAG_THREAD_SAFE;
     }
   void *oldheap = clib_mem_set_heap (am->acl_mheap);
   return oldheap;
@@ -164,6 +165,7 @@ void
 acl_plugin_acl_set_validate_heap (acl_main_t * am, int on)
 {
   clib_mem_set_heap (acl_set_heap (am));
+#if USE_DLMALLOC == 0
   mheap_t *h = mheap_header (am->acl_mheap);
   if (on)
     {
@@ -176,12 +178,14 @@ acl_plugin_acl_set_validate_heap (acl_main_t * am, int on)
       h->flags &= ~MHEAP_FLAG_VALIDATE;
       h->flags |= MHEAP_FLAG_SMALL_OBJECT_CACHE;
     }
+#endif
 }
 
 void
 acl_plugin_acl_set_trace_heap (acl_main_t * am, int on)
 {
   clib_mem_set_heap (acl_set_heap (am));
+#if USE_DLMALLOC == 0
   mheap_t *h = mheap_header (am->acl_mheap);
   if (on)
     {
@@ -191,6 +195,7 @@ acl_plugin_acl_set_trace_heap (acl_main_t * am, int on)
     {
       h->flags &= ~MHEAP_FLAG_TRACE;
     }
+#endif
 }
 
 static void

commit 9b36a89c7cd4836f25727b6c82868190d729433e

svm: handles heap dlmalloc allocation failure
Type: fix
Fixes: cbfd165d86

Change-Id: I21091fc2938cababeb28bacf7c5e457a05ab6272
Signed-off-by: Benoît Ganne <bganne@cisco.com>

src/svm/ssvm.c
@@ -370,6 +370,12 @@ ssvm_master_init_private (ssvm_private_t * ssvm)
   }
 #else
   heap = create_mspace (rnd_size, 1 /* locked */ );
+  if (heap == 0)
+    {
+      clib_unix_warning ("mheap alloc");
+      return -1;
+    }
+
   mspace_disable_expand (heap);
 #endif
 