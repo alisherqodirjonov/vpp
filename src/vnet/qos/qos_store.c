alisherqodirjonov/vpp

commit 47f59edc13a7b1fd68ab7ab7dd886043e9d82c00

qos: Store function
Type: feature

store: write a QoS value into the buffer meta-data
record: Extract a QoS value from a packet header and store it.
mark: Make a change to the content of a packet header by writing a stored
      QoS value

Change-Id: I07d1e87dd1ca90d40ac1ae1774fee1b272cab83f
Signed-off-by: Neale Ranns <nranns@cisco.com>

src/vnet/qos/qos_store.c
@@ -1,0 +1,310 @@
+/*
+ * Copyright (c) 2018 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <vnet/qos/qos_store.h>
+#include <vnet/ip/ip.h>
+#include <vnet/feature/feature.h>
+
+/**
+ * QoS Store configuration
+ */
+typedef struct qos_store_t_
+{
+  u8 qst_n_cfgs;
+  qos_bits_t qst_value;
+} qos_store_t;
+
+/**
+ * Per-interface, per-protocol vector of feature on/off configurations
+ */
+qos_store_t *qos_store_configs[QOS_N_SOURCES];
+
+static void
+qos_store_feature_config (u32 sw_if_index,
+			  qos_source_t input_source,
+			  u8 enable, qos_bits_t value)
+{
+  switch (input_source)
+    {
+    case QOS_SOURCE_IP:
+      vnet_feature_enable_disable ("ip6-unicast", "ip6-qos-store",
+				   sw_if_index, enable, &value,
+				   sizeof (value));
+      vnet_feature_enable_disable ("ip6-multicast", "ip6-qos-store",
+				   sw_if_index, enable, &value,
+				   sizeof (value));
+      vnet_feature_enable_disable ("ip4-unicast", "ip4-qos-store",
+				   sw_if_index, enable, &value,
+				   sizeof (value));
+      vnet_feature_enable_disable ("ip4-multicast", "ip4-qos-store",
+				   sw_if_index, enable, &value,
+				   sizeof (value));
+      break;
+    case QOS_SOURCE_MPLS:
+    case QOS_SOURCE_VLAN:
+    case QOS_SOURCE_EXT:
+      /* not a valid option for storeing */
+      break;
+    }
+}
+
+int
+qos_store_enable (u32 sw_if_index,
+		  qos_source_t input_source, qos_bits_t value)
+{
+  qos_store_t *qst;
+
+  if (QOS_SOURCE_IP != input_source)
+    return VNET_API_ERROR_UNIMPLEMENTED;
+
+  vec_validate (qos_store_configs[input_source], sw_if_index);
+
+  qst = &qos_store_configs[input_source][sw_if_index];
+
+  if (0 == qst->qst_n_cfgs)
+    {
+      qst->qst_value = value;
+      qos_store_feature_config (sw_if_index, input_source, 1, value);
+    }
+
+  qst->qst_n_cfgs++;
+
+  return (0);
+}
+
+int
+qos_store_disable (u32 sw_if_index, qos_source_t input_source)
+{
+  qos_store_t *qst;
+
+  if (vec_len (qos_store_configs[input_source]) <= sw_if_index)
+    return VNET_API_ERROR_NO_MATCHING_INTERFACE;
+
+  qst = &qos_store_configs[input_source][sw_if_index];
+
+  if (0 == qst->qst_n_cfgs)
+    return VNET_API_ERROR_VALUE_EXIST;
+
+  qst->qst_n_cfgs--;
+
+  if (0 == qst->qst_n_cfgs)
+    {
+      qos_store_feature_config (sw_if_index, input_source, 0, qst->qst_value);
+    }
+
+  return (0);
+}
+
+void
+qos_store_walk (qos_store_walk_cb_t fn, void *c)
+{
+  qos_source_t qs;
+
+  FOR_EACH_QOS_SOURCE (qs)
+  {
+    qos_store_t *qst;
+    u32 sw_if_index;
+
+    vec_foreach_index (sw_if_index, qos_store_configs[qs])
+    {
+      qst = &qos_store_configs[qs][sw_if_index];
+      if (0 != qst->qst_n_cfgs)
+	fn (sw_if_index, qs, qst->qst_value, c);
+    }
+  }
+}
+
+/*
+ * Disable storeing feature for all protocols when the interface
+ * is deleted
+ */
+static clib_error_t *
+qos_store_ip_interface_add_del (vnet_main_t * vnm,
+				u32 sw_if_index, u32 is_add)
+{
+  if (!is_add)
+    {
+      qos_source_t qs;
+
+      FOR_EACH_QOS_SOURCE (qs)
+      {
+	while (qos_store_disable (sw_if_index, qs) == 0);
+      }
+    }
+
+  return (NULL);
+}
+
+VNET_SW_INTERFACE_ADD_DEL_FUNCTION (qos_store_ip_interface_add_del);
+
+clib_error_t *
+qos_store_init (vlib_main_t * vm)
+{
+  return 0;
+}
+
+VLIB_INIT_FUNCTION (qos_store_init);
+
+static clib_error_t *
+qos_store_cli (vlib_main_t * vm,
+	       unformat_input_t * input, vlib_cli_command_t * cmd)
+{
+  vnet_main_t *vnm = vnet_get_main ();
+  u32 sw_if_index, qs, value;
+  u8 enable;
+
+  qs = 0xff;
+  enable = 1;
+  sw_if_index = ~0;
+
+  while (unformat_check_input (input) != UNFORMAT_END_OF_INPUT)
+    {
+      if (unformat (input, "%U", unformat_vnet_sw_interface,
+		    vnm, &sw_if_index))
+	;
+      else if (unformat (input, "%U", unformat_qos_source, &qs))
+	;
+      else if (unformat (input, "enable"))
+	enable = 1;
+      else if (unformat (input, "disable"))
+	enable = 0;
+      else if (unformat (input, "value &d", &value))
+	;
+      else
+	break;
+    }
+
+  if (~0 == sw_if_index)
+    return clib_error_return (0, "interface must be specified");
+  if (0xff == qs)
+    return clib_error_return (0, "input location must be specified");
+
+  if (enable)
+    qos_store_enable (sw_if_index, qs, value);
+  else
+    qos_store_disable (sw_if_index, qs);
+
+  return (NULL);
+}
+
+/*?
+ * Enable QoS bit storeing on an interface using the packet's input DSCP bits
+ * Which input QoS bits to use are either; IP, MPLS or VLAN. If more than
+ * one protocol is chosen (which is foolish) the higher layers override the
+ * lower.
+ *
+ * @cliexpar
+ * @cliexcmd{qos store ip GigEthernet0/1/0}
+ ?*/
+/* *INDENT-OFF* */
+VLIB_CLI_COMMAND (qos_store_command, static) = {
+  .path = "qos store",
+  .short_help = "qos store <store-source> <INTERFACE> [disable]",
+  .function = qos_store_cli,
+  .is_mp_safe = 1,
+};
+/* *INDENT-ON* */
+
+static void
+qos_store_show_one_interface (vlib_main_t * vm, u32 sw_if_index)
+{
+  u8 n_cfgs[QOS_N_SOURCES] = { };
+  qos_source_t qs;
+  bool set;
+
+  set = false;
+
+  FOR_EACH_QOS_SOURCE (qs)
+  {
+    if (vec_len (qos_store_configs[qs]) <= sw_if_index)
+      continue;
+    if (0 != (n_cfgs[qs] = qos_store_configs[qs][sw_if_index].qst_n_cfgs))
+      set = true;
+  }
+
+  if (set)
+    {
+      vlib_cli_output (vm, " %U:", format_vnet_sw_if_index_name,
+		       vnet_get_main (), sw_if_index);
+
+      FOR_EACH_QOS_SOURCE (qs)
+      {
+	if (n_cfgs[qs] != 0)
+	  vlib_cli_output (vm, "  %U -> %d",
+			   format_qos_source, qs,
+			   qos_store_configs[qs][sw_if_index].qst_value);
+      }
+    }
+}
+
+static clib_error_t *
+qos_store_show (vlib_main_t * vm,
+		unformat_input_t * input, vlib_cli_command_t * cmd)
+{
+  vnet_main_t *vnm = vnet_get_main ();
+  qos_source_t qs;
+  u32 sw_if_index;
+
+  sw_if_index = ~0;
+
+  while (unformat_check_input (input) != UNFORMAT_END_OF_INPUT)
+    {
+      if (unformat (input, "%U", unformat_vnet_sw_interface,
+		    vnm, &sw_if_index))
+	;
+    }
+
+  if (~0 == sw_if_index)
+    {
+      u32 ii, n_ints = 0;
+
+      FOR_EACH_QOS_SOURCE (qs)
+      {
+	n_ints = clib_max (n_ints, vec_len (qos_store_configs[qs]));
+      }
+
+      for (ii = 0; ii < n_ints; ii++)
+	{
+	  qos_store_show_one_interface (vm, ii);
+	}
+    }
+  else
+    qos_store_show_one_interface (vm, sw_if_index);
+
+  return (NULL);
+}
+
+/*?
+ * Show Egress Qos Maps
+ *
+ * @cliexpar
+ * @cliexcmd{show qos egress map}
+ ?*/
+/* *INDENT-OFF* */
+VLIB_CLI_COMMAND (qos_store_show_command, static) = {
+  .path = "show qos store",
+  .short_help = "show qos store [interface]",
+  .function = qos_store_show,
+  .is_mp_safe = 1,
+};
+/* *INDENT-ON* */
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */

extras/vom/vom/CMakeLists.txt
@@ -173,6 +173,8 @@ list(APPEND VOM_SOURCES
   qos_mark_cmds.cpp
   qos_record.cpp
   qos_record_cmds.cpp
+  qos_store.cpp
+  qos_store_cmds.cpp
   qos_types.cpp
   qos_types_api.cpp
   ra_config.cpp
@@ -280,6 +282,7 @@ list(APPEND VOM_HEADERS
   qos_map.hpp
   qos_mark.hpp
   qos_record.hpp
+  qos_store.hpp
   qos_types.hpp
   ra_config.hpp
   ra_prefix.hpp

extras/vom/vom/qos_store.cpp
@@ -1,0 +1,186 @@
+/*
+ * Copyright (c) 2019 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "vom/qos_store.hpp"
+#include "vom/api_types.hpp"
+#include "vom/qos_store_cmds.hpp"
+#include "vom/qos_types_api.hpp"
+#include "vom/singular_db_funcs.hpp"
+
+namespace VOM {
+namespace QoS {
+
+singular_db<store::key_t, store> store::m_db;
+
+store::event_handler store::m_evh;
+
+store::store(const interface& itf, const source_t& src, bits_t value)
+  : m_config(false)
+  , m_itf(itf.singular())
+  , m_src(src)
+  , m_value(value)
+{
+}
+
+store::store(const store& s)
+  : m_config(s.m_config)
+  , m_itf(s.m_itf)
+  , m_src(s.m_src)
+  , m_value(s.m_value)
+{
+}
+
+store::~store()
+{
+  sweep();
+  m_db.release(key(), this);
+}
+
+const store::key_t
+store::key() const
+{
+  return (std::make_pair(m_itf->key(), m_src));
+}
+
+bool
+store::operator==(const store& r) const
+{
+  return (key() == r.key());
+}
+
+void
+store::sweep()
+{
+  if (m_config) {
+    HW::enqueue(new store_cmds::delete_cmd(m_config, m_itf->handle(), m_src));
+  }
+  HW::write();
+}
+
+void
+store::replay()
+{
+  if (m_config) {
+    HW::enqueue(
+      new store_cmds::create_cmd(m_config, m_itf->handle(), m_src, m_value));
+  }
+}
+
+std::string
+store::to_string() const
+{
+  std::ostringstream s;
+  s << "qos-store:[" << m_itf->to_string() << ", src:" << m_src.to_string()
+    << ", value:" << static_cast<int>(m_value);
+
+  return (s.str());
+}
+
+void
+store::update(const store& r)
+{
+  if (rc_t::OK != m_config.rc()) {
+    HW::enqueue(
+      new store_cmds::create_cmd(m_config, m_itf->handle(), m_src, m_value));
+  }
+}
+
+std::shared_ptr<store>
+store::find_or_add(const store& temp)
+{
+  return (m_db.find_or_add(temp.key(), temp));
+}
+
+std::shared_ptr<store>
+store::find(const key_t& k)
+{
+  return (m_db.find(k));
+}
+
+std::shared_ptr<store>
+store::singular() const
+{
+  return find_or_add(*this);
+}
+
+void
+store::dump(std::ostream& os)
+{
+  db_dump(m_db, os);
+}
+
+store::event_handler::event_handler()
+{
+  OM::register_listener(this);
+  inspect::register_handler({ "qos-store" }, "QoS Store", this);
+}
+
+void
+store::event_handler::handle_replay()
+{
+  m_db.replay();
+}
+
+void
+store::event_handler::handle_populate(const client_db::key_t& key)
+{
+  std::shared_ptr<store_cmds::dump_cmd> cmd =
+    std::make_shared<store_cmds::dump_cmd>();
+
+  HW::enqueue(cmd);
+  HW::write();
+
+  for (auto& rr : *cmd) {
+    auto& payload = rr.get_payload();
+
+    std::shared_ptr<interface> itf = interface::find(payload.store.sw_if_index);
+
+    VOM_LOG(log_level_t::DEBUG) << "data: " << payload.store.sw_if_index;
+
+    if (itf) {
+      store qr(*itf, from_api(payload.store.input_source), payload.store.value);
+      OM::commit(key, qr);
+
+      VOM_LOG(log_level_t::DEBUG) << "read: " << qr.to_string();
+    } else {
+      VOM_LOG(log_level_t::ERROR) << "no interface:"
+                                  << payload.store.sw_if_index;
+    }
+  }
+}
+
+dependency_t
+store::event_handler::order() const
+{
+  return (dependency_t::ENTRY);
+}
+
+void
+store::event_handler::show(std::ostream& os)
+{
+  db_dump(m_db, os);
+}
+
+}; // namespace QoS
+
+}; // namespace VOM
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "mozilla")
+ * End:
+ */

extras/vom/vom/qos_store.hpp
@@ -13,8 +13,8 @@
  * limitations under the License.
  */
 
-#ifndef __VOM_QOS_MAP_H__
-#define __VOM_QOS_MAP_H__
+#ifndef __VOM_QOS_STORE_H__
+#define __VOM_QOS_STORE_H__
 
 #include <ostream>
 
@@ -28,21 +28,15 @@ namespace VOM {
  */
 namespace QoS {
 
-/**
- * A QoS map determines how value from one source are translated to
- * values of another source
- */
-class map : public object_base
+class store : public object_base
 {
 public:
-  typedef std::array<std::array<bits_t, 256>, 4> outputs_t;
+  store(const interface& i, const source_t& source, bits_t value);
+  store(const store& r);
 
-  map(uint32_t id, const outputs_t& o);
-  map(const map& r);
+  ~store();
 
-  ~map();
-
-  typedef uint32_t key_t;
+  typedef std::pair<interface::key_t, source_t> key_t;
 
   /**
    * Return the object's key
@@ -50,24 +44,19 @@ public:
   const key_t key() const;
 
   /**
-   * Return the object's ID
-   */
-  const key_t id() const;
-
-  /**
    * comparison operator
    */
-  bool operator==(const map& bdae) const;
+  bool operator==(const store& bdae) const;
 
   /**
    * Return the matching 'singular instance'
    */
-  std::shared_ptr<map> singular() const;
+  std::shared_ptr<store> singular() const;
 
   /**
    * Find the instnace of the bridge_domain domain in the OM
    */
-  static std::shared_ptr<map> find(const key_t& k);
+  static std::shared_ptr<store> find(const key_t& k);
 
   /**
    * Dump all bridge_domain-doamin into the stream provided
@@ -123,12 +112,12 @@ private:
   /**
    * Commit the acculmulated changes into VPP. i.e. to a 'HW" write.
    */
-  void update(const map& obj);
+  void update(const store& obj);
 
   /**
    * Find or add the instnace of the bridge_domain domain in the OM
    */
-  static std::shared_ptr<map> find_or_add(const map& temp);
+  static std::shared_ptr<store> find_or_add(const store& temp);
 
   /*
    * It's the VPPHW class that updates the objects in HW
@@ -138,7 +127,7 @@ private:
   /**
    * It's the singular_db class that calls replay()
    */
-  friend class singular_db<key_t, map>;
+  friend class singular_db<key_t, store>;
 
   /**
    * Sweep/reap the object if still stale
@@ -152,22 +141,29 @@ private:
   HW::item<bool> m_config;
 
   /**
-   * unique ID of the MAP.
+   * The interface the endpoint is attached to.
    */
-  uint32_t m_id;
+  std::shared_ptr<interface> m_itf;
 
   /**
-   * outputs from the translation
+   * QoS source to store from
    */
-  outputs_t m_outputs;
+  source_t m_src;
+
+  /**
+   * QoS Value to store in the buffer
+   */
+  bits_t m_value;
 
   /**
    * A map of all bridge_domains
    */
-  static singular_db<key_t, map> m_db;
+  static singular_db<key_t, store> m_db;
 };
 
 }; // namesapce QoS
+
+std::ostream& operator<<(std::ostream& os, const QoS::store::key_t& key);
 
 }; // namespace VOM
 

extras/vom/vom/qos_store_cmds.cpp
@@ -1,0 +1,143 @@
+/*
+ * Copyright (c) 2019 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "vom/qos_store_cmds.hpp"
+#include "vom/qos_types_api.hpp"
+
+namespace VOM {
+namespace QoS {
+namespace store_cmds {
+
+create_cmd::create_cmd(HW::item<bool>& item,
+                       const handle_t& itf,
+                       const source_t& s,
+                       bits_t value)
+  : rpc_cmd(item)
+  , m_itf(itf)
+  , m_src(s)
+  , m_value(value)
+{
+}
+
+bool
+create_cmd::operator==(const create_cmd& other) const
+{
+  return ((m_itf == other.m_itf) && (m_src == other.m_src) &&
+          (m_value == other.m_value));
+}
+
+rc_t
+create_cmd::issue(connection& con)
+{
+  msg_t req(con.ctx(), std::ref(*this));
+
+  auto& payload = req.get_request().get_payload();
+  payload.store.sw_if_index = m_itf.value();
+  payload.store.input_source = to_api(m_src);
+  payload.store.value = m_value;
+
+  VAPI_CALL(req.execute());
+
+  return (wait());
+}
+
+std::string
+create_cmd::to_string() const
+{
+  std::ostringstream s;
+  s << "qos-store-create: " << m_hw_item.to_string() << " itf:" << m_itf
+    << " src:" << m_src.to_string() << " value:" << static_cast<int>(m_value);
+
+  return (s.str());
+}
+
+delete_cmd::delete_cmd(HW::item<bool>& item,
+                       const handle_t& itf,
+                       const source_t& s)
+  : rpc_cmd(item)
+  , m_itf(itf)
+  , m_src(s)
+{
+}
+
+bool
+delete_cmd::operator==(const delete_cmd& other) const
+{
+  return (m_hw_item == other.m_hw_item && m_itf == other.m_itf &&
+          m_src == other.m_src);
+}
+
+rc_t
+delete_cmd::issue(connection& con)
+{
+  msg_t req(con.ctx(), std::ref(*this));
+
+  auto& payload = req.get_request().get_payload();
+  payload.store.sw_if_index = m_itf.value();
+  payload.store.input_source = to_api(m_src);
+
+  VAPI_CALL(req.execute());
+
+  return (wait());
+}
+
+std::string
+delete_cmd::to_string() const
+{
+  std::ostringstream s;
+  s << "qos-store-delete: " << m_hw_item.to_string();
+
+  return (s.str());
+}
+
+dump_cmd::dump_cmd()
+{
+}
+
+bool
+dump_cmd::operator==(const dump_cmd& other) const
+{
+  return (true);
+}
+
+rc_t
+dump_cmd::issue(connection& con)
+{
+  m_dump.reset(new msg_t(con.ctx(), std::ref(*this)));
+
+  VAPI_CALL(m_dump->execute());
+
+  wait();
+
+  return rc_t::OK;
+}
+
+std::string
+dump_cmd::to_string() const
+{
+  return ("qos-store-dump");
+}
+
+}; // namespace store_cmds
+}; // namespace QoS
+}; // namespace VOM
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "mozilla")
+ * End:
+ */

extras/vom/vom/qos_store_cmds.hpp
@@ -1,0 +1,140 @@
+/*
+ * Copyright (c) 2019 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __VOM_QOS_STORE_CMDS_H__
+#define __VOM_QOS_STORE_CMDS_H__
+
+#include "vom/dump_cmd.hpp"
+#include "vom/qos_store.hpp"
+
+#include <vapi/qos.api.vapi.hpp>
+
+namespace VOM {
+namespace QoS {
+namespace store_cmds {
+
+/**
+ * A command class that creates or updates the GBP endpoint
+ */
+class create_cmd
+  : public rpc_cmd<HW::item<bool>, vapi::Qos_store_enable_disable>
+{
+public:
+  /**
+   * Constructor
+   */
+  create_cmd(HW::item<bool>& item,
+             const handle_t& itf,
+             const source_t& src,
+             bits_t value);
+
+  /**
+   * Issue the command to VPP/HW
+   */
+  rc_t issue(connection& con);
+
+  /**
+   * convert to string format for debug purposes
+   */
+  std::string to_string() const;
+
+  /**
+   * Comparison operator - only used for UT
+   */
+  bool operator==(const create_cmd& i) const;
+
+private:
+  handle_t m_itf;
+  const source_t& m_src;
+  bits_t m_value;
+};
+
+/**
+ * A cmd class that deletes a GBP endpoint
+ */
+class delete_cmd
+  : public rpc_cmd<HW::item<bool>, vapi::Qos_store_enable_disable>
+{
+public:
+  /**
+   * Constructor
+   */
+  delete_cmd(HW::item<bool>& item, const handle_t& itf, const source_t& src);
+
+  /**
+   * Issue the command to VPP/HW
+   */
+  rc_t issue(connection& con);
+
+  /**
+   * convert to string format for debug purposes
+   */
+  std::string to_string() const;
+
+  /**
+   * Comparison operator - only used for UT
+   */
+  bool operator==(const delete_cmd& i) const;
+
+private:
+  const handle_t m_itf;
+  const source_t& m_src;
+};
+
+/**
+ * A cmd class that Dumps all the GBP endpoints
+ */
+class dump_cmd : public VOM::dump_cmd<vapi::Qos_store_dump>
+{
+public:
+  /**
+   * Constructor
+   */
+  dump_cmd();
+  dump_cmd(const dump_cmd& d);
+
+  /**
+   * Issue the command to VPP/HW
+   */
+  rc_t issue(connection& con);
+  /**
+   * convert to string format for debug purposes
+   */
+  std::string to_string() const;
+
+  /**
+   * Comparison operator - only used for UT
+   */
+  bool operator==(const dump_cmd& i) const;
+
+private:
+  /**
+   * HW reutrn code
+   */
+  HW::item<bool> item;
+};
+}; // namespace store_cmds
+}; // namespace Qos
+}; // namespace VOM
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "mozilla")
+ * End:
+ */
+
+#endif

extras/vom/vom/qos_types.hpp
@@ -24,6 +24,8 @@ namespace VOM {
  */
 namespace QoS {
 
+typedef uint8_t bits_t;
+
 /**
  * The Source of the QoS classification (i.e. which header the bits are
  * associated with).

src/vnet/CMakeLists.txt
@@ -1451,6 +1451,8 @@ list(APPEND VNET_SOURCES
   qos/qos_egress_map.c
   qos/qos_record.c
   qos/qos_record_node.c
+  qos/qos_store.c
+  qos/qos_store_node.c
   qos/qos_mark.c
   qos/qos_mark_node.c
 )

commit 4752f1c81fb536792c92291184df2b8c7d8bddcd

qos: Uninitialised variable (coverity)
Type: fix
Fixes: 47f59edc13a7b1fd68ab7ab7dd886043e9d82c00

Change-Id: I63130c442f71d1aef0d389ab00bac8092224bec2
Signed-off-by: Neale Ranns <nranns@cisco.com>

src/vnet/qos/qos_store.c
@@ -168,6 +168,7 @@ qos_store_cli (vlib_main_t * vm,
   qs = 0xff;
   enable = 1;
   sw_if_index = ~0;
+  value = ~0;
 
   while (unformat_check_input (input) != UNFORMAT_END_OF_INPUT)
     {
@@ -188,6 +189,8 @@ qos_store_cli (vlib_main_t * vm,
 
   if (~0 == sw_if_index)
     return clib_error_return (0, "interface must be specified");
+  if (~0 == value)
+    return clib_error_return (0, "value to be stored must be specified");
   if (0xff == qs)
     return clib_error_return (0, "input location must be specified");
 