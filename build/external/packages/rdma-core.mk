alisherqodirjonov/vpp

commit 0d10af5bc6908c969fdcd5f43d6b4bc8169dcf87

rdma: bump rdma-core to 25.0
Type: make

Change-Id: Ie7596e2976917657b089162c44b3b669e3a2d048
Signed-off-by: Benoît Ganne <bganne@cisco.com>

build/external/packages/rdma-core.mk
@@ -13,10 +13,11 @@
 
 RDMA_CORE_DEBUG?=n
 
-rdma-core_version             := 23
+rdma-core_version             := 25.0
 rdma-core_tarball             := rdma-core-$(rdma-core_version).tar.gz
 rdma-core_tarball_md5sum_22.1 := dde4d30e3db20893408ae51041117034
-rdma-core_tarball_md5sum_23 := c78575735c4a71609c1a214ea16cd8dc
+rdma-core_tarball_md5sum_23   := c78575735c4a71609c1a214ea16cd8dc
+rdma-core_tarball_md5sum_25.0 := d8839edaae4cb6dacdacc4b5a824854c
 rdma-core_tarball_md5sum      := $(rdma-core_tarball_md5sum_$(rdma-core_version))
 rdma-core_tarball_strip_dirs  := 1
 rdma-core_url                 := http://github.com/linux-rdma/rdma-core/releases/download/v$(rdma-core_version)/$(rdma-core_tarball)

commit 902511f706b86503c91d13cc04941e755c597dd2

rdma: add missing symbols
rdma-core-25.0 move some symbol to another library, make sure we link
to it.

Type: fix
Fixes: 0d10af5bc6908c969fdcd5f43d6b4bc8169dcf87

Change-Id: I6b8a56ea5fc7787d3de0982015ed47b767557c06
Signed-off-by: Benoît Ganne <bganne@cisco.com>

build/external/packages/rdma-core.mk
@@ -32,6 +32,7 @@ RDMA_FILES := include/infiniband/verbs.h \
 	      include/infiniband/ib_user_ioctl_verbs.h \
 	      include/rdma/ib_user_verbs.h \
 	      lib/statics/libibverbs.a \
+	      util/librdma_util.a \
 	      lib/statics/libmlx5.a
 
 define  rdma-core_config_cmds
@@ -43,12 +44,14 @@ define  rdma-core_config_cmds
 endef
 
 define  rdma-core_build_cmds
-	$(CMAKE) --build $(rdma-core_build_dir) -- libibverbs.a libmlx5.a > $(rdma-core_build_log)
+	$(CMAKE) --build $(rdma-core_build_dir) -- libibverbs.a librdma_util.a libmlx5.a > $(rdma-core_build_log)
 endef
 
 define  rdma-core_install_cmds
 	mkdir -p $(rdma-core_install_dir)
 	tar -C $(rdma-core_build_dir) --xform='s|/statics/|/|' -hc $(RDMA_FILES) | tar -C $(rdma-core_install_dir) -xv > $(rdma-core_install_log)
+	mv -v $(rdma-core_install_dir)/util/librdma_util.a $(rdma-core_install_dir)/lib >> $(rdma-core_install_log)
+	rmdir $(rdma-core_install_dir)/util
 endef
 
 $(eval $(call package,rdma-core))

src/plugins/rdma/CMakeLists.txt
@@ -19,9 +19,10 @@ if (NOT IBVERBS_INCLUDE_DIR)
 endif()
 
 vpp_plugin_find_library(rdma IBVERBS_LIB libibverbs.a)
+vpp_plugin_find_library(rdma RDMA_UTIL_LIB librdma_util.a)
 vpp_plugin_find_library(rdma MLX5_LIB libmlx5.a)
 
-if (NOT IBVERBS_LIB OR NOT MLX5_LIB)
+if (NOT IBVERBS_LIB OR NOT RDMA_UTIL_LIB OR NOT MLX5_LIB)
   message(WARNING "rdma plugin - ibverbs not found - rdma plugin disabled")
   return()
 endif()
@@ -30,7 +31,7 @@ if (MLX5_LIB)
   string_append(RDMA_LINK_FLAGS "-Wl,--whole-archive,${MLX5_LIB},--no-whole-archive")
 endif()
 
-set(CMAKE_REQUIRED_FLAGS "-fPIC -shared ${IBVERBS_LIB} ${RDMA_LINK_FLAGS}")
+set(CMAKE_REQUIRED_FLAGS "-fPIC -shared ${IBVERBS_LIB} ${RDMA_UTIL_LIB} ${RDMA_LINK_FLAGS}")
 CHECK_C_SOURCE_COMPILES("" IBVERBS_COMPILES_CHECK)
 
 if (NOT IBVERBS_COMPILES_CHECK)
@@ -58,4 +59,5 @@ add_vpp_plugin(rdma
 
   LINK_LIBRARIES
   ${IBVERBS_LIB}
+  ${RDMA_UTIL_LIB}
 )