alisherqodirjonov/vpp

commit e32c2de2fe1bcd9e1083fe33c82874f73a596be6

ip: fix the punt redirect for ip4
Type: fix

Change-Id: I39341f201209931392f315ead5adfddd8b567caf
Signed-off-by: Mohsin Kazmi <sykazmi@cisco.com>

src/vnet/ip/ip4_punt_drop.c
@@ -334,10 +334,10 @@ ip4_punt_redirect_cmd (vlib_main_t * vm,
 		       vlib_cli_command_t * cmd)
 {
   unformat_input_t _line_input, *line_input = &_line_input;
-  fib_route_path_t *rpaths = NULL, rpath;
-  dpo_proto_t payload_proto;
+  ip46_address_t nh = { 0 };
   clib_error_t *error = 0;
   u32 rx_sw_if_index = ~0;
+  u32 tx_sw_if_index = ~0;
   vnet_main_t *vnm;
   u8 is_add;
 
@@ -358,9 +358,13 @@ ip4_punt_redirect_cmd (vlib_main_t * vm,
       else if (unformat (line_input, "rx %U",
 			 unformat_vnet_sw_interface, vnm, &rx_sw_if_index))
 	;
+      else if (unformat (line_input, "via %U %U",
+			 unformat_ip4_address, unformat_vnet_sw_interface,
+			 &nh.ip4, vnm, &tx_sw_if_index))
+	;
       else if (unformat (line_input, "via %U",
-			 unformat_fib_route_path, &rpath, &payload_proto))
-	vec_add1 (rpaths, rpath);
+			 unformat_vnet_sw_interface, vnm, &tx_sw_if_index))
+	;
       else
 	{
 	  error = unformat_parse_error (line_input);
@@ -376,8 +380,7 @@ ip4_punt_redirect_cmd (vlib_main_t * vm,
 
   if (is_add)
     {
-      if (vec_len (rpaths))
-	ip4_punt_redirect_add_paths (rx_sw_if_index, rpaths);
+      ip4_punt_redirect_add (rx_sw_if_index, tx_sw_if_index, &nh);
     }
   else
     {

commit 1d3a4f97def620c8b1b5bcb478b888454f84d95d

ip: fix the order in ip4 punt redirect
Type: fix
Fixes: e32c2de2fe1bcd9e1083fe33c82874f73a596be6

Change-Id: Id448d6ae9cfdd3122e8187121c509412835117c5
Signed-off-by: Mohsin Kazmi <sykazmi@cisco.com>

src/vnet/ip/ip4_punt_drop.c
@@ -359,8 +359,8 @@ ip4_punt_redirect_cmd (vlib_main_t * vm,
 			 unformat_vnet_sw_interface, vnm, &rx_sw_if_index))
 	;
       else if (unformat (line_input, "via %U %U",
-			 unformat_ip4_address, unformat_vnet_sw_interface,
-			 &nh.ip4, vnm, &tx_sw_if_index))
+			 unformat_ip4_address, &nh.ip4,
+			 unformat_vnet_sw_interface, vnm, &tx_sw_if_index))
 	;
       else if (unformat (line_input, "via %U",
 			 unformat_vnet_sw_interface, vnm, &tx_sw_if_index))