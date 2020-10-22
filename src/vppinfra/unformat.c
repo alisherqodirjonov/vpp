alisherqodirjonov/vpp

commit 6fa87ac0fffb5519835d88b8df45a3cf902e8f82

vppinfra: explicitly export symbols
Type: improvement
Change-Id: I57a9f85f7df1fc48656b72592349f4c544302f77
Signed-off-by: Damjan Marion <damarion@cisco.com>

src/vppinfra/unformat.c
@@ -38,7 +38,7 @@
 #include <vppinfra/format.h>
 
 /* Call user's function to fill input buffer. */
-uword
+__clib_export uword
 _unformat_fill_input (unformat_input_t * i)
 {
   uword l, first_mark;
@@ -87,7 +87,7 @@ is_white_space (uword c)
 }
 
 /* Format function for dumping input stream. */
-u8 *
+__clib_export u8 *
 format_unformat_error (u8 * s, va_list * va)
 {
   unformat_input_t *i = va_arg (*va, unformat_input_t *);
@@ -139,7 +139,7 @@ format_unformat_error (u8 * s, va_list * va)
 }
 
 /* Print everything: not just error context. */
-u8 *
+__clib_export u8 *
 format_unformat_input (u8 * s, va_list * va)
 {
   unformat_input_t *i = va_arg (*va, unformat_input_t *);
@@ -268,7 +268,7 @@ done:
   return 1;
 }
 
-uword
+__clib_export uword
 unformat_hex_string (unformat_input_t * input, va_list * va)
 {
   u8 **hexstring_return = va_arg (*va, u8 **);
@@ -317,14 +317,14 @@ unformat_hex_string (unformat_input_t * input, va_list * va)
 }
 
 /* unformat (input "foo%U", unformat_eof) matches terminal foo only */
-uword
+__clib_export uword
 unformat_eof (unformat_input_t * input, va_list * va)
 {
   return unformat_check_input (input) == UNFORMAT_END_OF_INPUT;
 }
 
 /* Parse a token containing given set of characters. */
-uword
+__clib_export uword
 unformat_token (unformat_input_t * input, va_list * va)
 {
   u8 *token_chars = va_arg (*va, u8 *);
@@ -379,7 +379,7 @@ unformat_token (unformat_input_t * input, va_list * va)
 
 /* Unformat (parse) function which reads a %s string and converts it
    to and unformat_input_t. */
-uword
+__clib_export uword
 unformat_input (unformat_input_t * i, va_list * args)
 {
   unformat_input_t *sub_input = va_arg (*args, unformat_input_t *);
@@ -395,7 +395,7 @@ unformat_input (unformat_input_t * i, va_list * args)
 }
 
 /* Parse a line ending with \n and return it. */
-uword
+__clib_export uword
 unformat_line (unformat_input_t * i, va_list * va)
 {
   u8 *line = 0, **result = va_arg (*va, u8 **);
@@ -411,7 +411,7 @@ unformat_line (unformat_input_t * i, va_list * va)
 }
 
 /* Parse a line ending with \n and return it as an unformat_input_t. */
-uword
+__clib_export uword
 unformat_line_input (unformat_input_t * i, va_list * va)
 {
   unformat_input_t *result = va_arg (*va, unformat_input_t *);
@@ -817,7 +817,7 @@ do_percent (unformat_input_t * input, va_list * va, const char *f)
   return n ? f : 0;
 }
 
-uword
+__clib_export uword
 unformat_skip_white_space (unformat_input_t * input)
 {
   uword n = 0;
@@ -974,7 +974,7 @@ parse_fail:
   return input_matches_format;
 }
 
-uword
+__clib_export uword
 unformat (unformat_input_t * input, const char *fmt, ...)
 {
   va_list va;
@@ -985,7 +985,7 @@ unformat (unformat_input_t * input, const char *fmt, ...)
   return result;
 }
 
-uword
+__clib_export uword
 unformat_user (unformat_input_t * input, unformat_function_t * func, ...)
 {
   va_list va;
@@ -1009,7 +1009,7 @@ unformat_user (unformat_input_t * input, unformat_function_t * func, ...)
 }
 
 /* Setup for unformat of Unix style command line. */
-void
+__clib_export void
 unformat_init_command_line (unformat_input_t * input, char *argv[])
 {
   uword i;
@@ -1025,7 +1025,7 @@ unformat_init_command_line (unformat_input_t * input, char *argv[])
     }
 }
 
-void
+__clib_export void
 unformat_init_string (unformat_input_t * input, char *string, int string_len)
 {
   unformat_init (input, 0, 0);
@@ -1033,7 +1033,7 @@ unformat_init_string (unformat_input_t * input, char *string, int string_len)
     vec_add (input->buffer, string, string_len);
 }
 
-void
+__clib_export void
 unformat_init_vector (unformat_input_t * input, u8 * vector_string)
 {
   unformat_init (input, 0, 0);
@@ -1060,7 +1060,7 @@ clib_file_fill_buffer (unformat_input_t * input)
     return input->index;
 }
 
-void
+__clib_export void
 unformat_init_clib_file (unformat_input_t * input, int file_descriptor)
 {
   unformat_init (input, clib_file_fill_buffer,
@@ -1077,7 +1077,7 @@ unformat_init_unix_env (unformat_input_t * input, char *var)
   return val != 0;
 }
 
-uword
+__clib_export uword
 unformat_data_size (unformat_input_t * input, va_list * args)
 {
   u64 _a;

src/vppinfra/CMakeLists.txt
@@ -36,6 +36,8 @@ install(
   COMPONENT vpp-dev
 )
 
+add_definitions(-fvisibility=hidden)
+
 ##############################################################################
 # vppinfra sources
 ##############################################################################

src/vppinfra/backtrace.c
@@ -45,7 +45,7 @@
 
 #include <vppinfra/asm_mips.h>
 
-uword
+__clib_export uword
 clib_backtrace (uword * callers, uword max_callers, uword n_frames_to_skip)
 {
   u32 *pc;
@@ -222,7 +222,7 @@ backtrace_done:
 /* use glibc backtrace for stack trace */
 #include <execinfo.h>
 
-uword
+__clib_export uword
 clib_backtrace (uword * callers, uword max_callers, uword n_frames_to_skip)
 {
   int size;

src/vppinfra/bihash_all_vector.c
@@ -16,10 +16,10 @@
 #include <vppinfra/mem.h>
 
 /* Vector of all bihashes */
-void **clib_all_bihashes;
+__clib_export void **clib_all_bihashes;
 static clib_mem_heap_t *clib_all_bihash_heap;
 
-clib_mem_heap_t *
+__clib_export clib_mem_heap_t *
 clib_all_bihash_set_heap (void)
 {
   if (PREDICT_FALSE (clib_all_bihash_heap == 0))
@@ -32,7 +32,7 @@ clib_all_bihash_set_heap (void)
  * Leave it to Beaver to change the size of a bihash
  * by making a clone in a stack local and then copying it...
  */
-void
+__clib_export void
 clib_bihash_copied (void *dst, void *src)
 {
   int i;

src/vppinfra/clib.h
@@ -100,6 +100,7 @@
 #define __clib_aligned(x) __attribute__ ((aligned(x)))
 #define __clib_section(s) __attribute__ ((section(s)))
 #define __clib_warn_unused_result __attribute__ ((warn_unused_result))
+#define __clib_export __attribute__ ((visibility("default")))
 
 #define never_inline __attribute__ ((__noinline__))
 

src/vppinfra/cpu.c
@@ -82,7 +82,7 @@
  _(0x43, 0x0b1, "Marvell", "OCTEON TX2 CN98XX", 1) \
  _(0x43, 0x0b2, "Marvell", "OCTEON TX2 CN96XX", 1)
 
-u8 *
+__clib_export u8 *
 format_cpu_uarch (u8 * s, va_list * args)
 {
 #if __x86_64__
@@ -146,7 +146,7 @@ format(s, "[0x%x] %s ([0x%02x] %s) stepping 0x%x", f, a, m, c, stepping);
 #endif
 }
 
-u8 *
+__clib_export u8 *
 format_cpu_model_name (u8 * s, va_list * args)
 {
 #if __x86_64__
@@ -202,7 +202,7 @@ flag_skip_prefix (char const *flag, const char *pfx, int len)
   return flag;
 }
 
-u8 *
+__clib_export u8 *
 format_cpu_flags (u8 * s, va_list * args)
 {
 #if defined(__x86_64__)

src/vppinfra/dlmalloc.c
@@ -4118,7 +4118,7 @@ void mspace_get_address_and_size (mspace msp, char **addrp, size_t *sizep)
   *sizep = this_seg->size;
 }
 
-CLIB_NOSANITIZE_ADDR
+CLIB_NOSANITIZE_ADDR __clib_export
 int mspace_is_heap_object (mspace msp, void *p)
 {
   msegment *this_seg;
@@ -4185,7 +4185,7 @@ int mspace_is_traced (mspace msp)
   return 0;
 }
 
-CLIB_NOSANITIZE_ADDR
+CLIB_NOSANITIZE_ADDR __clib_export
 void* mspace_get_aligned (mspace msp,
                           unsigned long n_user_data_bytes,
                           unsigned long align,
@@ -4286,7 +4286,7 @@ void* mspace_get_aligned (mspace msp,
   return (void *) searchp;
 }
 
-CLIB_NOSANITIZE_ADDR
+CLIB_NOSANITIZE_ADDR __clib_export
 void mspace_put (mspace msp, void *p_arg)
 {
   char *object_header;
@@ -4336,7 +4336,7 @@ void mspace_put_no_offset (mspace msp, void *p_arg)
   mspace_free (msp, p_arg);
 }
 
-CLIB_NOSANITIZE_ADDR
+CLIB_NOSANITIZE_ADDR __clib_export
 size_t mspace_usable_size_with_delta (const void *p)
 {
   size_t usable_size;

src/vppinfra/elf.c
@@ -43,7 +43,7 @@ elf_swap_verneed_aux (elf_dynamic_version_need_aux_t * n)
 #undef _
 }
 
-clib_error_t *
+__clib_export clib_error_t *
 elf_get_section_by_name (elf_main_t * em, char *section_name,
 			 elf_section_t ** result)
 {
@@ -1325,7 +1325,7 @@ elf_parse_dynamic (elf_main_t * em)
 #include <sys/stat.h>
 #include <fcntl.h>
 
-clib_error_t *
+__clib_export clib_error_t *
 elf_read_file (elf_main_t * em, char *file_name)
 {
   int fd;

commit e9791add9e3f518e19696facab71933d1725961a

vppinfra: missing __clib_export va_unformat
Fix an issue observed on aarch64 systems when running with dpdk plugin:
dpdk_plugin.so: undefined symbol: va_unformat

Type: fix
Fixes: 6fa87ac0fffb5519835d88b8df45a3cf902e8f82

Signed-off-by: Juraj Linkeš <juraj.linkes@pantheon.tech>
Change-Id: I22b7ee842dc606f96effdeb7aa4e161496986aed

src/vppinfra/unformat.c
@@ -835,7 +835,7 @@ unformat_skip_white_space (unformat_input_t * input)
   return n;
 }
 
-uword
+__clib_export uword
 va_unformat (unformat_input_t * input, const char *fmt, va_list * va)
 {
   const char *f;