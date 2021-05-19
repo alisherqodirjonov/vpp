alisherqodirjonov/vpp

commit 24b55760da0678cf0ad06e70d359e001a9890927

Intel Adaptive Virtual Function native device driver plugin
Change-Id: If168a9c54baaa516ecbe78de2141f11c17aa2f53
Signed-off-by: Damjan Marion <damarion@cisco.com>

src/plugins/avf/avf.h
@@ -1,0 +1,265 @@
+/*
+ *------------------------------------------------------------------
+ * Copyright (c) 2018 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *------------------------------------------------------------------
+ */
+
+#include <avf/virtchnl.h>
+
+#define foreach_avf_device_flags \
+  _(0, INITIALIZED, "initialized") \
+  _(1, ERROR, "error") \
+  _(2, ADMIN_UP, "admin-up") \
+  _(3, IOVA, "iova") \
+  _(4, LINK_UP, "link-up") \
+  _(5, SHARED_TXQ_LOCK, "shared-txq-lock") \
+  _(6, ELOG, "elog")
+
+enum
+{
+#define _(a, b, c) AVF_DEVICE_F_##b = (1 << a),
+  foreach_avf_device_flags
+#undef _
+};
+
+typedef struct
+{
+  u64 qword[4];
+} avf_rx_desc_t;
+
+STATIC_ASSERT_SIZEOF (avf_rx_desc_t, 32);
+
+typedef struct
+{
+  union
+  {
+    u64 qword[2];
+    u64x2 as_u64x2;
+  };
+} avf_tx_desc_t;
+
+STATIC_ASSERT_SIZEOF (avf_tx_desc_t, 16);
+
+typedef struct
+{
+  CLIB_CACHE_LINE_ALIGN_MARK (cacheline0);
+  volatile u32 *qrx_tail;
+  u16 next;
+  u16 size;
+  avf_rx_desc_t *descs;
+  u32 *bufs;
+  u16 n_bufs;
+} avf_rxq_t;
+
+typedef struct
+{
+  CLIB_CACHE_LINE_ALIGN_MARK (cacheline0);
+  volatile u32 *qtx_tail;
+  u16 next;
+  u16 size;
+  clib_spinlock_t lock;
+  avf_tx_desc_t *descs;
+  u32 *bufs;
+  u16 n_bufs;
+} avf_txq_t;
+
+typedef struct
+{
+  CLIB_CACHE_LINE_ALIGN_MARK (cacheline0);
+  u32 flags;
+  u32 per_interface_next_index;
+
+  u32 dev_instance;
+  u32 sw_if_index;
+  u32 hw_if_index;
+  vlib_pci_dev_handle_t pci_dev_handle;
+  void *bar0;
+
+  /* queues */
+  avf_rxq_t *rxqs;
+  avf_txq_t *txqs;
+
+  /* Admin queues */
+  avf_aq_desc_t *atq;
+  avf_aq_desc_t *arq;
+  void *atq_bufs;
+  void *arq_bufs;
+  u64 atq_bufs_pa;
+  u64 arq_bufs_pa;
+  u16 atq_next_slot;
+  u16 arq_next_slot;
+  virtchnl_pf_event_t *events;
+
+  u16 vsi_id;
+  u32 feature_bitmap;
+  u8 hwaddr[6];
+  u16 num_queue_pairs;
+  u16 max_vectors;
+  u16 max_mtu;
+  u32 rss_key_size;
+  u32 rss_lut_size;
+  virtchnl_link_speed_t link_speed;
+
+  /* stats */
+  virtchnl_eth_stats_t eth_stats;
+
+  /* error */
+  clib_error_t *error;
+} avf_device_t;
+
+typedef struct
+{
+  u32 bi;
+  u32 status;
+  u16 length;
+  u16 tail_length;
+  u8 ptype;
+  u8 error;
+} avf_rx_vector_entry_t;
+
+STATIC_ASSERT_SIZEOF (avf_rx_vector_entry_t, 16);
+
+#define AVF_RX_VECTOR_SZ VLIB_FRAME_SIZE
+
+enum
+{
+  AVF_PROCESS_EVENT_START = 1,
+  AVF_PROCESS_EVENT_STOP = 2,
+  AVF_PROCESS_EVENT_AQ_INT = 3,
+} avf_process_event_t;
+
+typedef struct
+{
+  CLIB_CACHE_LINE_ALIGN_MARK (cacheline0);
+  avf_rx_vector_entry_t rx_vector[AVF_RX_VECTOR_SZ];
+  u32 *to_free;
+} avf_per_thread_data_t;
+
+typedef struct
+{
+  u8 next_node;
+  i8 buffer_advance;
+  u32 flags;
+} avf_ptype_t;
+
+STATIC_ASSERT (VNET_DEVICE_INPUT_N_NEXT_NODES < 256, "too many next nodes");
+
+typedef struct
+{
+  avf_device_t *devices;
+  avf_per_thread_data_t *per_thread_data;
+  vlib_physmem_region_index_t physmem_region;
+  int physmem_region_alloc;
+
+  /* 256 element array for ptype based lookup */
+  avf_ptype_t *ptypes;
+} avf_main_t;
+
+extern avf_main_t avf_main;
+
+typedef struct
+{
+  vlib_pci_addr_t addr;
+  int enable_elog;
+  /* return */
+  int rv;
+  clib_error_t *error;
+} avf_create_if_args_t;
+
+void avf_create_if (vlib_main_t * vm, avf_create_if_args_t * args);
+void avf_delete_if (vlib_main_t * vm, avf_device_t * ad);
+
+extern vlib_node_registration_t avf_input_node;
+extern vnet_device_class_t avf_device_class;
+uword avf_interface_tx (vlib_main_t * vm, vlib_node_runtime_t * node,
+			vlib_frame_t * frame);
+
+/* format.c */
+format_function_t format_avf_device;
+format_function_t format_avf_device_name;
+format_function_t format_avf_input_trace;
+
+static inline u32
+avf_get_u32 (void *start, int offset)
+{
+  return *(u32 *) (((u8 *) start) + offset);
+}
+
+static inline u64
+avf_get_u64 (void *start, int offset)
+{
+  return *(u64 *) (((u8 *) start) + offset);
+}
+
+static inline u32
+avf_get_u32_bits (void *start, int offset, int first, int last)
+{
+  u32 value = avf_get_u32 (start, offset);
+  if ((last == 0) && (first == 31))
+    return value;
+  value >>= last;
+  value &= (1 << (first - last + 1)) - 1;
+  return value;
+}
+
+static inline u64
+avf_get_u64_bits (void *start, int offset, int first, int last)
+{
+  u64 value = avf_get_u64 (start, offset);
+  if ((last == 0) && (first == 63))
+    return value;
+  value >>= last;
+  value &= (1 << (first - last + 1)) - 1;
+  return value;
+}
+
+static inline void
+avf_set_u32 (void *start, int offset, u32 value)
+{
+  (*(u32 *) (((u8 *) start) + offset)) = value;
+}
+
+static inline void
+avf_reg_write (avf_device_t * ad, u32 addr, u32 val)
+{
+  *(volatile u32 *) ((u8 *) ad->bar0 + addr) = val;
+}
+
+static inline u32
+avf_reg_read (avf_device_t * ad, u32 addr)
+{
+  return *(volatile u32 *) (ad->bar0 + addr);
+}
+
+static inline void
+avf_reg_flush (avf_device_t * ad)
+{
+  avf_reg_read (ad, AVFGEN_RSTAT);
+  asm volatile ("":::"memory");
+}
+
+typedef struct
+{
+  u32 next_index;
+  u32 hw_if_index;
+  avf_rx_vector_entry_t rxve;
+} avf_input_trace_t;
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */

src/configure.ac
@@ -211,6 +211,7 @@ AC_SUBST(AR_FLAGS)
 
 # Please keep alphabetical order
 PLUGIN_ENABLED(acl)
+PLUGIN_ENABLED(avf)
 PLUGIN_ENABLED(cdp)
 PLUGIN_ENABLED(dpdk)
 PLUGIN_ENABLED(flowprobe)

src/plugins/Makefile.am
@@ -34,6 +34,10 @@ if ENABLE_ACL_PLUGIN
 include acl.am
 endif
 
+if ENABLE_AVF_PLUGIN
+include avf.am
+endif
+
 if ENABLE_CDP_PLUGIN
 include cdp.am
 endif

src/plugins/avf.am
@@ -1,0 +1,57 @@
+# Copyright (c) 2018 Cisco Systems, Inc.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+vppplugins_LTLIBRARIES += avf_plugin.la
+
+avf_plugin_la_LIBADD =
+avf_plugin_la_SOURCES = \
+	avf/cli.c			\
+	avf/device.c			\
+	avf/format.c			\
+	avf/input.c			\
+	avf/output.c			\
+	avf/plugin.c
+
+noinst_HEADERS += avf/avf.h
+
+if CPU_X86_64
+avf_multiversioning_files = 					\
+	avf/input.c						\
+	avf/output.c
+
+if CC_SUPPORTS_AVX2
+###############################################################
+# AVX2
+###############################################################
+libavf_plugin_avx2_la_SOURCES = $(avf_multiversioning_files)
+libavf_plugin_avx2_la_CFLAGS =				\
+	$(AM_CFLAGS)  @CPU_AVX2_FLAGS@				\
+	-DCLIB_MULTIARCH_VARIANT=avx2
+noinst_LTLIBRARIES += libavf_plugin_avx2.la
+avf_plugin_la_LIBADD += libavf_plugin_avx2.la
+endif
+
+if CC_SUPPORTS_AVX512
+###############################################################
+# AVX512
+###############################################################
+libavf_plugin_avx512_la_SOURCES = $(avf_multiversioning_files)
+libavf_plugin_avx512_la_CFLAGS =				\
+	$(AM_CFLAGS) @CPU_AVX512_FLAGS@				\
+	-DCLIB_MULTIARCH_VARIANT=avx512
+noinst_LTLIBRARIES += libavf_plugin_avx512.la
+avf_plugin_la_LIBADD += libavf_plugin_avx512.la
+endif
+endif
+
+# vi:syntax=automake

src/plugins/avf/cli.c
@@ -1,0 +1,200 @@
+/*
+ *------------------------------------------------------------------
+ * Copyright (c) 2018 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *------------------------------------------------------------------
+ */
+#include <stdint.h>
+#include <net/if.h>
+#include <sys/ioctl.h>
+#include <inttypes.h>
+
+#include <vlib/vlib.h>
+#include <vlib/unix/unix.h>
+#include <vlib/pci/pci.h>
+#include <vnet/ethernet/ethernet.h>
+
+#include <avf/avf.h>
+
+static clib_error_t *
+avf_create_command_fn (vlib_main_t * vm, unformat_input_t * input,
+		       vlib_cli_command_t * cmd)
+{
+  unformat_input_t _line_input, *line_input = &_line_input;
+  avf_create_if_args_t args;
+
+  memset (&args, 0, sizeof (avf_create_if_args_t));
+
+  /* Get a line of input. */
+  if (!unformat_user (input, unformat_line_input, line_input))
+    return 0;
+
+  while (unformat_check_input (line_input) != UNFORMAT_END_OF_INPUT)
+    {
+      if (unformat (line_input, "%U", unformat_vlib_pci_addr, &args.addr))
+	;
+      else if (unformat (line_input, "elog"))
+	args.enable_elog = 1;
+      else
+	return clib_error_return (0, "unknown input `%U'",
+				  format_unformat_error, input);
+    }
+  unformat_free (line_input);
+
+
+  avf_create_if (vm, &args);
+
+  return args.error;
+}
+
+/* *INDENT-OFF* */
+VLIB_CLI_COMMAND (avf_create_command, static) = {
+  .path = "create interface avf",
+  .short_help = "create interface avf <pci-address>",
+  .function = avf_create_command_fn,
+};
+/* *INDENT-ON* */
+
+static clib_error_t *
+avf_delete_command_fn (vlib_main_t * vm, unformat_input_t * input,
+		       vlib_cli_command_t * cmd)
+{
+  unformat_input_t _line_input, *line_input = &_line_input;
+  u32 sw_if_index = ~0;
+  vnet_hw_interface_t *hw;
+  avf_main_t *am = &avf_main;
+  avf_device_t *ad;
+  vnet_main_t *vnm = vnet_get_main ();
+
+  /* Get a line of input. */
+  if (!unformat_user (input, unformat_line_input, line_input))
+    return 0;
+
+  while (unformat_check_input (line_input) != UNFORMAT_END_OF_INPUT)
+    {
+      if (unformat (line_input, "sw_if_index %d", &sw_if_index))
+	;
+      else if (unformat (line_input, "%U", unformat_vnet_sw_interface,
+			 vnm, &sw_if_index))
+	;
+      else
+	return clib_error_return (0, "unknown input `%U'",
+				  format_unformat_error, input);
+    }
+  unformat_free (line_input);
+
+  if (sw_if_index == ~0)
+    return clib_error_return (0,
+			      "please specify interface name or sw_if_index");
+
+  hw = vnet_get_sup_hw_interface (vnm, sw_if_index);
+  if (hw == NULL || avf_device_class.index != hw->dev_class_index)
+    return clib_error_return (0, "not a AVF interface");
+
+  ad = pool_elt_at_index (am->devices, hw->dev_instance);
+
+  avf_delete_if (vm, ad);
+
+  return 0;
+}
+
+/* *INDENT-OFF* */
+VLIB_CLI_COMMAND (avf_delete_command, static) = {
+  .path = "delete interface avf",
+  .short_help = "delete interface avf "
+    "{<interface> | sw_if_index <sw_idx>}",
+  .function = avf_delete_command_fn,
+};
+/* *INDENT-ON* */
+
+static clib_error_t *
+avf_test_command_fn (vlib_main_t * vm, unformat_input_t * input,
+		     vlib_cli_command_t * cmd)
+{
+  unformat_input_t _line_input, *line_input = &_line_input;
+  u32 sw_if_index = ~0;
+  vnet_hw_interface_t *hw;
+  avf_main_t *am = &avf_main;
+  avf_device_t *ad;
+  vnet_main_t *vnm = vnet_get_main ();
+  int test_irq = 0, enable_elog = 0, disable_elog = 0;
+
+  /* Get a line of input. */
+  if (!unformat_user (input, unformat_line_input, line_input))
+    return 0;
+
+  while (unformat_check_input (line_input) != UNFORMAT_END_OF_INPUT)
+    {
+      if (unformat (line_input, "sw_if_index %d", &sw_if_index))
+	;
+      else if (unformat (line_input, "irq"))
+	test_irq = 1;
+      else if (unformat (line_input, "elog-on"))
+	enable_elog = 1;
+      else if (unformat (line_input, "elog-off"))
+	disable_elog = 1;
+      else if (unformat (line_input, "%U", unformat_vnet_sw_interface,
+			 vnm, &sw_if_index))
+	;
+      else
+	return clib_error_return (0, "unknown input `%U'",
+				  format_unformat_error, input);
+    }
+  unformat_free (line_input);
+
+  if (sw_if_index == ~0)
+    return clib_error_return (0,
+			      "please specify interface name or sw_if_index");
+
+  hw = vnet_get_sup_hw_interface (vnm, sw_if_index);
+  if (hw == NULL || avf_device_class.index != hw->dev_class_index)
+    return clib_error_return (0, "not a AVF interface");
+
+  ad = pool_elt_at_index (am->devices, hw->dev_instance);
+
+  if (enable_elog)
+    ad->flags |= AVF_DEVICE_F_ELOG;
+
+  if (disable_elog)
+    ad->flags &= ~AVF_DEVICE_F_ELOG;
+
+  if (test_irq)
+    avf_reg_write (ad, AVFINT_DYN_CTL0, (1 << 0) | (3 << 3) | (1 << 2));
+
+  return 0;
+}
+
+/* *INDENT-OFF* */
+VLIB_CLI_COMMAND (avf_test_command, static) = {
+  .path = "test avf",
+  .short_help = "test avf [<interface> | sw_if_index <sw_idx>] [irq] "
+    "[elog-on] [elog-off]",
+  .function = avf_test_command_fn,
+};
+/* *INDENT-ON* */
+
+clib_error_t *
+avf_cli_init (vlib_main_t * vm)
+{
+  return 0;
+}
+
+VLIB_INIT_FUNCTION (avf_cli_init);
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */

src/plugins/avf/format.c
@@ -1,0 +1,145 @@
+/*
+ *------------------------------------------------------------------
+ * Copyright (c) 2018 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *------------------------------------------------------------------
+ */
+
+#include <vlib/vlib.h>
+#include <vlib/unix/unix.h>
+#include <vlib/pci/pci.h>
+#include <vnet/ethernet/ethernet.h>
+
+#include <avf/avf.h>
+
+u8 *
+format_avf_device_name (u8 * s, va_list * args)
+{
+  u32 i = va_arg (*args, u32);
+  avf_main_t *am = &avf_main;
+  avf_device_t *ad = vec_elt_at_index (am->devices, i);
+  vlib_pci_addr_t *addr = vlib_pci_get_addr (ad->pci_dev_handle);
+
+  s = format (s, "AVF%x/%x/%x/%x",
+	      addr->domain, addr->bus, addr->slot, addr->function);
+  return s;
+}
+
+u8 *
+format_avf_device_flags (u8 * s, va_list * args)
+{
+  avf_device_t *ad = va_arg (*args, avf_device_t *);
+  u8 *t = 0;
+
+#define _(a, b, c) if (ad->flags & (1 << a)) \
+t = format (t, "%s%s", t ? " ":"", c);
+  foreach_avf_device_flags
+#undef _
+    s = format (s, "%v", t);
+  vec_free (t);
+  return s;
+}
+
+u8 *
+format_avf_vf_cap_flags (u8 * s, va_list * args)
+{
+  u32 flags = va_arg (*args, u32);
+  u8 *t = 0;
+
+#define _(a, b, c) if (flags & (1 << a)) \
+  t = format (t, "%s%s", t ? " ":"", c);
+  foreach_avf_vf_cap_flag;
+#undef _
+  s = format (s, "%v", t);
+  vec_free (t);
+  return s;
+}
+
+static u8 *
+format_virtchnl_link_speed (u8 * s, va_list * args)
+{
+  virtchnl_link_speed_t speed = va_arg (*args, virtchnl_link_speed_t);
+
+  if (speed == 0)
+    return format (s, "unknown");
+#define _(a, b, c) \
+  else if (speed == VIRTCHNL_LINK_SPEED_##b) \
+    return format (s, c);
+  foreach_virtchnl_link_speed;
+#undef _
+  return s;
+}
+
+u8 *
+format_avf_device (u8 * s, va_list * args)
+{
+  u32 i = va_arg (*args, u32);
+  avf_main_t *am = &avf_main;
+  avf_device_t *ad = vec_elt_at_index (am->devices, i);
+  u32 indent = format_get_indent (s);
+  u8 *a = 0;
+
+  s = format (s, "flags: %U", format_avf_device_flags, ad);
+  s = format (s, "\n%Uoffload features: %U", format_white_space, indent,
+	      format_avf_vf_cap_flags, ad->feature_bitmap);
+
+  s = format (s, "\n%Unum-queue-pairs %d max-vectors %u max-mtu %u "
+	      "rss-key-size %u rss-lut-size %u", format_white_space, indent,
+	      ad->num_queue_pairs, ad->max_vectors, ad->max_mtu,
+	      ad->rss_key_size, ad->rss_lut_size);
+  s = format (s, "\n%Uspeed %U", format_white_space, indent,
+	      format_virtchnl_link_speed, ad->link_speed);
+  if (ad->error)
+    s = format (s, "\n%Uerror %U", format_white_space, indent,
+		format_clib_error, ad->error);
+
+#define _(c) if (ad->eth_stats.c) \
+  a = format (a, "\n%U%-20U %u", format_white_space, indent + 2, \
+	      format_c_identifier, #c, ad->eth_stats.c);
+  foreach_virtchnl_eth_stats;
+#undef _
+  if (a)
+    s = format (s, "\n%Ustats:%v", format_white_space, indent, a);
+
+  vec_free (a);
+  return s;
+}
+
+u8 *
+format_avf_input_trace (u8 * s, va_list * args)
+{
+  vlib_main_t *vm = va_arg (*args, vlib_main_t *);
+  vlib_node_t *node = va_arg (*args, vlib_node_t *);
+  avf_input_trace_t *t = va_arg (*args, avf_input_trace_t *);
+  vnet_main_t *vnm = vnet_get_main ();
+  vnet_hw_interface_t *hi = vnet_get_hw_interface (vnm, t->hw_if_index);
+  u32 indent = format_get_indent (s);
+  avf_rx_vector_entry_t *rxve = &t->rxve;
+
+  s = format (s, "avf: %v (%d) next-node %U",
+	      hi->name, t->hw_if_index, format_vlib_next_node_name, vm,
+	      node->index, t->next_index);
+  s = format (s, "\n%Ustatus 0x%x error 0x%x ptype 0x%x length %u",
+	      format_white_space, indent + 2, rxve->status, rxve->error,
+	      rxve->ptype, rxve->length);
+
+  return s;
+}
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */

commit def01489fc7851d12991f09860b84e6811c354cc

avf: improve elogs
Change-Id: I6221e1a5924223865b3caf53590d3668965b564c
Type: fix
Fixes: 24b5576
Signed-off-by: Damjan Marion <damarion@cisco.com>

src/plugins/avf/CMakeLists.txt
@@ -15,6 +15,7 @@ add_vpp_plugin(avf
   SOURCES
   cli.c
   device.c
+  elog.c
   format.c
   input.c
   output.c

src/plugins/avf/avf.h
@@ -370,6 +370,13 @@ avf_get_device (u32 dev_instance)
   return pool_elt_at_index (avf_main.devices, dev_instance)[0];
 }
 
+/* elog.c */
+void avf_elog_init ();
+void avf_elog_reg (avf_device_t *ad, u32 addr, u32 val, int is_read);
+void avf_elog_aq_enq_req (avf_device_t *ad, avf_aq_desc_t *d);
+void avf_elog_aq_enq_resp (avf_device_t *ad, avf_aq_desc_t *d);
+void avf_elog_arq_desc (avf_device_t *ad, avf_aq_desc_t *d);
+
 static inline u32
 avf_get_u32 (void *start, int offset)
 {
@@ -413,13 +420,20 @@ avf_set_u32 (void *start, int offset, u32 value)
 static inline void
 avf_reg_write (avf_device_t * ad, u32 addr, u32 val)
 {
+  if (ad->flags & AVF_DEVICE_F_ELOG)
+    avf_elog_reg (ad, addr, val, 0);
   *(volatile u32 *) ((u8 *) ad->bar0 + addr) = val;
 }
 
 static inline u32
 avf_reg_read (avf_device_t * ad, u32 addr)
 {
-  return *(volatile u32 *) (ad->bar0 + addr);
+  u32 val = *(volatile u32 *) (ad->bar0 + addr);
+
+  if (ad->flags & AVF_DEVICE_F_ELOG)
+    avf_elog_reg (ad, addr, val, 1);
+
+  return val;
 }
 
 static inline void

src/plugins/avf/device.c
@@ -1614,7 +1614,10 @@ avf_create_if (vlib_main_t * vm, avf_create_if_args_t * args)
   ad->name = vec_dup (args->name);
 
   if (args->enable_elog)
-    ad->flags |= AVF_DEVICE_F_ELOG;
+    {
+      ad->flags |= AVF_DEVICE_F_ELOG;
+      avf_elog_init ();
+    }
 
   if ((error = vlib_pci_device_open (vm, &args->addr, avf_pci_device_ids,
 				     &h)))

src/plugins/avf/elog.c
@@ -1,0 +1,207 @@
+/* SPDX-License-Identifier: Apache-2.0
+ * Copyright(c) 2021 Cisco Systems, Inc.
+ */
+
+#include <vlib/vlib.h>
+#include <vlib/unix/unix.h>
+#include <vlib/pci/pci.h>
+#include <vnet/ethernet/ethernet.h>
+
+#include <avf/avf.h>
+
+static uword *register_name_by_addr = 0;
+
+void
+avf_elog_aq_enq_req (avf_device_t *ad, avf_aq_desc_t *d)
+{
+  if (d->opcode == 0x801) /* send_to_pf */
+    {
+      ELOG_TYPE_DECLARE (el) = {
+	.format = "avf[%d] aq_enq_req: send_to_pf flags 0x%x datalen %d "
+	  "v_opcode %s (%d)",
+	.format_args = "i4i2i2t2i2",
+	.n_enum_strings = VIRTCHNL_N_OPS,
+	.enum_strings = {
+#define _(v, n) [v] = #n,
+	      foreach_virtchnl_op
+#undef _
+	  },
+      };
+
+      struct
+      {
+	u32 dev_instance;
+	u16 flags;
+	u16 datalen;
+	u16 v_opcode;
+	u16 v_opcode_val;
+      } * ed;
+
+      ed = ELOG_DATA (&vlib_global_main.elog_main, el);
+      ed->dev_instance = ad->dev_instance;
+      ed->flags = d->flags;
+      ed->datalen = d->datalen;
+      ed->v_opcode = ed->v_opcode_val = d->v_opcode;
+    }
+  else
+    {
+      ELOG_TYPE_DECLARE (el) = {
+	.format = "avf[%d] aq_enq_req: opcode 0x%x flags 0x%x datalen %d",
+	.format_args = "i4i2i2i2"
+      };
+
+      struct
+      {
+	u32 dev_instance;
+	u16 opcode;
+	u16 flags;
+	u16 datalen;
+      } * ed;
+
+      ed = ELOG_DATA (&vlib_global_main.elog_main, el);
+      ed->dev_instance = ad->dev_instance;
+      ed->opcode = d->opcode;
+      ed->flags = d->flags;
+      ed->datalen = d->datalen;
+    }
+}
+
+void
+avf_elog_aq_enq_resp (avf_device_t *ad, avf_aq_desc_t *d)
+{
+  ELOG_TYPE_DECLARE (el) = { .format =
+			       "avf[%d] aq_enq_resp: flags 0x%x retval %d",
+			     .format_args = "i4i2i2" };
+
+  struct
+  {
+    u32 dev_instance;
+    u16 flags;
+    u16 retval;
+  } * ed;
+
+  ed = ELOG_DATA (&vlib_global_main.elog_main, el);
+  ed->dev_instance = ad->dev_instance;
+  ed->flags = d->flags;
+  ed->retval = d->retval;
+}
+
+void
+avf_elog_arq_desc (avf_device_t *ad, avf_aq_desc_t *d)
+{
+  if (d->opcode == 0x802)
+    {
+      ELOG_TYPE_DECLARE (el) = {
+	.format = "avf[%d] arq_desc: msg_from_pf flags 0x%x retval %d "
+	  "v_opcode %s (%d) v_retval %d",
+	.format_args = "i4i2i2t2i2i2",
+	.n_enum_strings = VIRTCHNL_N_OPS,
+	.enum_strings = {
+#define _(v, n) [v] = #n,
+	      foreach_virtchnl_op
+#undef _
+	  },
+      };
+
+      struct
+      {
+	u32 dev_instance;
+	u16 flags;
+	u16 retval;
+	u16 v_opcode;
+	u16 v_opcode_val;
+	u16 v_retval;
+      } * ed;
+
+      ed = ELOG_DATA (&vlib_global_main.elog_main, el);
+      ed->dev_instance = ad->dev_instance;
+      ed->flags = d->flags;
+      ed->retval = d->retval;
+      ed->v_opcode = ed->v_opcode_val = d->v_opcode;
+      ed->v_retval = d->v_retval;
+    }
+  else
+    {
+      ELOG_TYPE_DECLARE (
+	el) = { .format = "avf[%d] arq_desc: flags 0x%x retval %d opcode 0x%x",
+		.format_args = "i4i2i2i2" };
+
+      struct
+      {
+	u32 dev_instance;
+	u16 flags;
+	u16 retval;
+	u16 opcode;
+      } * ed;
+
+      ed = ELOG_DATA (&vlib_global_main.elog_main, el);
+      ed->dev_instance = ad->dev_instance;
+      ed->flags = d->flags;
+      ed->retval = d->retval;
+      ed->opcode = d->opcode;
+    }
+}
+
+void
+avf_elog_reg (avf_device_t *ad, u32 addr, u32 val, int is_read)
+{
+  uword *p;
+  ELOG_TYPE_DECLARE (el) = {
+    .format = "avf[%d] reg: %s %s [0x%04x] val 0x%08x",
+    .format_args = "i4s4s4i4i4",
+  };
+
+  struct
+  {
+    u32 dev_instance;
+    char rw[4];
+    char reg_name[24];
+    u32 addr;
+    u32 val;
+  } * ed;
+
+  ed = ELOG_DATA (&vlib_global_main.elog_main, el);
+  ed->dev_instance = ad->dev_instance;
+  ed->addr = addr;
+  ed->val = val;
+  ed->rw[0] = is_read ? 'r' : 'w';
+  ed->rw[1] = 0;
+
+  p = hash_get (register_name_by_addr, addr);
+  strncpy (ed->reg_name, p ? (char *) p[0] : "unknown", 24);
+  ed->reg_name[23] = 0;
+}
+
+void
+avf_elog_init (void)
+{
+  if (register_name_by_addr)
+    return;
+
+  register_name_by_addr = hash_create (0, sizeof (uword));
+
+  hash_set (register_name_by_addr, AVFINT_ICR0, "AVFINT_ICR0");
+  hash_set (register_name_by_addr, AVFINT_ICR0_ENA1, "INT_ICR0_ENA1");
+  hash_set (register_name_by_addr, AVFINT_DYN_CTL0, "INT_DYN_CTL0");
+  hash_set (register_name_by_addr, AVF_ARQBAH, "ARQBAH");
+  hash_set (register_name_by_addr, AVF_ATQH, "ATQH");
+  hash_set (register_name_by_addr, AVF_ATQLEN, "ATQLEN");
+  hash_set (register_name_by_addr, AVF_ARQBAL, "ARQBAL");
+  hash_set (register_name_by_addr, AVF_ARQT, "ARQT");
+  hash_set (register_name_by_addr, AVF_ARQH, "ARQH");
+  hash_set (register_name_by_addr, AVF_ATQBAH, "ATQBAH");
+  hash_set (register_name_by_addr, AVF_ATQBAL, "ATQBAL");
+  hash_set (register_name_by_addr, AVF_ARQLEN, "ARQLEN");
+  hash_set (register_name_by_addr, AVF_ATQT, "ATQT");
+  hash_set (register_name_by_addr, AVFGEN_RSTAT, "GEN_RSTAT");
+
+  for (int i = 0; i < 16; i++)
+    {
+      hash_set (register_name_by_addr, AVFINT_DYN_CTLN (i),
+		format (0, "INT_DYN_CTLN(%u)%c", i, 0));
+      hash_set (register_name_by_addr, AVF_QTX_TAIL (i),
+		format (0, "QTX_TAIL(%u)%c", i, 0));
+      hash_set (register_name_by_addr, AVF_QRX_TAIL (i),
+		format (0, "QRX_TAIL(%u)%c", i, 0));
+    }
+}