alisherqodirjonov/vpp

commit 566ded4b32101dd38c76e61f9453feecf11660f5

docs: better docs, mv doxygen to sphinx
This patch refactors the VPP sphinx docs
in order to make it easier to consume
for external readers as well as VPP developers.

It also makes sphinx the single source
of documentation, which simplifies maintenance
and operation.

Most important updates are:

- reformat the existing documentation as rst
- split RELEASE.md and move it into separate rst files
- remove section 'events'
- remove section 'archive'
- remove section 'related projects'
- remove section 'feature by release'
- remove section 'Various links'
- make (Configuration reference, CLI docs,
  developer docs) top level items in the list
- move 'Use Cases' as part of 'About VPP'
- move 'Troubleshooting' as part of 'Getting Started'
- move test framework docs into 'Developer Documentation'
- add a 'Contributing' section for gerrit,
  docs and other contributer related infos
- deprecate doxygen and test-docs targets
- redirect the "make doxygen" target to "make docs"

Type: refactor

Change-Id: I552a5645d5b7964d547f99b1336e2ac24e7c209f
Signed-off-by: Nathan Skrzypczak <nathan.skrzypczak@gmail.com>
Signed-off-by: Andrew Yourtchenko <ayourtch@gmail.com>

Makefile
@@ -209,9 +209,9 @@ help:
 	@echo " checkstyle-test-diff - check test framework coding style (only changed files)"
 	@echo " checkstyle-api       - check api for incompatible changes"
 	@echo " fixstyle             - fix coding style"
-	@echo " doxygen              - (re)generate documentation"
-	@echo " bootstrap-doxygen    - setup Doxygen dependencies"
-	@echo " wipe-doxygen         - wipe all generated documentation"
+	@echo " doxygen              - DEPRECATED - use 'make docs'"
+	@echo " bootstrap-doxygen    - DEPRECATED"
+	@echo " wipe-doxygen         - DEPRECATED"
 	@echo " checkfeaturelist     - check FEATURE.yaml according to schema"
 	@echo " featurelist          - dump feature list in markdown"
 	@echo " json-api-files       - (re)-generate json api files"
@@ -451,11 +451,13 @@ test-dep:
 
 .PHONY: test-doc
 test-doc:
-	@make -C test doc
+	@echo "make test-doc is DEPRECATED: use 'make doc'"
+	sleep 300
 
 .PHONY: test-wipe-doc
 test-wipe-doc:
-	@make -C test wipe-doc
+	@echo "make test-wipe-doc is DEPRECATED"
+	sleep 300
 
 .PHONY: test-cov
 test-cov:
@@ -667,42 +669,28 @@ checkfeaturelist: centos-pyyaml
 # Build the documentation
 #
 
-# Doxygen configuration and our utility scripts
-export DOXY_DIR ?= $(WS_ROOT)/doxygen
-
-define make-doxy
-	@OS_ID="$(OS_ID)" make -C $(DOXY_DIR) $@
-endef
-
 .PHONY: bootstrap-doxygen
 bootstrap-doxygen:
-	$(call make-doxy)
+	@echo "make bootstrap-doxygen is DEPRECATED"
+	sleep 300
 
 .PHONY: doxygen
-doxygen: bootstrap-doxygen
-	$(call make-doxy)
+doxygen: docs
+	@echo "make doxygen is DEPRECATED: use 'make docs'"
+	sleep 300
 
 .PHONY: wipe-doxygen
 wipe-doxygen:
-	$(call make-doxy)
+	@echo "make wipe-doxygen is DEPRECATED"
+	sleep 300
 
-# Sphinx Documents
-export DOCS_DIR = $(WS_ROOT)/docs
-export VENV_DIR = $(WS_ROOT)/sphinx_venv
-export SPHINX_SCRIPTS_DIR = $(WS_ROOT)/docs/scripts
-
-.PHONY: docs-venv
-docs-venv:
-	@($(SPHINX_SCRIPTS_DIR)/sphinx-make.sh venv)
+.PHONY: docs-%
+docs-%:
+	@make -C $(WS_ROOT)/docs $*
 
 .PHONY: docs
-docs: $(DOCS_DIR)
-	@($(SPHINX_SCRIPTS_DIR)/sphinx-make.sh html)
-
-.PHONY: docs-clean
-docs-clean:
-	@rm -rf $(DOCS_DIR)/_build
-	@rm -rf $(VENV_DIR)
+docs:
+	@make -C $(WS_ROOT)/docs docs
 
 .PHONY: pkg-verify
 pkg-verify: install-dep $(BR)/.deps.ok install-ext-deps

docs/Makefile
@@ -5,33 +5,46 @@ ifeq ($(shell uname),Darwin)
 OS_ID = darwin
 endif
 
+# These should be passed in by the root Makefile
+WS_ROOT ?= $(CURDIR)/..
+BR ?= $(WS_ROOT)/build-root
+DOCS_DIR ?= $(WS_ROOT)/docs
+
+VENV_DIR ?= $(DOCS_DIR)/venv
+SPHINX_SCRIPTS_DIR ?= $(WS_ROOT)/docs/scripts
+
 # Work out the OS if we haven't already
 OS_ID ?= $(shell grep '^ID=' /etc/os-release | cut -f2- -d= | sed -e 's/\"//g')
+OS_VERSION ?= $(shell grep '^VERSION_ID=' /etc/os-release  | cut -f2- -d= | sed -e 's/\"//g')
+PIP_VERSION ?= $(shell grep 'PIP_VERSION=' ${WS_ROOT}/test/Makefile | cut -d'=' -f2)
+PIP_TOOLS_VERSION ?= $(shell grep 'PIP_TOOLS_VERSION=' ${WS_ROOT}/test/Makefile | cut -d'=' -f2)
+
+PYTHON ?= "python3"
 
 DOC_DEB_DEPENDS = enchant
 DOC_RPM_DEPENDS = enchant
 
 # You can set these variables from the command line.
-SPHINXOPTS    =
-SPHINXBUILD   = sphinx-build
-SPHINXPROJ    = fdio-vpp
-SOURCEDIR     = .
-BUILDDIR      = _build
+SPHINXOPTS        = --keep-going -n -W
+SPHINXBUILD       = sphinx-build
+SPHINXPROJ        = fdio-vpp
+SOURCEDIR         = .
+BUILDDIR          = ${BR}/docs
+BUILDDIR_SRC      = ${BUILDDIR}/src
+BUILDDIR_OUT      = ${BUILDDIR}/html
+SCRIPTS_DIR       = _scripts
+
 
 # Put it first so that "make" without argument is like "make help".
+.PHONY: help
 help:
-	@$(SPHINXBUILD) -M help "$(SOURCEDIR)" "$(BUILDDIR)" $(SPHINXOPTS) $(O)
+	@( \
+	  . ${VENV_DIR}/bin/activate; \
+	  $(SPHINXBUILD) --help ;\
+	)
 
-.PHONY: help Makefile
-
-# Catch-all target: route all unknown targets to Sphinx using the new
-# "make mode" option.  $(O) is meant as a shortcut for $(SPHINXOPTS).
-%: Makefile
-# Generate dynamic content
-	@python3 ./includes_renderer.py
-	@$(SPHINXBUILD) -M $@ "$(SOURCEDIR)" "$(BUILDDIR)" $(SPHINXOPTS) $(O)
-
-spell:
+.PHONY: checkdeps
+checkdeps:
 	@echo "Checking whether dependencies for Docs are installed..."
 ifeq ($(OS_ID),ubuntu)
 	@set -e; inst=; \
@@ -45,4 +58,45 @@ ifeq ($(OS_ID),ubuntu)
 else ifneq ("$(wildcard /etc/redhat-release)","")
 	@sudo yum install $(CONFIRM) $(DOC_RPM_DEPENDS)
 endif
-	@$(SPHINXBUILD) -M $@ "$(SOURCEDIR)" "$(BUILDDIR)" -W -b spelling  $(O)
+
+.PHONY: spell
+spell: clean checkdeps venv ${BUILDDIR_SRC}
+	@( \
+	  . ${VENV_DIR}/bin/activate; \
+	  make -C ${SCRIPTS_DIR} generate && \
+	  $(SPHINXBUILD) -b spelling $(SPHINXOPTS) $(BUILDDIR_SRC) $(BUILDDIR_OUT); \
+	)
+
+.PHONY: venv
+venv:
+	@( \
+	    if [ ! -d ${VENV_DIR} ]; then \
+	      ${PYTHON} -m venv ${VENV_DIR}; \
+	      . ${VENV_DIR}/bin/activate; \
+	      ${PYTHON} -m pip install pip==${PIP_VERSION}; \
+	      ${PYTHON} -m pip install pip-tools==${PIP_TOOLS_VERSION}; \
+	      ${PYTHON} -m pip install -r ${WS_ROOT}/test/requirements-3.txt; \
+	    fi; \
+	)
+
+${BUILDDIR_SRC}:
+	@mkdir -p ${BUILDDIR_SRC}
+	@cp -r $(SOURCEDIR) ${BUILDDIR_SRC}
+	@cd ${BUILDDIR_SRC} && find . -type l -exec cp --remove-destination -L ${DOCS_DIR}/{} {} \;
+
+.PHONY: docs
+docs: clean venv ${BUILDDIR_SRC}
+	@( \
+	  . ${VENV_DIR}/bin/activate; \
+	  make -C ${SCRIPTS_DIR} generate && \
+	  $(SPHINXBUILD) $(SPHINXOPTS) -b html $(BUILDDIR_SRC) $(BUILDDIR_OUT); \
+	)
+
+.PHONY: clean
+clean:
+	@rm -rf $(BUILDDIR)
+	@make -C ${SCRIPTS_DIR} clean
+
+.PHONY: build
+build: docs
+

docs/_scripts/Makefile
@@ -1,0 +1,212 @@
+# Copyright (c) 2021 Comcast Cable Communications Management, LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# Build the documentation
+#
+
+# Default target
+.PHONY: all
+all: siphon
+
+# These should be passed in by the root Makefile
+WS_ROOT ?= $(CURDIR)/../..
+BR ?= $(WS_ROOT)/build-root
+
+# Tag used in github repository path.
+# Change this when genearting for a release
+VPP_TAG ?= master
+
+REPOSITORY_URL ?= https://github.com/FDio/vpp/blob/$(VPP_TAG)/
+
+# Doxygen configuration and our utility scripts
+SCRIPTS_DIR ?= $(WS_ROOT)/docs/_scripts
+
+# docs root directory
+DOCS_DIR ?= ${BR}/docs/src
+
+FEATURE_LIST_FILE = ${DOCS_DIR}/aboutvpp/featurelist.md
+
+# Siphoned fragements are processed into here
+DOCS_GENERATED_DIR ?= $(DOCS_DIR)/_generated
+
+# Siphoned fragments end up in here
+SIPHON_INPUT_DIR ?= $(DOCS_GENERATED_DIR)/fragments
+
+DYNAMIC_RENDER_DIR ?= ${DOCS_GENERATED_DIR}/includes
+
+# Primary source directories
+SIPHON_SRC ?= $(WS_ROOT)/src
+SIPHON_SRC_DIRECTORIES = \
+	$(shell find $(SIPHON_SRC) -name '*.md' -print | xargs dirname \
+	| sort | uniq) \
+	$(SIPHON_SRC)/vppinfra \
+	$(SIPHON_SRC)/svm \
+	$(SIPHON_SRC)/vlib \
+	$(SIPHON_SRC)/vlibapi \
+	$(SIPHON_SRC)/vlibmemory \
+	$(SIPHON_SRC)/vnet \
+	$(SIPHON_SRC)/vpp \
+	$(SIPHON_SRC)/vpp-api \
+	$(SIPHON_SRC)/examples
+
+# Input directories and files
+SIPHON_INPUT ?= \
+	$(wildcard $(WS_ROOT)/*.md) \
+	$(wildcard $(SCRIPTS_DIR)/*.md) \
+	$(SIPHON_SRC_DIRECTORIES) \
+	$(SIPHON_SRC)/plugins \
+	extras
+
+# Strip leading workspace path from input names
+SIPHON_INPUT := $(subst $(WS_ROOT)/,,$(SIPHON_INPUT))
+
+# Files to exclude, from pre-Doxygen steps, eg because they're
+# selectively compiled.
+# Examples would be to exclude non-DPDK related sources when
+# there's a DPDK equivalent that conflicts.
+# These must be left-anchored paths for the regexp below to work.
+SIPHON_EXCLUDE ?= \
+	$(SIPHON_SRC)/vpp-api/lua
+
+# Generate a regexp for filenames to exclude
+SIPHON_EXCLUDE_REGEXP = ($(subst .,\.,$(shell echo '$(strip $(SIPHON_EXCLUDE))' | sed -e 's/ /|/g')))
+
+# Include all the normal source directories in the include file path
+SIPHON_INCLUDE_PATH = $(SIPHON_SRC_DIRECTORIES)
+
+# Find API header directories and include them in the header path.
+# This is only useful if VPP and plugins are already built; nothing
+# here depends on those targets. We don't build documentation for these
+# header files, they're just added to the INCLUDE search path for Doxygen.
+_vpp_br = $(shell find "$(BR)" -maxdepth 1 -type d \
+	'(' -name build-vpp_debug-native -o -name build-vpp-native ')' -print \
+	| sed -e 's@^$(WS_ROOT)/*@@' -e 1q)
+ifneq ($(strip $(_vpp_br)),)
+SIPHON_INCLUDE_PATH += \
+	$(_vpp_br)/vlib-api \
+	$(_vpp_br)/vpp
+# Also include any plugin directories that exist
+SIPHON_INCLUDE_PATH += \
+	$(shell find $(WS_ROOT)/$(_vpp_br)/plugins -maxdepth 1 -type d | sed -e 's@^$(WS_ROOT)/*@@')
+endif
+
+# Discover if we have CPP available
+_cpp = $(shell which cpp)
+ifneq ($(strip $(_cpp)),)
+# Add whatever directories CPP normally includes to the header path
+SIPHON_INCLUDE_PATH += $(shell set -e; $(_cpp) -v </dev/null 2>&1 | awk 'f&&/^ /{print $$1} /^\#include/{f=1}')
+endif
+
+# All the siphon types we know about
+SIPHONS ?= clicmd syscfg
+
+SIPHON_FILES = $(addprefix $(SIPHON_INPUT_DIR)/,$(addsuffix .siphon,$(SIPHONS)))
+SIPHON_DOCS = $(addprefix $(DOCS_GENERATED_DIR)/,$(addsuffix .rst,$(SIPHONS)))
+
+BUILT_ON = $(shell date '+%d %B %Y')
+VPP_VERSION = $(shell ${WS_ROOT}/src/scripts/version)
+
+.PHONY: featurelist
+featurelist:
+	@( \
+	  cd $(WS_ROOT) && \
+	  find . -name FEATURE.yaml | \
+	    ./src/scripts/fts.py \
+	      --markdown \
+	      --repolink $(REPOSITORY_URL) > \
+	    $(FEATURE_LIST_FILE) ; \
+	)
+
+
+.PHONY: includes-render
+includes-render:
+	@mkdir -p "$(DYNAMIC_RENDER_DIR)"
+	@python3 $(SCRIPTS_DIR)/includes_renderer.py ${WS_ROOT} ${DYNAMIC_RENDER_DIR}
+
+.PHONY: template-index
+template-index:
+	@sed -ie "s/__VPP_VERSION__/${VPP_VERSION}/g" ${DOCS_DIR}/index.rst
+	@sed -ie "s/__BUILT_ON__/${BUILT_ON}/g" ${DOCS_DIR}/index.rst
+
+.NOTPARALLEL: $(SIPHON_FILES)
+$(SIPHON_FILES): $(SCRIPTS_DIR)/siphon-generate \
+		$(addprefix,$(WSROOT),$(SIPHON_INPUT)) \
+		$(wildcard $(SCRIPTS_DIR)/siphon/*.py)
+	@echo "Validating source tree..."
+	@set -e; for input in $(SIPHON_INPUT); do \
+		if [ ! -e "$(WS_ROOT)/$$input" ]; then \
+			echo "ERROR: Input path '$$input' does not exist." >&2; \
+			exit 1; \
+		fi; \
+	done
+	@rm -rf "$(SIPHON_INPUT_DIR)" "$(DOCS_GENERATED_DIR)"
+	@mkdir -p "$(SIPHON_INPUT_DIR)" "$(DOCS_GENERATED_DIR)"
+	@touch $(SIPHON_INPUT_DIR)/files
+	@echo "Collating source file list for siphoning..."
+	@for input in $(SIPHON_INPUT); do \
+		cd "$(WS_ROOT)"; \
+		find "$$input" -type f \
+			\( -name '*.[ch]' -or -name '*.dox' \) -print \
+			| grep -v -E '^src/examples/' \
+			| grep -v -E '^$(SIPHON_EXCLUDE_REGEXP)' \
+			>> $(SIPHON_INPUT_DIR)/files; \
+	done
+	@echo "Generating siphons..."
+	@set -e; \
+	cd "$(WS_ROOT)"; \
+	$(SCRIPTS_DIR)/siphon-generate \
+		--output="$(SIPHON_INPUT_DIR)" \
+		"@$(SIPHON_INPUT_DIR)/files"
+
+# Evaluate this to build a siphon doc output target for each desired
+# output type:
+# $(eval $(call siphon-process,file_extension,output_type_name))
+define siphon-process
+$(DOCS_GENERATED_DIR)/%.$(1): $(SIPHON_INPUT_DIR)/%.siphon \
+		$(SCRIPTS_DIR)/siphon-process \
+		$(wildcard $(SCRIPTS_DIR)/siphon/*.py) \
+		$(wildcard $(SCRIPTS_DIR)/siphon_templates/$(2)/*/*.$(1))
+	@echo "Processing siphon for $(2) from $$(notdir $$<)..."
+	@set -e; \
+	cd "$(WS_ROOT)"; \
+	mkdir -p $(DOCS_GENERATED_DIR)/$$(basename $$(notdir $$<)).$(1).dir; \
+	$(SCRIPTS_DIR)/siphon-process \
+		--type=$$(basename $$(notdir $$<)) \
+		--format=$(2) \
+		--repolink=$(REPOSITORY_URL)/ \
+		--outdir=$(DOCS_GENERATED_DIR)/$$(basename $$(notdir $$<)).$(1).dir \
+		--output="$$@" \
+		"$$<"
+endef
+
+# Process the .siphon source fragments and render them into siphon flavored
+# markdown documentation
+.DELETE_ON_ERROR: $(SIPHON_DOCS)
+$(eval $(call siphon-process,rst,markdown))
+
+# This target can be used just to generate the siphoned things
+.PHONY: siphon
+siphon: $(SIPHON_DOCS)
+	@cp $(DOCS_GENERATED_DIR)/clicmd.rst $(DOCS_DIR)/cli-reference/index.rst
+	@cp -r $(DOCS_GENERATED_DIR)/clicmd.rst.dir $(DOCS_DIR)/cli-reference/clis
+
+.PHONY: generate
+generate: siphon includes-render template-index featurelist
+
+.PHONY: clean
+clean:
+	@rm -rf $(BR)/.siphon.dep
+	@rm -rf $(SCRIPTS_DIR)/siphon/__pycache__
+

doxygen/Makefile
@@ -1,270 +1,0 @@
-# Copyright (c) 2016 Comcast Cable Communications Management, LLC.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at:
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-#
-# Build the documentation
-#
-
-# Default target
-.PHONY: all
-all: doxygen
-
-# These should be passed in by the root Makefile
-WS_ROOT ?= $(CURDIR)/..
-BR ?= $(WS_ROOT)/build-root
-
-# We support MacOS for docs generation
-ifeq ($(shell uname),Darwin)
-OS_ID = darwin
-endif
-
-# Work out the OS if we haven't already
-OS_ID ?= $(shell grep '^ID=' /etc/os-release | cut -f2- -d= | sed -e 's/\"//g')
-
-# Package dependencies
-DOC_DEB_DEPENDS = doxygen graphviz python3-pyparsing python3-jinja2
-DOC_RPM_DEPENDS = doxygen graphviz python3-pyparsing python3-jinja2
-DOC_MAC_BIN_DEPENDS = doxygen dot git
-DOC_MAC_PY_DEPENDS = pyparsing jinja2
-
-# Doxygen configuration and our utility scripts
-DOXY_DIR ?= $(WS_ROOT)/doxygen
-
-# Primary source directories
-DOXY_SRC ?= $(WS_ROOT)/src
-DOXY_SRC_DIRECTORIES = \
-	$(shell find $(DOXY_SRC) -name '*.md' -print | xargs dirname \
-	| sort | uniq) \
-	$(DOXY_SRC)/vppinfra \
-	$(DOXY_SRC)/svm \
-	$(DOXY_SRC)/vlib \
-	$(DOXY_SRC)/vlibapi \
-	$(DOXY_SRC)/vlibmemory \
-	$(DOXY_SRC)/vnet \
-	$(DOXY_SRC)/vpp \
-	$(DOXY_SRC)/vpp-api \
-	$(DOXY_SRC)/examples
-
-# Input directories and files
-DOXY_INPUT ?= \
-	$(wildcard $(WS_ROOT)/*.md) \
-	$(wildcard $(DOXY_DIR)/*.md) \
-	$(DOXY_SRC_DIRECTORIES) \
-	$(DOXY_SRC)/plugins \
-	extras
-
-# Strip leading workspace path from input names
-DOXY_INPUT := $(subst $(WS_ROOT)/,,$(DOXY_INPUT))
-
-# Files to exclude, from pre-Doxygen steps, eg because they're
-# selectively compiled.
-# Examples would be to exclude non-DPDK related sources when
-# there's a DPDK equivalent that conflicts.
-# These must be left-anchored paths for the regexp below to work.
-DOXY_EXCLUDE ?= \
-	$(DOXY_SRC)/vpp-api/lua
-
-# Generate a regexp for filenames to exclude
-DOXY_EXCLUDE_REGEXP = ($(subst .,\.,$(shell echo '$(strip $(DOXY_EXCLUDE))' | sed -e 's/ /|/g')))
-
-# Include all the normal source directories in the include file path
-DOXY_INCLUDE_PATH = $(DOXY_SRC_DIRECTORIES)
-
-# Find API header directories and include them in the header path.
-# This is only useful if VPP and plugins are already built; nothing
-# here depends on those targets. We don't build documentation for these
-# header files, they're just added to the INCLUDE search path for Doxygen.
-_vpp_br = $(shell find "$(BR)" -maxdepth 1 -type d \
-	'(' -name build-vpp_debug-native -o -name build-vpp-native ')' -print \
-	| sed -e 's@^$(WS_ROOT)/*@@' -e 1q)
-ifneq ($(strip $(_vpp_br)),)
-DOXY_INCLUDE_PATH += \
-	$(_vpp_br)/vlib-api \
-	$(_vpp_br)/vpp
-# Also include any plugin directories that exist
-DOXY_INCLUDE_PATH += \
-	$(shell find $(WS_ROOT)/$(_vpp_br)/plugins -maxdepth 1 -type d | sed -e 's@^$(WS_ROOT)/*@@')
-endif
-
-# Discover if we have CPP available
-_cpp = $(shell which cpp)
-ifneq ($(strip $(_cpp)),)
-# Add whatever directories CPP normally includes to the header path
-DOXY_INCLUDE_PATH += $(shell set -e; $(_cpp) -v </dev/null 2>&1 | awk 'f&&/^ /{print $$1} /^\#include/{f=1}')
-endif
-
-# Target directory for doxygen output
-DOXY_OUTPUT ?= $(BR)/docs
-
-# Siphoned fragments end up in here
-SIPHON_INPUT ?= $(DOXY_OUTPUT)/siphon_fragments
-
-# Siphoned fragements are processed into here
-SIPHON_OUTPUT ?= $(DOXY_OUTPUT)/siphon_docs
-
-# Extra document inputs that are processed in addition to DOXY_INPUT
-EXTRA_DOXY_INPUT += $(SIPHON_OUTPUT)
-
-# All the siphon types we know about
-SIPHONS ?= clicmd syscfg
-
-SIPHON_FILES = $(addprefix $(SIPHON_INPUT)/,$(addsuffix .siphon,$(SIPHONS)))
-SIPHON_DOCS = $(addprefix $(SIPHON_OUTPUT)/,$(addsuffix .md,$(SIPHONS)))
-SIPHON_ITEMLIST = $(addprefix $(SIPHON_OUTPUT)/,$(addsuffix .itemlist,$(filter clicmd,$(SIPHONS))))
-
-$(BR)/.doxygen-bootstrap.ok: Makefile
-	@echo "Checking whether dependencies for Doxygen are installed..."
-ifeq ($(OS_ID),ubuntu)
-	@set -e; inst=; \
-		for i in $(DOC_DEB_DEPENDS); do \
-			dpkg-query --show $$i >/dev/null 2>&1 || inst="$$inst $$i"; \
-		done; \
-		if [ "$$inst" ]; then \
-			sudo apt-get update; \
-			sudo apt-get $(CONFIRM) $(FORCE) install $$inst; \
-		fi
-	@if [ ! -s /usr/lib/graphviz/config6a ]; then \
-		echo "Rebuilding system Graphviz configuration."; \
-		sudo dot -c; \
-	fi
-else ifneq ("$(wildcard /etc/redhat-release)","")
-	@sudo yum install $(CONFIRM) $(DOC_RPM_DEPENDS)
-else ifeq ($(OS_ID),darwin)
-	@set -e; \
-	for bin in $(DOC_MAC_BIN_DEPENDS); do \
-		which -s $${bin} || (\
-			echo "Program '$${bin}' not found, please install it."; \
-			false; \
-		); \
-	done
-	@set -e; \
-	for py in $(DOC_MAC_PY_DEPENDS); do \
-		python -c "import $${py}" >/dev/null 2>&1 || (\
-			echo "Python package '$${py}' not found, please install it."; \
-			false; \
-		); \
-	done
-else
-	$(error "Building documentation currently works only on Ubuntu, CentOS, MacOS and OpenSUSE systems.")
-endif
-	@touch $@
-
-.PHONY: bootstrap-doxygen
-bootstrap-doxygen: $(BR)/.doxygen-bootstrap.ok
-
-.DELETE_ON_ERROR: $(BR)/.doxygen-siphon.dep
-$(BR)/.doxygen-siphon.dep: Makefile \
-		$(addprefix,$(WSROOT),$(DOXY_INPUT))
-	@echo "Building siphon dependencies..."
-	@rm -f "$@"; for input in $(DOXY_INPUT); do \
-		[ -e "$(WS_ROOT)/$$input" ] && \
-		find "$(WS_ROOT)/$$input" -type f \
-			\( -name '*.[ch]' -or -name '*.dox' \) -print \
-			| grep -v -E '^$(WS_ROOT)/$(DOXY_EXCLUDE_REGEXP)' \
-			| sed -e "s/^/\$$(SIPHON_FILES): /" \
-			>> $@; \
-	done
-
-# Include the source -> siphon dependencies
--include $(BR)/.doxygen-siphon.dep
-
-# Generate .siphon files that contain fragments of source file that
-# relate to the siphons we support.
-.NOTPARALLEL: $(SIPHON_FILES)
-$(SIPHON_FILES): $(BR)/.doxygen-bootstrap.ok \
-		$(DOXY_DIR)/siphon-generate \
-		$(addprefix,$(WSROOT),$(DOXY_INPUT)) \
-		$(wildcard $(DOXY_DIR)/siphon/*.py)
-	@echo "Validating source tree..."
-	@set -e; for input in $(DOXY_INPUT); do \
-		if [ ! -e "$(WS_ROOT)/$$input" ]; then \
-			echo "ERROR: Input path '$$input' does not exist." >&2; \
-			exit 1; \
-		fi; \
-	done
-	@rm -rf "$(SIPHON_INPUT)" "$(SIPHON_OUTPUT)"
-	@mkdir -p "$(SIPHON_INPUT)" "$(SIPHON_OUTPUT)"
-	@touch $(SIPHON_INPUT)/files
-	@echo "Collating source file list for siphoning..."
-	@for input in $(DOXY_INPUT); do \
-		cd "$(WS_ROOT)"; \
-		find "$$input" -type f \
-			\( -name '*.[ch]' -or -name '*.dox' \) -print \
-			| grep -v -E '^src/examples/' \
-			| grep -v -E '^$(DOXY_EXCLUDE_REGEXP)' \
-			>> $(SIPHON_INPUT)/files; \
-	done
-	@echo "Generating siphons..."
-	@set -e; \
-	cd "$(WS_ROOT)"; \
-	$(DOXY_DIR)/siphon-generate \
-		--output="$(SIPHON_INPUT)" \
-		"@$(SIPHON_INPUT)/files"
-
-# Evaluate this to build a siphon doc output target for each desired
-# output type:
-# $(eval $(call siphon-process,file_extension,output_type_name))
-define siphon-process
-$(SIPHON_OUTPUT)/%.$(1): $(SIPHON_INPUT)/%.siphon \
-		$(DOXY_DIR)/siphon-process \
-		$(wildcard $(DOXY_DIR)/siphon/*.py) \
-		$(wildcard $(DOXY_DIR)/siphon_templates/$(2)/*/*.$(1))
-	@echo "Processing siphon for $(2) from $$(notdir $$<)..."
-	@set -e; \
-	cd "$(WS_ROOT)"; \
-	$(DOXY_DIR)/siphon-process \
-		--type=$$(basename $$(notdir $$<)) \
-		--format=$(2) \
-		--output="$$@" \
-		"$$<"
-endef
-
-# Process the .siphon source fragments and render them into doxygen flavored
-# markdown documentation
-.DELETE_ON_ERROR: $(SIPHON_DOCS)
-$(eval $(call siphon-process,md,markdown))
-
-# Process the .siphon source fragments and render them into a list of cli
-# commands.
-.DELETE_ON_ERROR: $(SIPHON_ITEMLIST)
-$(eval $(call siphon-process,itemlist,itemlist))
-
-# This target can be used just to generate the siphoned things
-.PHONY: doxygen-siphon
-doxygen-siphon: $(SIPHON_DOCS) $(SIPHON_ITEMLIST)
-
-# Generate the doxygen docs
-.PHONY: doxygen
-doxygen: $(SIPHON_DOCS)
-	@mkdir -p "$(DOXY_OUTPUT)"
-	@echo "Running Doxygen..."
-	set -e; cd "$(WS_ROOT)"; \
-	    ROOT="$(WS_ROOT)" \
-	    BUILD_ROOT="$(BR)" \
-	    INPUT="$(addprefix $(WS_ROOT)/,$(DOXY_INPUT)) $(EXTRA_DOXY_INPUT)" \
-	    INCLUDE_PATH="$(DOXY_INCLUDE_PATH)" \
-	    EXCLUDE="$(DOXY_EXCLUDE)" \
-	    HTML=YES \
-	    VERSION="`git describe --tags --dirty`" \
-	    doxygen $(DOXY_DIR)/doxygen.cfg
-
-.PHONY: wipe-doxygen
-wipe-doxygen:
-	@rm -rf $(BR)/docs
-	@rm -rf $(BR)/.doxygen-siphon.dep
-	@rm -rf $(BR)/.doxygen-bootstrap.ok
-	@rm -rf $(DOXY_DIR)/siphon/__pycache__
-
-.PHONY: clean
-clean: wipe-doxygen

test/Makefile
@@ -14,7 +14,6 @@ ifndef TEST_DIR
 endif
 
 export TEST_BR = $(TEST_DIR)
-export TEST_DOC_BR = $(TEST_DIR)/doc/build
 FAILED_DIR=/tmp/vpp-failed-unittests/
 VPP_TEST_DIRS=$(shell ls -d $(TEST_DIR) $(EXTERN_TESTS))
 
@@ -239,21 +238,6 @@ wipe: reset
 	@rm -rf $(VENV_PATH)
 	@rm -rf $(patsubst %,%/__pycache__, $(VPP_TEST_DIRS))
 
-$(TEST_DOC_BR): $(PIP_INSTALL_DONE)
-	@mkdir -p $@
-	@bash -c "source $(VENV_PATH)/bin/activate && make -C doc html"
-
-.PHONY: doc
-doc: $(PIP_PATCH_DONE) $(TEST_DOC_BR)
-	@echo
-	@echo "Test Documentation URL: $(TEST_DOC_BR)/html/index.html"
-	@echo "Run 'make test-wipe-doc test-doc' to rebuild the test docs"
-	@echo
-
-.PHONY: wipe-doc
-wipe-doc:
-	@rm -rf $(TEST_DOC_BR)
-
 $(BUILD_COV_DIR):
 	@mkdir -p $@
 
@@ -279,7 +263,7 @@ wipe-papi:
 	@rm -rf $(PAPI_INSTALL_DONE) $(PAPI_WIPE_DIST)
 
 .PHONY: wipe-all
-wipe-all: wipe wipe-papi wipe-doc wipe-cov
+wipe-all: wipe wipe-papi wipe-cov
 	@rm -rf $(TEST_BR)
 
 .PHONY: checkstyle-diff
@@ -333,9 +317,8 @@ help:
 	@echo " test-gcov                      - build and run functional tests (gcov build)"
 	@echo " test-wipe              - wipe (temporary) files generated by unit tests"
 	@echo " test-wipe-cov          - wipe code coverage report for test framework"
-	@echo " test-wipe-doc          - wipe documentation for test framework"
 	@echo " test-wipe-papi         - rebuild vpp_papi sources"
-	@echo " test-wipe-all          - wipe (temporary) files generated by unit tests, docs, and coverage"
+	@echo " test-wipe-all          - wipe (temporary) files generated by unit tests, and coverage"
 	@echo " test-shell             - enter shell with test environment"
 	@echo " test-shell-debug       - enter shell with test environment (debug build)"
 	@echo " test-checkstyle        - check PEP8 compliance for test framework"
@@ -404,10 +387,6 @@ help:
 	@echo " VPP_IN_GDB_TMP_DIR     - specify directory to run VPP IN (default: /tmp/unittest-attach-gdb)"
 	@echo " VPP_IN_GDB_NO_RMDIR=0  - don't remove existing tmp dir but fail instead"
 	@echo " VPP_IN_GDB_CMDLINE=1   - add 'interactive' to VPP arguments to run with command line"
-	@echo ""
-	@echo "Creating test documentation"
-	@echo " test-doc               - generate documentation for test framework"
-	@echo " test-wipe-doc          - wipe documentation for test framework"
 	@echo ""
 	@echo "Creating test code coverage report"
 	@echo " test-cov               - generate code coverage report for test framework"

test/doc/Makefile
@@ -1,36 +1,0 @@
-# Makefile for VPP Test documentation
-#
-
-SPHINXOPTS    =
-SRC_DOC_DIR = $(TEST_DIR)/doc
-SPHINXBUILD = sphinx-build
-HTML_DOC_GEN_DIR = $(TEST_DOC_BR)/html
-API_DOC_GEN_DIR = $(TEST_DOC_BR)/api
-
-# Internal variables.
-ALLSPHINXOPTS   = -d $(TEST_DOC_BR)/.sphinx-cache $(SPHINXOPTS) $(API_DOC_GEN_DIR) -c $(SRC_DOC_DIR)
-IN_VENV:=$(shell if pip -V | grep "venv" 2>&1 > /dev/null; then echo 1; else echo 0; fi)
-
-.PHONY: verify-virtualenv
-verify-virtualenv:
-ifndef TEST_DIR
-	$(error TEST_DIR is not set)
-endif
-ifndef TEST_DOC_BR
-	$(error TEST_DOC_BR is not set)
-endif
-ifeq ($(IN_VENV),0)
-	$(error "Not running inside virtualenv (are you running 'make test-doc' from root?)")
-endif
-
-.PHONY: regen-api-doc
-regen-api-doc: verify-virtualenv
-	@mkdir -p $(API_DOC_GEN_DIR)
-	@cp $(SRC_DOC_DIR)/index.rst $(API_DOC_GEN_DIR)
-	@cp $(SRC_DOC_DIR)/indices.rst $(API_DOC_GEN_DIR)
-	@cp $(SRC_DOC_DIR)/overview.rst $(API_DOC_GEN_DIR)
-	sphinx-apidoc -o $(API_DOC_GEN_DIR) -H "Module documentation" $(TEST_DIR)
-
-.PHONY: html
-html: regen-api-doc verify-virtualenv
-	$(SPHINXBUILD) -b html $(ALLSPHINXOPTS) $(HTML_DOC_GEN_DIR)

commit 0968c08e20d39e12311e4d7102be76455d74d762

tests docs: fix jsonschema dependency
- docs requires jsonschema which is only supported
  on python 3.7 or newer.  This causes 'make test'
  to fail on Ubuntu 18.04

Type: fix
Fixes: 566ded4b3

Change-Id: I0935c569ac102ea1dba6112f458e6ee10330e474
Signed-off-by: Dave Wallace <dwallacelf@gmail.com>

Makefile
@@ -1,4 +1,4 @@
-# Copyright (c) 2016 Cisco and/or its affiliates.
+# Copyright (c) 2021 Cisco and/or its affiliates.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at:
@@ -452,7 +452,7 @@ test-dep:
 
 .PHONY: test-doc
 test-doc:
-	@echo "make test-doc is DEPRECATED: use 'make doc'"
+	@echo "make test-doc is DEPRECATED: use 'make docs'"
 	sleep 300
 
 .PHONY: test-wipe-doc

docs/Makefile
@@ -1,4 +1,15 @@
-# Minimal makefile for Sphinx documentation
+# Copyright (c) 2021 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 # We support MacOS for docs generation
 ifeq ($(shell uname),Darwin)
@@ -20,6 +31,10 @@ PIP_VERSION ?= $(shell grep 'PIP_VERSION=' ${WS_ROOT}/test/Makefile | cut -d'=' 
 PIP_TOOLS_VERSION ?= $(shell grep 'PIP_TOOLS_VERSION=' ${WS_ROOT}/test/Makefile | cut -d'=' -f2)
 
 PYTHON ?= "python3"
+PYTHON_VERSION_OK := $(shell $(PYTHON) -c "exec('import sys\nif sys.hexversion >= 0x03070000: print(\"true\")\nelse: print(\"false\")')")
+ifneq ($(PYTHON_VERSION_OK),true)
+$(error "ERROR: docs build requires python version >= to 3.7")
+endif
 
 # You can set these variables from the command line.
 SPHINXOPTS        = --keep-going -n -W
@@ -96,4 +111,3 @@ clean:
 
 .PHONY: build
 build: docs
-