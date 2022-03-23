alisherqodirjonov/vpp

commit 79339514b2af4ad7f550c67eb5eac8dc417c52d1

vlib: vpp banner is outputted to non-interactive vppctl session
Running a batch file which contains many vppctl commands, occasionally,
VPP may spit out the banner for some of the commands. This happens
when VPP erroneously views the vppctl session as interactive.

A simple way to recreate the problem is to run a batch script as followed
while [ 1 ]
do
     vppctl create loopback interface
     vppctl delete loopback interface intfc loop0
done

We have two processes which may display the banner,
unix_cli_new_session_process and unix_cli_process. Normally,
unix_cli_process parses the input tokens and displays the banner after
it negotiates the terminal type with the vppctl app.
unix_cli_new_session_process only displays the banner just in case the
client fails to negotiate terminal type. It runs on a timer and expires
in 1 second to display the banner if by then the terminal type is still
not yet negotiated.

The problem is when the session is killed or exitted, VPP does not remove
the element that was enqueued for cli_new_session_process. The index
for the connection (cf) is recycled. The timer for the queue element
continues to run. When the timer expires for the queue element, it
finds the wrong new session due to index recycling. If the new
session has not had negotiated the terminal type, the banner is printed
erroneously to the new session from cli_new_session_process.

The fix is to clean up the queue element to stop cli_new_session_process
from processing the wrong connection when the session is killed.

Type: fix

Signed-off-by: Steven Luong <sluong@cisco.com>
Change-Id: Ife2f1b1c95661e442f0fc6b73505e330e6641fc1

src/vlib/unix/cli.c
@@ -2687,6 +2687,17 @@ unix_cli_kill (unix_cli_main_t * cm, uword cli_file_index)
   if (pool_is_free_index (cm->cli_file_pool, cli_file_index))
     return;
 
+  vec_foreach_index (i, cm->new_sessions)
+    {
+      unix_cli_new_session_t *ns = vec_elt_at_index (cm->new_sessions, i);
+
+      if (ns->cf_index == cli_file_index)
+	{
+	  vec_del1 (cm->new_sessions, i);
+	  break;
+	}
+    }
+
   cf = pool_elt_at_index (cm->cli_file_pool, cli_file_index);
   uf = pool_elt_at_index (fm->file_pool, cf->clib_file_index);
 

commit 980083310f6c4b47807df64694e0668023f66aed

vlib: mitigate outdated new cli session events
Possible races while concurrent additon to the new sessions vector
in a one process and remove from it in an another need to be avoided.
Let the vector be changed in the new session process function only.
Also cli_file_pool element may be freed already at the new session
event arrive timepoint, still causing unexpected cli banner for
noninteracive cli sessions.

Type: fix
Fixes: 79339514b2af4ad7f550c67eb5eac8dc417c52d1
Signed-off-by: Vladislav Grishenko <themiron@yandex-team.ru>
Change-Id: I022d16dd3aad9c9330834d35c58938f04b015b08

src/vlib/unix/cli.c
@@ -1312,6 +1312,10 @@ unix_cli_new_session_process (vlib_main_t * vm, vlib_node_runtime_t * rt,
 	    /* Add an identifier to the new session list */
 	    unix_cli_new_session_t ns;
 
+	    /* Check the connection didn't close already */
+	    if (pool_is_free_index (cm->cli_file_pool, event_data[0]))
+	      break;
+
 	    ns.cf_index = event_data[0];
 	    ns.deadline = vlib_time_now (vm) + 1.0;
 
@@ -2693,7 +2697,7 @@ unix_cli_kill (unix_cli_main_t * cm, uword cli_file_index)
 
       if (ns->cf_index == cli_file_index)
 	{
-	  vec_del1 (cm->new_sessions, i);
+	  ns->cf_index = ~0;
 	  break;
 	}
     }