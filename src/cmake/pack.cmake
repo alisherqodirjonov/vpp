alisherqodirjonov/vpp

commit a504bf466b11b2574375e28d8585d5750e48d1ff

build: set OS_ID_LIKE on to be OS_ID on Debian proper
OS_ID_LIKE from /etc/os-release is used to determine
the type of packaging. On Debian it ends up being empty,
so the "ninja package" does not work for out of tree plugin
builds.

Solution: if OS_ID_LIKE is not set, set it to OS_ID

Type: fix
Signed-off-by: Andrew Yourtchenko <ayourtch@gmail.com>
Change-Id: I077d4fdb509ea94b187d11391b1f49edb94c4e30
Signed-off-by: Andrew Yourtchenko <ayourtch@gmail.com>

src/cmake/pack.cmake
@@ -64,6 +64,11 @@ macro(add_vpp_packaging)
   set(CPACK_${type}_PACKAGE_DESCRIPTION "${ARG_DESCRIPTION}")
   set(CPACK_${type}_PACKAGE_RELEASE 1)
 
+  # Pure Debian does not set the "OS_ID_LIKE", it only sets "OS_ID"
+  if (OS_ID_LIKE MATCHES "")
+    set(OS_ID_LIKE "${OS_ID}")
+  endif()
+
   if(OS_ID_LIKE MATCHES "debian")
     set(CPACK_GENERATOR "DEB")
     set(type "DEBIAN")

commit 42cf6864f5ee9001ddefde7cc5d34ff208c4f216

build: set OS_ID_LIKE only if unset
cmake MATCHES directive with the empty regex "" always match, including
non-empty strings.

Type: fix
Fixes: a504bf466b11b2574375e28d8585d5750e48d1ff

Change-Id: If085b29da15a6d7fc680cebb823183fd3c7eea68
Signed-off-by: Benoît Ganne <bganne@cisco.com>

src/cmake/pack.cmake
@@ -65,7 +65,7 @@ macro(add_vpp_packaging)
   set(CPACK_${type}_PACKAGE_RELEASE 1)
 
   # Pure Debian does not set the "OS_ID_LIKE", it only sets "OS_ID"
-  if (OS_ID_LIKE MATCHES "")
+  if (NOT DEFINED OS_ID_LIKE)
     set(OS_ID_LIKE "${OS_ID}")
   endif()
 