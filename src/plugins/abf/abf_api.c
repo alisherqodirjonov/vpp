alisherqodirjonov/vpp

commit cee705dc5e710f848adde1280ef64468c18ff0de

ACL based forwarding
A poor man's flow switching or policy based rounting.

An ACL is used to match packets and is associated with a [set of] forwarding paths
that determine how to forward matched packets - collectively this association is a
'policy'.
Policies are then 'attached', in a priority order, to an interface when thaey are
encountered as an input feature. If a packet matches no policies it is forwarded
normally in the IP FIB.

This commit is used to test the "ACL-as-a-service" functionality,
which currently compiles, and the existing traffic ACL tests pass in both hash and linear modes.

Change-Id: I0b274ec9f2e645352fa898b43eb54c457e195964
Signed-off-by: Neale Ranns <nranns@cisco.com>
Signed-off-by: Andrew Yourtchenko <ayourtch@gmail.com>
Signed-off-by: Ole Troan <ot@cisco.com>

src/plugins/abf/abf.api
@@ -1,0 +1,131 @@
+/* Hey Emacs use -*- mode: C -*- */
+/*
+ * Copyright (c) 2016 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/** \file
+    This file defines the vpp control-plane API messages
+    used to control the ABF plugin
+*/
+
+option version = "1.0.0";
+import "vnet/fib/fib_types.api";
+
+/** \brief Get the plugin version
+    @param client_index - opaque cookie to identify the sender
+    @param context - sender context, to match reply w/ request
+*/
+define abf_plugin_get_version
+{
+  u32 client_index;
+  u32 context;
+};
+
+/** \brief Reply to get the plugin version
+    @param context - returned sender context, to match reply w/ request
+    @param major - Incremented every time a known breaking behavior change is introduced
+    @param minor - Incremented with small changes, may be used to avoid buggy versions
+*/
+define abf_plugin_get_version_reply
+{
+  u32 context;
+  u32 major;
+  u32 minor;
+};
+
+/** \brief A description of an ABF policy
+    @param policy_id User chosen Identifier for the policy
+    @param acl_index The ACL that the policy will match against
+    @param n_paths Number of paths
+    @param paths The set of forwarding paths that are being added or removed.
+ */
+typeonly define abf_policy
+{
+  u32 policy_id;
+  u32 acl_index;
+  u8 n_paths;
+  vl_api_fib_path_t paths[n_paths];
+};
+
+/** \brief A description of an ABF policy
+    @param is_add Is this the addition or removal of paths from the policy
+           If the policy does not exist it is created. If the last path
+           Is being removed, the policy is deleted
+    @param policy The policy
+ */
+autoreply define abf_policy_add_del
+{
+  u32 client_index;
+  u32 context;
+  u8 is_add;
+  vl_api_abf_policy_t policy;
+};
+
+/** \brief Policy description returned in the dump
+ */
+define abf_policy_details
+{
+  u32 context;
+  vl_api_abf_policy_t policy;
+};
+
+/** \brief Dump all ABF policies
+ */
+define abf_policy_dump
+{
+  u32 client_index;
+  u32 context;
+};
+
+/** \brief A description of a policy attachment to an interface
+    @param The policy ID to attach
+    @param sw_if_index The interface to attach to
+    @param priority The priority of the attachment, w.r.t. to other attachments
+              on this interface. lower value is 'better'
+    @param is_ipv6 Does this attachment apply to IPv6 packets (or IPv4)
+*/
+typeonly define abf_itf_attach
+{
+  u32 policy_id;
+  u32 sw_if_index;
+  u32 priority;
+  u8 is_ipv6;
+};
+
+/** \brief Add or delete a policy attachment to an interface
+ */
+autoreply define abf_itf_attach_add_del
+{
+  u32 client_index;
+  u32 context;
+  u8 is_add;
+  vl_api_abf_itf_attach_t attach;
+};
+
+/** \brief Attachment details from a dump
+ */
+define abf_itf_attach_details
+{
+  u32 context;
+  vl_api_abf_itf_attach_t attach;
+};
+
+/** \brief Dump all the policy attachments
+ */
+define abf_itf_attach_dump
+{
+  u32 client_index;
+  u32 context;
+};
+

src/plugins/abf/abf_api.c
@@ -1,0 +1,327 @@
+/*
+ * Copyright (c) 2016 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stddef.h>
+
+#include <vnet/vnet.h>
+#include <vnet/plugin/plugin.h>
+#include <abf/abf_policy.h>
+#include <abf/abf_itf_attach.h>
+#include <vnet/mpls/mpls_types.h>
+#include <vnet/fib/fib_path_list.h>
+#include <vnet/fib/fib_api.h>
+
+#include <vpp/app/version.h>
+
+#include <vlibapi/api.h>
+#include <vlibmemory/api.h>
+
+/* define message IDs */
+#include <abf/abf_msg_enum.h>
+
+/* define message structures */
+#define vl_typedefs
+#include <abf/abf_all_api_h.h>
+#undef vl_typedefs
+
+/* define generated endian-swappers */
+#define vl_endianfun
+#include <abf/abf_all_api_h.h>
+#undef vl_endianfun
+
+/* instantiate all the print functions we know about */
+#define vl_print(handle, ...) vlib_cli_output (handle, __VA_ARGS__)
+#define vl_printfun
+#include <abf/abf_all_api_h.h>
+#undef vl_printfun
+
+/* Get the API version number */
+#define vl_api_version(n,v) static u32 api_version=(v);
+#include <abf/abf_all_api_h.h>
+#undef vl_api_version
+
+/**
+ * Base message ID fot the plugin
+ */
+static u32 abf_base_msg_id;
+
+#include <vlibapi/api_helper_macros.h>
+
+/* List of message types that this plugin understands */
+
+#define foreach_abf_plugin_api_msg                    \
+_(ABF_PLUGIN_GET_VERSION, abf_plugin_get_version)     \
+_(ABF_POLICY_ADD_DEL, abf_policy_add_del)             \
+_(ABF_POLICY_DUMP, abf_policy_dump)                   \
+_(ABF_ITF_ATTACH_ADD_DEL, abf_itf_attach_add_del)     \
+_(ABF_ITF_ATTACH_DUMP, abf_itf_attach_dump)
+
+static void
+vl_api_abf_plugin_get_version_t_handler (vl_api_abf_plugin_get_version_t * mp)
+{
+  vl_api_abf_plugin_get_version_reply_t *rmp;
+  int msg_size = sizeof (*rmp);
+  unix_shared_memory_queue_t *q;
+
+  q = vl_api_client_index_to_input_queue (mp->client_index);
+  if (q == 0)
+    {
+      return;
+    }
+
+  rmp = vl_msg_api_alloc (msg_size);
+  memset (rmp, 0, msg_size);
+  rmp->_vl_msg_id =
+    ntohs (VL_API_ABF_PLUGIN_GET_VERSION_REPLY + abf_base_msg_id);
+  rmp->context = mp->context;
+  rmp->major = htonl (ABF_PLUGIN_VERSION_MAJOR);
+  rmp->minor = htonl (ABF_PLUGIN_VERSION_MINOR);
+
+  vl_msg_api_send_shmem (q, (u8 *) & rmp);
+}
+
+static void
+vl_api_abf_policy_add_del_t_handler (vl_api_abf_policy_add_del_t * mp)
+{
+  vl_api_abf_policy_add_del_reply_t *rmp;
+  fib_route_path_t *paths = NULL, *path;
+  int rv = 0;
+  u8 pi;
+
+  vec_validate (paths, mp->policy.n_paths - 1);
+
+  for (pi = 0; pi < mp->policy.n_paths; pi++)
+    {
+      path = &paths[pi];
+      rv = fib_path_api_parse (&mp->policy.paths[pi], path);
+
+      if (0 != rv)
+	{
+	  goto done;
+	}
+    }
+
+  if (mp->is_add)
+    {
+      abf_policy_update (ntohl (mp->policy.policy_id),
+			 ntohl (mp->policy.acl_index), paths);
+    }
+  else
+    {
+      abf_policy_delete (ntohl (mp->policy.policy_id), paths);
+    }
+done:
+  vec_free (paths);
+
+  REPLY_MACRO (VL_API_ABF_POLICY_ADD_DEL_REPLY + abf_base_msg_id);
+}
+
+static void
+vl_api_abf_itf_attach_add_del_t_handler (vl_api_abf_itf_attach_add_del_t * mp)
+{
+  vl_api_abf_itf_attach_add_del_reply_t *rmp;
+  fib_protocol_t fproto = (mp->attach.is_ipv6 ?
+			   FIB_PROTOCOL_IP6 : FIB_PROTOCOL_IP4);
+  int rv = 0;
+
+  if (mp->is_add)
+    {
+      abf_itf_attach (fproto,
+		      ntohl (mp->attach.policy_id),
+		      ntohl (mp->attach.priority),
+		      ntohl (mp->attach.sw_if_index));
+    }
+  else
+    {
+      abf_itf_detach (fproto,
+		      ntohl (mp->attach.policy_id),
+		      ntohl (mp->attach.sw_if_index));
+    }
+
+  REPLY_MACRO (VL_API_ABF_ITF_ATTACH_ADD_DEL_REPLY + abf_base_msg_id);
+}
+
+typedef struct abf_dump_walk_ctx_t_
+{
+  unix_shared_memory_queue_t *q;
+  u32 context;
+} abf_dump_walk_ctx_t;
+
+static int
+abf_policy_send_details (u32 api, void *args)
+{
+  fib_route_path_encode_t *api_rpaths = NULL, *api_rpath;
+  vl_api_abf_policy_details_t *mp;
+  abf_dump_walk_ctx_t *ctx;
+  vl_api_fib_path_t *fp;
+  size_t msg_size;
+  abf_policy_t *ap;
+  u8 n_paths;
+
+  ctx = args;
+  ap = abf_policy_get (api);
+  n_paths = fib_path_list_get_n_paths (ap->ap_pl);
+  msg_size = sizeof (*mp) + sizeof (mp->policy.paths[0]) * n_paths;
+
+  mp = vl_msg_api_alloc (msg_size);
+  memset (mp, 0, msg_size);
+  mp->_vl_msg_id = ntohs (VL_API_ABF_POLICY_DETAILS + abf_base_msg_id);
+
+  /* fill in the message */
+  mp->context = ctx->context;
+  mp->policy.n_paths = n_paths;
+  mp->policy.acl_index = htonl (ap->ap_acl);
+  mp->policy.policy_id = htonl (ap->ap_id);
+
+  fib_path_list_walk (ap->ap_pl, fib_path_encode, &api_rpaths);
+
+  fp = mp->policy.paths;
+  vec_foreach (api_rpath, api_rpaths)
+  {
+    fib_api_path_encode (api_rpath, fp);
+    fp++;
+  }
+
+  vl_msg_api_send_shmem (ctx->q, (u8 *) & mp);
+
+  return (1);
+}
+
+static void
+vl_api_abf_policy_dump_t_handler (vl_api_abf_policy_dump_t * mp)
+{
+  unix_shared_memory_queue_t *q;
+
+  q = vl_api_client_index_to_input_queue (mp->client_index);
+  if (q == 0)
+    {
+      return;
+    }
+
+  abf_dump_walk_ctx_t ctx = {
+    .q = q,
+    .context = mp->context,
+  };
+
+  abf_policy_walk (abf_policy_send_details, &ctx);
+}
+
+static int
+abf_itf_attach_send_details (u32 aiai, void *args)
+{
+  vl_api_abf_itf_attach_details_t *mp;
+  abf_dump_walk_ctx_t *ctx;
+  abf_itf_attach_t *aia;
+  abf_policy_t *ap;
+
+  ctx = args;
+  aia = abf_itf_attach_get (aiai);
+  ap = abf_policy_get (aia->aia_abf);
+
+  mp = vl_msg_api_alloc (sizeof (*mp));
+  mp->_vl_msg_id = ntohs (VL_API_ABF_ITF_ATTACH_DETAILS + abf_base_msg_id);
+
+  mp->context = ctx->context;
+  mp->attach.policy_id = htonl (ap->ap_id);
+  mp->attach.sw_if_index = htonl (aia->aia_sw_if_index);
+  mp->attach.priority = htonl (aia->aia_prio);
+  mp->attach.is_ipv6 = (aia->aia_proto == FIB_PROTOCOL_IP6);
+
+  vl_msg_api_send_shmem (ctx->q, (u8 *) & mp);
+
+  return (1);
+}
+
+static void
+vl_api_abf_itf_attach_dump_t_handler (vl_api_abf_itf_attach_dump_t * mp)
+{
+  unix_shared_memory_queue_t *q;
+
+  q = vl_api_client_index_to_input_queue (mp->client_index);
+  if (q == 0)
+    {
+      return;
+    }
+
+  abf_dump_walk_ctx_t ctx = {
+    .q = q,
+    .context = mp->context,
+  };
+
+  abf_itf_attach_walk (abf_itf_attach_send_details, &ctx);
+}
+
+#define vl_msg_name_crc_list
+#include <abf/abf_all_api_h.h>
+#undef vl_msg_name_crc_list
+
+/* Set up the API message handling tables */
+static clib_error_t *
+abf_plugin_api_hookup (vlib_main_t * vm)
+{
+#define _(N,n)                                                  \
+    vl_msg_api_set_handlers((VL_API_##N + abf_base_msg_id),     \
+                            #n,					\
+                            vl_api_##n##_t_handler,             \
+                            vl_noop_handler,                    \
+                            vl_api_##n##_t_endian,              \
+                            vl_api_##n##_t_print,               \
+                            sizeof(vl_api_##n##_t), 1);
+  foreach_abf_plugin_api_msg;
+#undef _
+
+  return 0;
+}
+
+static void
+setup_message_id_table (api_main_t * apim)
+{
+#define _(id,n,crc) \
+  vl_msg_api_add_msg_name_crc (apim, #n "_" #crc, id + abf_base_msg_id);
+  foreach_vl_msg_name_crc_abf;
+#undef _
+}
+
+static clib_error_t *
+abf_api_init (vlib_main_t * vm)
+{
+  clib_error_t *error = 0;
+
+  u8 *name = format (0, "abf_%08x%c", api_version, 0);
+
+  /* Ask for a correctly-sized block of API message decode slots */
+  abf_base_msg_id = vl_msg_api_get_msg_ids ((char *) name,
+					    VL_MSG_FIRST_AVAILABLE);
+
+  error = abf_plugin_api_hookup (vm);
+
+  /* Add our API messages to the global name_crc hash table */
+  setup_message_id_table (&api_main);
+
+  vec_free (name);
+
+  return error;
+}
+
+VLIB_INIT_FUNCTION (abf_api_init);
+
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */

src/plugins/abf/abf_policy.c
@@ -1,0 +1,464 @@
+/*
+ * Copyright (c) 2017 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <plugins/abf/abf_policy.h>
+
+#include <vlib/vlib.h>
+#include <vnet/plugin/plugin.h>
+#include <vpp/app/version.h>
+#include <vnet/fib/fib_path_list.h>
+#include <vnet/fib/fib_walk.h>
+
+/**
+ * FIB node type the attachment is registered
+ */
+fib_node_type_t abf_policy_fib_node_type;
+
+/**
+ * Pool of ABF objects
+ */
+static abf_policy_t *abf_policy_pool;
+
+/**
+ * DB of ABF policy objects
+ *  - policy ID to index conversion.
+ */
+static uword *abf_policy_db;
+
+
+abf_policy_t *
+abf_policy_get (u32 index)
+{
+  return (pool_elt_at_index (abf_policy_pool, index));
+}
+
+static u32
+abf_policy_get_index (const abf_policy_t * abf)
+{
+  return (abf - abf_policy_pool);
+}
+
+static abf_policy_t *
+abf_policy_find_i (u32 policy_id)
+{
+  u32 api;
+
+  api = abf_policy_find (policy_id);
+
+  if (INDEX_INVALID != api)
+    return (abf_policy_get (api));
+
+  return (NULL);
+}
+
+u32
+abf_policy_find (u32 policy_id)
+{
+  uword *p;
+
+  p = hash_get (abf_policy_db, policy_id);
+
+  if (NULL != p)
+    return (p[0]);
+
+  return (INDEX_INVALID);
+}
+
+
+void
+abf_policy_update (u32 policy_id,
+		   u32 acl_index, const fib_route_path_t * rpaths)
+{
+  abf_policy_t *ap;
+  u32 api;
+
+  api = abf_policy_find (policy_id);
+
+  if (INDEX_INVALID == api)
+    {
+      /*
+       * create a new policy
+       */
+      pool_get (abf_policy_pool, ap);
+
+      api = ap - abf_policy_pool;
+      fib_node_init (&ap->ap_node, abf_policy_fib_node_type);
+      ap->ap_acl = acl_index;
+      ap->ap_id = policy_id;
+      ap->ap_pl = fib_path_list_create ((FIB_PATH_LIST_FLAG_SHARED |
+					 FIB_PATH_LIST_FLAG_NO_URPF), rpaths);
+
+      /*
+       * become a child of the path list so we get poked when
+       * the forwarding changes.
+       */
+      ap->ap_sibling = fib_path_list_child_add (ap->ap_pl,
+						abf_policy_fib_node_type,
+						api);
+
+      /*
+       * add this new policy to the DB
+       */
+      hash_set (abf_policy_db, policy_id, api);
+
+      /*
+       * take a lock on behalf of the CLI/API creation
+       */
+      fib_node_lock (&ap->ap_node);
+    }
+  else
+    {
+      /*
+       * update an existing policy.
+       * - add the path to the path-list and swap our ancestory
+       * - backwalk to poke all attachments to update
+       */
+      fib_node_index_t old_pl;
+
+      ap = abf_policy_get (api);
+      old_pl = ap->ap_pl;
+
+      if (FIB_NODE_INDEX_INVALID != old_pl)
+	{
+	  ap->ap_pl = fib_path_list_copy_and_path_add (old_pl,
+						       (FIB_PATH_LIST_FLAG_SHARED
+							|
+							FIB_PATH_LIST_FLAG_NO_URPF),
+						       rpaths);
+	  fib_path_list_child_remove (old_pl, ap->ap_sibling);
+	}
+      else
+	{
+	  ap->ap_pl = fib_path_list_create ((FIB_PATH_LIST_FLAG_SHARED |
+					     FIB_PATH_LIST_FLAG_NO_URPF),
+					    rpaths);
+	}
+
+      ap->ap_sibling = fib_path_list_child_add (ap->ap_pl,
+						abf_policy_fib_node_type,
+						api);
+
+      fib_node_back_walk_ctx_t ctx = {
+	.fnbw_reason = FIB_NODE_BW_REASON_FLAG_EVALUATE,
+      };
+
+      fib_walk_sync (abf_policy_fib_node_type, api, &ctx);
+    }
+}
+
+static void
+abf_policy_destroy (abf_policy_t * ap)
+{
+  /*
+   * this ABF should not be a sibling on the path list, since
+   * that was removed when the API config went
+   */
+  ASSERT (ap->ap_sibling == ~0);
+  ASSERT (ap->ap_pl == FIB_NODE_INDEX_INVALID);
+
+  hash_unset (abf_policy_db, ap->ap_id);
+  pool_put (abf_policy_pool, ap);
+}
+
+int
+abf_policy_delete (u32 policy_id, const fib_route_path_t * rpaths)
+{
+  abf_policy_t *ap;
+  u32 api;
+
+  api = abf_policy_find (policy_id);
+
+  if (INDEX_INVALID == api)
+    {
+      /*
+       * no such policy
+       */
+      return (-1);
+    }
+  else
+    {
+      /*
+       * update an existing policy.
+       * - add the path to the path-list and swap our ancestory
+       * - backwalk to poke all attachments to update
+       */
+      fib_node_index_t old_pl;
+
+      ap = abf_policy_get (api);
+      old_pl = ap->ap_pl;
+
+      ap->ap_pl =
+	fib_path_list_copy_and_path_remove (ap->ap_pl,
+					    (FIB_PATH_LIST_FLAG_SHARED |
+					     FIB_PATH_LIST_FLAG_NO_URPF),
+					    rpaths);
+
+      fib_path_list_child_remove (old_pl, ap->ap_sibling);
+      ap->ap_sibling = ~0;
+
+      if (FIB_NODE_INDEX_INVALID == ap->ap_pl)
+	{
+	  /*
+	   * no more paths on this policy. It's toast
+	   * remove the CLI/API's lock
+	   */
+	  fib_node_unlock (&ap->ap_node);
+	}
+      else
+	{
+	  ap->ap_sibling = fib_path_list_child_add (ap->ap_pl,
+						    abf_policy_fib_node_type,
+						    api);
+
+	  fib_node_back_walk_ctx_t ctx = {
+	    .fnbw_reason = FIB_NODE_BW_REASON_FLAG_EVALUATE,
+	  };
+
+	  fib_walk_sync (abf_policy_fib_node_type, api, &ctx);
+	}
+    }
+
+  return (0);
+}
+
+static clib_error_t *
+abf_policy_cmd (vlib_main_t * vm,
+		unformat_input_t * main_input, vlib_cli_command_t * cmd)
+{
+  unformat_input_t _line_input, *line_input = &_line_input;
+  u32 acl_index, policy_id;
+  fib_route_path_t *rpaths = NULL, rpath;
+  u32 is_del;
+
+  is_del = 0;
+  acl_index = INDEX_INVALID;
+  policy_id = INDEX_INVALID;
+
+  /* Get a line of input. */
+  if (!unformat_user (main_input, unformat_line_input, line_input))
+    return 0;
+
+  while (unformat_check_input (line_input) != UNFORMAT_END_OF_INPUT)
+    {
+      if (unformat (line_input, "acl %d", &acl_index))
+	;
+      else if (unformat (line_input, "id %d", &policy_id))
+	;
+      else if (unformat (line_input, "del"))
+	is_del = 1;
+      else if (unformat (line_input, "add"))
+	is_del = 0;
+      else if (unformat (line_input, "via %U",
+			 unformat_fib_route_path, &rpath))
+	vec_add1 (rpaths, rpath);
+      else
+	return (clib_error_return (0, "unknown input '%U'",
+				   format_unformat_error, line_input));
+    }
+
+  if (INDEX_INVALID == policy_id)
+    {
+      vlib_cli_output (vm, "Specify a Policy ID");
+      return 0;
+    }
+
+  if (!is_del)
+    {
+      if (INDEX_INVALID == acl_index)
+	{
+	  vlib_cli_output (vm, "ACL index must be set");
+	  return 0;
+	}
+
+      abf_policy_update (policy_id, acl_index, rpaths);
+    }
+  else
+    {
+      abf_policy_delete (policy_id, rpaths);
+    }
+
+  unformat_free (line_input);
+  return (NULL);
+}
+
+/* *INDENT-OFF* */
+/**
+ * Create an ABF policy.
+ */
+VLIB_CLI_COMMAND (abf_policy_cmd_node, static) = {
+  .path = "abf policy",
+  .function = abf_policy_cmd,
+  .short_help = "abf policy [add|del] id <index> acl <index> via ...",
+  .is_mp_safe = 1,
+};
+/* *INDENT-ON* */
+
+static u8 *
+format_abf (u8 * s, va_list * args)
+{
+  abf_policy_t *ap = va_arg (*args, abf_policy_t *);
+
+  s = format (s, "abf:[%d]: policy:%d acl:%d",
+	      ap - abf_policy_pool, ap->ap_id, ap->ap_acl);
+  s = format (s, "\n ");
+  if (FIB_NODE_INDEX_INVALID == ap->ap_pl)
+    {
+      s = format (s, "no forwarding");
+    }
+  else
+    {
+      s = fib_path_list_format (ap->ap_pl, s);
+    }
+
+  return (s);
+}
+
+void
+abf_policy_walk (abf_policy_walk_cb_t cb, void *ctx)
+{
+  u32 api;
+
+  /* *INDENT-OFF* */
+  pool_foreach_index(api, abf_policy_pool,
+  ({
+    if (!cb(api, ctx))
+      break;
+  }));
+  /* *INDENT-ON* */
+}
+
+static clib_error_t *
+abf_show_policy_cmd (vlib_main_t * vm,
+		     unformat_input_t * input, vlib_cli_command_t * cmd)
+{
+  u32 policy_id;
+  abf_policy_t *ap;
+
+  policy_id = INDEX_INVALID;
+
+  while (unformat_check_input (input) != UNFORMAT_END_OF_INPUT)
+    {
+      if (unformat (input, "%d", &policy_id))
+	;
+      else
+	return (clib_error_return (0, "unknown input '%U'",
+				   format_unformat_error, input));
+    }
+
+  if (INDEX_INVALID == policy_id)
+    {
+      /* *INDENT-OFF* */
+      pool_foreach(ap, abf_policy_pool,
+      ({
+        vlib_cli_output(vm, "%U", format_abf, ap);
+      }));
+      /* *INDENT-ON* */
+    }
+  else
+    {
+      ap = abf_policy_find_i (policy_id);
+
+      if (NULL != ap)
+	vlib_cli_output (vm, "%U", format_abf, ap);
+      else
+	vlib_cli_output (vm, "Invalid policy ID:%d", policy_id);
+    }
+
+  return (NULL);
+}
+
+/* *INDENT-OFF* */
+VLIB_CLI_COMMAND (abf_policy_show_policy_cmd_node, static) = {
+  .path = "show abf policy",
+  .function = abf_show_policy_cmd,
+  .short_help = "show abf policy <value>",
+  .is_mp_safe = 1,
+};
+/* *INDENT-ON* */
+
+static fib_node_t *
+abf_policy_get_node (fib_node_index_t index)
+{
+  abf_policy_t *ap = abf_policy_get (index);
+  return (&(ap->ap_node));
+}
+
+static abf_policy_t *
+abf_policy_get_from_node (fib_node_t * node)
+{
+  return ((abf_policy_t *) (((char *) node) -
+			    STRUCT_OFFSET_OF (abf_policy_t, ap_node)));
+}
+
+static void
+abf_policy_last_lock_gone (fib_node_t * node)
+{
+  abf_policy_destroy (abf_policy_get_from_node (node));
+}
+
+/*
+ * A back walk has reached this ABF policy
+ */
+static fib_node_back_walk_rc_t
+abf_policy_back_walk_notify (fib_node_t * node,
+			     fib_node_back_walk_ctx_t * ctx)
+{
+  /*
+   * re-stack the fmask on the n-eos of the via
+   */
+  abf_policy_t *abf = abf_policy_get_from_node (node);
+
+  /*
+   * propagate further up the graph.
+   * we can do this synchronously since the fan out is small.
+   */
+  fib_walk_sync (abf_policy_fib_node_type, abf_policy_get_index (abf), ctx);
+
+  return (FIB_NODE_BACK_WALK_CONTINUE);
+}
+
+/*
+ * The BIER fmask's graph node virtual function table
+ */
+static const fib_node_vft_t abf_policy_vft = {
+  .fnv_get = abf_policy_get_node,
+  .fnv_last_lock = abf_policy_last_lock_gone,
+  .fnv_back_walk = abf_policy_back_walk_notify,
+};
+
+static clib_error_t *
+abf_policy_init (vlib_main_t * vm)
+{
+  abf_policy_fib_node_type = fib_node_register_new_type (&abf_policy_vft);
+
+  return (NULL);
+}
+
+VLIB_INIT_FUNCTION (abf_policy_init);
+
+/* *INDENT-OFF* */
+VLIB_PLUGIN_REGISTER () = {
+    .version = VPP_BUILD_VER,
+    .description = "ACL based Forwarding",
+};
+/* *INDENT-ON* */
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */

src/configure.ac
@@ -218,6 +218,7 @@ AC_SUBST(AR_FLAGS)
 ###############################################################################
 
 # Please keep alphabetical order
+PLUGIN_ENABLED(abf)
 PLUGIN_ENABLED(acl)
 PLUGIN_ENABLED(avf)
 PLUGIN_ENABLED(cdp)

src/plugins/Makefile.am
@@ -30,6 +30,10 @@ nobase_include_HEADERS =
 vppapitestpluginsdir = ${libdir}/vpp_api_test_plugins
 vpppluginsdir = ${libdir}/vpp_plugins
 
+if ENABLE_ABF_PLUGIN
+include abf.am
+endif
+
 if ENABLE_ACL_PLUGIN
 include acl.am
 endif

src/plugins/abf.am
@@ -1,0 +1,28 @@
+# Copyright (c) 2016 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+vppplugins_LTLIBRARIES += abf_plugin.la
+
+abf_plugin_la_SOURCES =				\
+	abf/abf_policy.c			\
+	abf/abf_api.c				\
+	abf/abf_itf_attach.c
+
+API_FILES += abf/abf.api
+
+nobase_apiinclude_HEADERS +=			\
+  abf/abf_all_api_h.h				\
+  abf/abf_msg_enum.h				\
+  abf/abf.api.h
+
+# vi:syntax=automake

commit 0912848068960b70e91060f33ca17043944114fa

abf: add API parameter n_paths range checks
Also check for non-zero rpath length in CLI cmd.
While there, no need to use "else" after a return.
Also while there, notice and fix numerous input_line
buffer leaks and fix them.

Type: fix
Fixes: cee705dc5e710f848adde1280ef64468c18ff0de

Signed-off-by: Jon Loeliger <jdl@netgate.com>
Change-Id: I18ea44b7b82e8938c3e793e7c2a04dfe157076d8

src/plugins/abf/abf.api
@@ -51,7 +51,7 @@ define abf_plugin_get_version_reply
 /** \brief A description of an ABF policy
     @param policy_id User chosen Identifier for the policy
     @param acl_index The ACL that the policy will match against
-    @param n_paths Number of paths
+    @param n_paths Number of paths, 1..255
     @param paths The set of forwarding paths that are being added or removed.
  */
 typedef abf_policy

src/plugins/abf/abf_api.c
@@ -69,6 +69,12 @@ vl_api_abf_policy_add_del_t_handler (vl_api_abf_policy_add_del_t * mp)
   int rv = 0;
   u8 pi;
 
+  if (mp->policy.n_paths == 0)
+    {
+      rv = VNET_API_ERROR_INVALID_VALUE;
+      goto done;
+    }
+
   vec_validate (paths, mp->policy.n_paths - 1);
 
   for (pi = 0; pi < mp->policy.n_paths; pi++)

src/plugins/abf/abf_policy.c
@@ -192,50 +192,45 @@ abf_policy_delete (u32 policy_id, const fib_route_path_t * rpaths)
        */
       return (VNET_API_ERROR_INVALID_VALUE);
     }
-  else
+
+  /*
+   * update an existing policy.
+   * - add the path to the path-list and swap our ancestry
+   * - backwalk to poke all attachments to update
+   */
+  fib_node_index_t old_pl;
+
+  ap = abf_policy_get (api);
+  old_pl = ap->ap_pl;
+
+  fib_path_list_lock (old_pl);
+  ap->ap_pl = fib_path_list_copy_and_path_remove (
+    ap->ap_pl, (FIB_PATH_LIST_FLAG_SHARED | FIB_PATH_LIST_FLAG_NO_URPF),
+    rpaths);
+
+  fib_path_list_child_remove (old_pl, ap->ap_sibling);
+  ap->ap_sibling = ~0;
+
+  if (FIB_NODE_INDEX_INVALID == ap->ap_pl)
     {
       /*
-       * update an existing policy.
-       * - add the path to the path-list and swap our ancestry
-       * - backwalk to poke all attachments to update
+       * no more paths on this policy. It's toast
+       * remove the CLI/API's lock
        */
-      fib_node_index_t old_pl;
-
-      ap = abf_policy_get (api);
-      old_pl = ap->ap_pl;
-
-      fib_path_list_lock (old_pl);
-      ap->ap_pl =
-	fib_path_list_copy_and_path_remove (ap->ap_pl,
-					    (FIB_PATH_LIST_FLAG_SHARED |
-					     FIB_PATH_LIST_FLAG_NO_URPF),
-					    rpaths);
-
-      fib_path_list_child_remove (old_pl, ap->ap_sibling);
-      ap->ap_sibling = ~0;
-
-      if (FIB_NODE_INDEX_INVALID == ap->ap_pl)
-	{
-	  /*
-	   * no more paths on this policy. It's toast
-	   * remove the CLI/API's lock
-	   */
-	  fib_node_unlock (&ap->ap_node);
-	}
-      else
-	{
-	  ap->ap_sibling = fib_path_list_child_add (ap->ap_pl,
-						    abf_policy_fib_node_type,
-						    api);
-
-	  fib_node_back_walk_ctx_t ctx = {
-	    .fnbw_reason = FIB_NODE_BW_REASON_FLAG_EVALUATE,
-	  };
-
-	  fib_walk_sync (abf_policy_fib_node_type, api, &ctx);
-	}
-      fib_path_list_unlock (old_pl);
+      fib_node_unlock (&ap->ap_node);
     }
+  else
+    {
+      ap->ap_sibling =
+	fib_path_list_child_add (ap->ap_pl, abf_policy_fib_node_type, api);
+
+      fib_node_back_walk_ctx_t ctx = {
+	.fnbw_reason = FIB_NODE_BW_REASON_FLAG_EVALUATE,
+      };
+
+      fib_walk_sync (abf_policy_fib_node_type, api, &ctx);
+    }
+  fib_path_list_unlock (old_pl);
 
   return (0);
 }
@@ -272,14 +267,25 @@ abf_policy_cmd (vlib_main_t * vm,
 			 unformat_fib_route_path, &rpath, &payload_proto))
 	vec_add1 (rpaths, rpath);
       else
-	return (clib_error_return (0, "unknown input '%U'",
-				   format_unformat_error, line_input));
+	{
+	  clib_error_t *err;
+	  err = clib_error_return (0, "unknown input '%U'",
+				   format_unformat_error, line_input);
+	  unformat_free (line_input);
+	  return err;
+	}
     }
 
   if (INDEX_INVALID == policy_id)
     {
       vlib_cli_output (vm, "Specify a Policy ID");
-      return 0;
+      goto out;
+    }
+
+  if (vec_len (rpaths) == 0)
+    {
+      vlib_cli_output (vm, "Hop path must not be empty");
+      goto out;
     }
 
   if (!is_del)
@@ -287,7 +293,7 @@ abf_policy_cmd (vlib_main_t * vm,
       if (INDEX_INVALID == acl_index)
 	{
 	  vlib_cli_output (vm, "ACL index must be set");
-	  return 0;
+	  goto out;
 	}
 
       rv = abf_policy_update (policy_id, acl_index, rpaths);
@@ -296,7 +302,7 @@ abf_policy_cmd (vlib_main_t * vm,
 	{
 	  vlib_cli_output (vm,
 			   "ACL index must match existing ACL index in policy");
-	  return 0;
+	  goto out;
 	}
     }
   else
@@ -304,6 +310,7 @@ abf_policy_cmd (vlib_main_t * vm,
       abf_policy_delete (policy_id, rpaths);
     }
 
+out:
   unformat_free (line_input);
   return (NULL);
 }