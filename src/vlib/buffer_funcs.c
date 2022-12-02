alisherqodirjonov/vpp

commit e8b1bfa121c69b1a326d958589b2d1dd9ddd9846

buffers: protect against bad thread indices
There is a very rare bug in NAT processing that yeilds a thread
index of ~0.  When this happens, vlib_get_frame_queue_elt()
suffers a segfault and VPP quits.  Prevent an outright fault
by dropping the packet instead.

Type: fix
Signed-off-by: Jon Loeliger <jdl@netgate.com>
Change-Id: I48c7a268925bb821ea15e58db5d4bfb211c40c09

src/vlib/buffer_funcs.c
@@ -261,6 +261,9 @@ vlib_get_frame_queue_elt (vlib_frame_queue_main_t *fqm, u32 index,
   vlib_frame_queue_t *fq;
   u64 nelts, tail, new_tail;
 
+  if (index >= vec_len (fqm->vlib_frame_queues))
+    return 0;
+
   fq = fqm->vlib_frame_queues[index];
   ASSERT (fq);
   nelts = fq->nelts;

commit 75d0da505da4a92680ab1e8786d42498ad2cf2a4

buffers: revert protect against bad thread indices
This change was introduced to workaround a bug in the NAT code, but
we should not woraround plugin bugs in infra.

Type: fix
Fixes: e8b1bfa121c69b1a326d958589b2d1dd9ddd9846

Change-Id: Id6ee281cf1fe8466b6522905fc2a176716e3d52f
Signed-off-by: Benoît Ganne <bganne@cisco.com>

src/vlib/buffer_funcs.c
@@ -261,10 +261,7 @@ vlib_get_frame_queue_elt (vlib_frame_queue_main_t *fqm, u32 index,
   vlib_frame_queue_t *fq;
   u64 nelts, tail, new_tail;
 
-  if (index >= vec_len (fqm->vlib_frame_queues))
-    return 0;
-
-  fq = fqm->vlib_frame_queues[index];
+  fq = vec_elt (fqm->vlib_frame_queues, index);
   ASSERT (fq);
   nelts = fq->nelts;
 