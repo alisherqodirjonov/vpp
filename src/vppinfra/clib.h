alisherqodirjonov/vpp

commit 69fd8742812a1a64ff96fdca101663bfa24d6bc1

Add memif - packet memory interface for intra-host communication
Change-Id: I94c06b07a39f07ceba87bf3e7fcfc70e43231e8a
Signed-off-by: Damjan Marion <damarion@cisco.com>
Co-Authored-By: Milan Lenco <Milan.Lenco@pantheon.tech>

src/vppinfra/clib.h
@@ -100,6 +100,12 @@
 /* Full memory barrier (read and write). */
 #define CLIB_MEMORY_BARRIER() __sync_synchronize ()
 
+#if __x86_64__
+#define CLIB_MEMORY_STORE_BARRIER() __builtin_ia32_sfence ()
+#else
+#define CLIB_MEMORY_STORE_BARRIER() __sync_synchronize ()
+#endif
+
 /* Arranges for function to be called before main. */
 #define INIT_FUNCTION(decl)			\
   decl __attribute ((constructor));		\

src/configure.ac
@@ -151,6 +151,7 @@ PLUGIN_ENABLED(ila)
 PLUGIN_ENABLED(ioam)
 PLUGIN_ENABLED(ixge)
 PLUGIN_ENABLED(lb)
+PLUGIN_ENABLED(memif)
 PLUGIN_ENABLED(sixrd)
 PLUGIN_ENABLED(snat)
 PLUGIN_DISABLED(srv6sample)

src/plugins/Makefile.am
@@ -57,6 +57,10 @@ if ENABLE_LB_PLUGIN
 include lb.am
 endif
 
+if ENABLE_MEMIF_PLUGIN
+include memif.am
+endif
+
 if ENABLE_SIXRD_PLUGIN
 include sixrd.am
 endif

src/plugins/memif.am
@@ -1,0 +1,32 @@
+# Copyright (c) 2017 Cisco Systems, Inc.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+vppplugins_LTLIBRARIES += memif_plugin.la
+
+memif_plugin_la_SOURCES = memif/memif.c   \
+	memif/memif_api.c                 \
+	memif/cli.c                       \
+	memif/node.c                      \
+	memif/device.c                    \
+	memif/memif_plugin.api.h
+
+noinst_HEADERS += memif/memif.h
+
+nobase_apiinclude_HEADERS +=              \
+		  memif/memif_all_api_h.h \
+		  memif/memif_msg_enum.h  \
+		  memif/memif.api.h
+
+API_FILES += memif/memif.api
+
+# vi:syntax=automake

src/plugins/memif/cli.c
@@ -1,0 +1,202 @@
+/*
+ *------------------------------------------------------------------
+ * Copyright (c) 2016 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *------------------------------------------------------------------
+ */
+#include <stdint.h>
+#include <net/if.h>
+#include <sys/ioctl.h>
+#include <inttypes.h>
+
+#include <vlib/vlib.h>
+#include <vlib/unix/unix.h>
+#include <vnet/ethernet/ethernet.h>
+
+#include <memif/memif.h>
+
+static clib_error_t *
+memif_create_command_fn (vlib_main_t * vm, unformat_input_t * input,
+			 vlib_cli_command_t * cmd)
+{
+  unformat_input_t _line_input, *line_input = &_line_input;
+  int r;
+  u32 ring_size = MEMIF_DEFAULT_RING_SIZE;
+  memif_create_if_args_t args = { 0 };
+  args.buffer_size = MEMIF_DEFAULT_BUFFER_SIZE;
+
+  /* Get a line of input. */
+  if (!unformat_user (input, unformat_line_input, line_input))
+    return 0;
+
+  while (unformat_check_input (line_input) != UNFORMAT_END_OF_INPUT)
+    {
+      if (unformat (line_input, "key 0x%" PRIx64, &args.key))
+	;
+      else if (unformat (line_input, "socket %s", &args.socket_filename))
+	;
+      else if (unformat (line_input, "ring-size %u", &ring_size))
+	;
+      else if (unformat (line_input, "buffer-size %u", &args.buffer_size))
+	;
+      else if (unformat (line_input, "master"))
+	args.is_master = 1;
+      else if (unformat (line_input, "slave"))
+	args.is_master = 0;
+      else if (unformat (line_input, "hw-addr %U",
+			 unformat_ethernet_address, args.hw_addr))
+	args.hw_addr_set = 1;
+      else
+	return clib_error_return (0, "unknown input `%U'",
+				  format_unformat_error, input);
+    }
+  unformat_free (line_input);
+
+  if (!is_pow2 (ring_size))
+    return clib_error_return (0, "ring size must be power of 2");
+
+  args.log2_ring_size = min_log2 (ring_size);
+
+  r = memif_create_if (vm, &args);
+
+  if (r <= VNET_API_ERROR_SYSCALL_ERROR_1
+      && r >= VNET_API_ERROR_SYSCALL_ERROR_10)
+    return clib_error_return (0, "%s (errno %d)", strerror (errno), errno);
+
+  if (r == VNET_API_ERROR_INVALID_INTERFACE)
+    return clib_error_return (0, "Invalid interface name");
+
+  if (r == VNET_API_ERROR_SUBIF_ALREADY_EXISTS)
+    return clib_error_return (0, "Interface already exists");
+
+  return 0;
+}
+
+/* *INDENT-OFF* */
+VLIB_CLI_COMMAND (memif_create_command, static) = {
+  .path = "create memif",
+  .short_help = "create memif [key <key>] [socket <path>] "
+                "[ring-size <size>] [buffer-size <size>] [hw-addr <mac-address>] "
+		"<master|slave>",
+  .function = memif_create_command_fn,
+};
+/* *INDENT-ON* */
+
+static clib_error_t *
+memif_delete_command_fn (vlib_main_t * vm, unformat_input_t * input,
+			 vlib_cli_command_t * cmd)
+{
+  unformat_input_t _line_input, *line_input = &_line_input;
+  u64 key = 0;
+  u8 key_defined = 0;
+
+  /* Get a line of input. */
+  if (!unformat_user (input, unformat_line_input, line_input))
+    return 0;
+
+  while (unformat_check_input (line_input) != UNFORMAT_END_OF_INPUT)
+    {
+      if (unformat (line_input, "key 0x%" PRIx64, &key))
+	key_defined = 1;
+      else
+	return clib_error_return (0, "unknown input `%U'",
+				  format_unformat_error, input);
+    }
+  unformat_free (line_input);
+
+  if (!key_defined)
+    return clib_error_return (0, "missing key");
+
+  memif_delete_if (vm, key);
+
+  return 0;
+}
+
+/* *INDENT-OFF* */
+VLIB_CLI_COMMAND (memif_delete_command, static) = {
+  .path = "delete memif",
+  .short_help = "delete memif key <key-value>",
+  .function = memif_delete_command_fn,
+};
+/* *INDENT-ON* */
+
+static clib_error_t *
+memif_show_command_fn (vlib_main_t * vm, unformat_input_t * input,
+		       vlib_cli_command_t * cmd)
+{
+  memif_main_t *mm = &memif_main;
+  memif_if_t *mif;
+  vnet_main_t *vnm = vnet_get_main ();
+  int i;
+
+  /* *INDENT-OFF* */
+  pool_foreach (mif, mm->interfaces,
+    ({
+       vlib_cli_output (vm, "interface %U", format_vnet_sw_if_index_name,
+			vnm, mif->sw_if_index);
+       vlib_cli_output (vm, "  key 0x%" PRIx64 " file %s", mif->key,
+			mif->socket_filename);
+       vlib_cli_output (vm, "  listener %d conn-fd %d int-fd %d", mif->listener_index,
+			mif->connection.fd, mif->interrupt_line.fd);
+       vlib_cli_output (vm, "  ring-size %u num-c2s-rings %u num-s2c-rings %u buffer_size %u",
+			(1 << mif->log2_ring_size),
+			mif->num_s2m_rings,
+			mif->num_m2s_rings,
+			mif->buffer_size);
+       for (i=0; i < mif->num_s2m_rings; i++)
+         {
+	   memif_ring_t * ring = memif_get_ring (mif, MEMIF_RING_S2M, i);
+	   if (ring)
+	     {
+	       vlib_cli_output (vm, "  slave-to-master ring %u:", i);
+	       vlib_cli_output (vm, "    head %u tail %u", ring->head, ring->tail);
+	     }
+	 }
+       for (i=0; i < mif->num_m2s_rings; i++)
+         {
+	   memif_ring_t * ring = memif_get_ring (mif, MEMIF_RING_M2S, i);
+	   if (ring)
+	     {
+	       vlib_cli_output (vm, "  master-to-slave ring %u:", i);
+	       vlib_cli_output (vm, "    head %u tail %u", ring->head, ring->tail);
+	     }
+	 }
+    }));
+  /* *INDENT-ON* */
+
+  return 0;
+}
+
+/* *INDENT-OFF* */
+VLIB_CLI_COMMAND (memif_show_command, static) = {
+  .path = "show memif",
+  .short_help = "show memif",
+  .function = memif_show_command_fn,
+};
+/* *INDENT-ON* */
+
+clib_error_t *
+memif_cli_init (vlib_main_t * vm)
+{
+  return 0;
+}
+
+VLIB_INIT_FUNCTION (memif_cli_init);
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */

src/plugins/memif/device.c
@@ -1,0 +1,330 @@
+/*
+ *------------------------------------------------------------------
+ * Copyright (c) 2016 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *------------------------------------------------------------------
+ */
+
+#define _GNU_SOURCE
+#include <stdint.h>
+#include <net/if.h>
+#include <sys/ioctl.h>
+#include <sys/uio.h>
+
+#include <vlib/vlib.h>
+#include <vlib/unix/unix.h>
+#include <vnet/ethernet/ethernet.h>
+
+#include <memif/memif.h>
+
+#define foreach_memif_tx_func_error	       \
+_(NO_FREE_SLOTS, "no free tx slots")           \
+_(PENDING_MSGS, "pending msgs in tx ring")
+
+typedef enum
+{
+#define _(f,s) MEMIF_TX_ERROR_##f,
+  foreach_memif_tx_func_error
+#undef _
+    MEMIF_TX_N_ERROR,
+} memif_tx_func_error_t;
+
+static char *memif_tx_func_error_strings[] = {
+#define _(n,s) s,
+  foreach_memif_tx_func_error
+#undef _
+};
+
+
+static u8 *
+format_memif_device_name (u8 * s, va_list * args)
+{
+  u32 i = va_arg (*args, u32);
+
+  s = format (s, "memif%u", i);
+  return s;
+}
+
+static u8 *
+format_memif_device (u8 * s, va_list * args)
+{
+  u32 dev_instance = va_arg (*args, u32);
+  int verbose = va_arg (*args, int);
+  uword indent = format_get_indent (s);
+
+  s = format (s, "MEMIF interface");
+  if (verbose)
+    {
+      s = format (s, "\n%U instance %u", format_white_space, indent + 2,
+		  dev_instance);
+    }
+  return s;
+}
+
+static u8 *
+format_memif_tx_trace (u8 * s, va_list * args)
+{
+  s = format (s, "Unimplemented...");
+  return s;
+}
+
+static_always_inline void
+memif_interface_lock (memif_if_t * mif)
+{
+  if (PREDICT_FALSE (mif->lockp != 0))
+    {
+      while (__sync_lock_test_and_set (mif->lockp, 1))
+	;
+    }
+}
+
+static_always_inline void
+memif_interface_unlock (memif_if_t * mif)
+{
+  if (PREDICT_FALSE (mif->lockp != 0))
+    *mif->lockp = 0;
+}
+
+static_always_inline void
+memif_prefetch_buffer_and_data (vlib_main_t * vm, u32 bi)
+{
+  vlib_buffer_t *b = vlib_get_buffer (vm, bi);
+  vlib_prefetch_buffer_header (b, LOAD);
+  CLIB_PREFETCH (b->data, CLIB_CACHE_LINE_BYTES, LOAD);
+}
+
+static_always_inline uword
+memif_interface_tx_inline (vlib_main_t * vm, vlib_node_runtime_t * node,
+			   vlib_frame_t * frame, memif_if_t * mif,
+			   memif_ring_type_t type)
+{
+  u8 rid = 0;
+  memif_ring_t *ring = memif_get_ring (mif, type, rid);
+  u32 *buffers = vlib_frame_args (frame);
+  u32 n_left = frame->n_vectors;
+  u16 ring_size = 1 << mif->log2_ring_size;
+  u16 mask = ring_size - 1;
+  u16 head, tail;
+  u16 free_slots;
+
+  memif_interface_lock (mif);
+
+  /* free consumed buffers */
+
+  head = ring->head;
+  tail = ring->tail;
+
+  if (tail > head)
+    free_slots = tail - head;
+  else
+    free_slots = ring_size - head + tail;
+
+  while (n_left > 5 && free_slots > 1)
+    {
+      if (PREDICT_TRUE (head + 5 < ring_size))
+	{
+	  CLIB_PREFETCH (memif_get_buffer (mif, ring, head + 2),
+			 CLIB_CACHE_LINE_BYTES, STORE);
+	  CLIB_PREFETCH (memif_get_buffer (mif, ring, head + 3),
+			 CLIB_CACHE_LINE_BYTES, STORE);
+	  CLIB_PREFETCH (&ring->desc[head + 4], CLIB_CACHE_LINE_BYTES, STORE);
+	  CLIB_PREFETCH (&ring->desc[head + 5], CLIB_CACHE_LINE_BYTES, STORE);
+	}
+      else
+	{
+	  CLIB_PREFETCH (memif_get_buffer (mif, ring, (head + 2) % mask),
+			 CLIB_CACHE_LINE_BYTES, STORE);
+	  CLIB_PREFETCH (memif_get_buffer (mif, ring, (head + 3) % mask),
+			 CLIB_CACHE_LINE_BYTES, STORE);
+	  CLIB_PREFETCH (&ring->desc[(head + 4) % mask],
+			 CLIB_CACHE_LINE_BYTES, STORE);
+	  CLIB_PREFETCH (&ring->desc[(head + 5) % mask],
+			 CLIB_CACHE_LINE_BYTES, STORE);
+	}
+
+      memif_prefetch_buffer_and_data (vm, buffers[2]);
+      memif_prefetch_buffer_and_data (vm, buffers[3]);
+
+      vlib_buffer_t *b0 = vlib_get_buffer (vm, buffers[0]);
+      vlib_buffer_t *b1 = vlib_get_buffer (vm, buffers[1]);
+
+      void *mb0 = memif_get_buffer (mif, ring, head);
+      clib_memcpy (mb0, vlib_buffer_get_current (b0), CLIB_CACHE_LINE_BYTES);
+      ring->desc[head].length = b0->current_length;
+      head = (head + 1) & mask;
+
+      void *mb1 = memif_get_buffer (mif, ring, head);
+      clib_memcpy (mb1, vlib_buffer_get_current (b1), CLIB_CACHE_LINE_BYTES);
+      ring->desc[head].length = b1->current_length;
+      head = (head + 1) & mask;
+
+      if (b0->current_length > CLIB_CACHE_LINE_BYTES)
+	{
+	  clib_memcpy (mb0 + CLIB_CACHE_LINE_BYTES,
+		       vlib_buffer_get_current (b0) + CLIB_CACHE_LINE_BYTES,
+		       b0->current_length - CLIB_CACHE_LINE_BYTES);
+	}
+      if (b1->current_length > CLIB_CACHE_LINE_BYTES)
+	{
+	  clib_memcpy (mb1 + CLIB_CACHE_LINE_BYTES,
+		       vlib_buffer_get_current (b1) + CLIB_CACHE_LINE_BYTES,
+		       b1->current_length - CLIB_CACHE_LINE_BYTES);
+	}
+
+
+      buffers += 2;
+      n_left -= 2;
+      free_slots -= 2;
+    }
+
+  while (n_left && free_slots)
+    {
+      vlib_buffer_t *b0 = vlib_get_buffer (vm, buffers[0]);
+      void *mb0 = memif_get_buffer (mif, ring, head);
+      clib_memcpy (mb0, vlib_buffer_get_current (b0), CLIB_CACHE_LINE_BYTES);
+
+      if (b0->current_length > CLIB_CACHE_LINE_BYTES)
+	{
+	  clib_memcpy (mb0 + CLIB_CACHE_LINE_BYTES,
+		       vlib_buffer_get_current (b0) + CLIB_CACHE_LINE_BYTES,
+		       b0->current_length - CLIB_CACHE_LINE_BYTES);
+	}
+      ring->desc[head].length = b0->current_length;
+      head = (head + 1) & mask;
+
+      buffers++;
+      n_left--;
+      free_slots--;
+    }
+
+  CLIB_MEMORY_STORE_BARRIER ();
+  ring->head = head;
+
+  memif_interface_unlock (mif);
+
+  if (n_left)
+    {
+      vlib_error_count (vm, node->node_index, MEMIF_TX_ERROR_NO_FREE_SLOTS,
+			n_left);
+      vlib_buffer_free (vm, buffers, n_left);
+    }
+
+  vlib_buffer_free (vm, vlib_frame_args (frame), frame->n_vectors);
+  if (mif->interrupt_line.fd > 0)
+    {
+      u8 b = rid;
+      CLIB_UNUSED (int r) = write (mif->interrupt_line.fd, &b, sizeof (b));
+    }
+
+  return frame->n_vectors;
+}
+
+static uword
+memif_interface_tx (vlib_main_t * vm,
+		    vlib_node_runtime_t * node, vlib_frame_t * frame)
+{
+  memif_main_t *nm = &memif_main;
+  vnet_interface_output_runtime_t *rund = (void *) node->runtime_data;
+  memif_if_t *mif = pool_elt_at_index (nm->interfaces, rund->dev_instance);
+
+  if (mif->flags & MEMIF_IF_FLAG_IS_SLAVE)
+    return memif_interface_tx_inline (vm, node, frame, mif, MEMIF_RING_S2M);
+  else
+    return memif_interface_tx_inline (vm, node, frame, mif, MEMIF_RING_M2S);
+}
+
+static void
+memif_set_interface_next_node (vnet_main_t * vnm, u32 hw_if_index,
+			       u32 node_index)
+{
+  memif_main_t *apm = &memif_main;
+  vnet_hw_interface_t *hw = vnet_get_hw_interface (vnm, hw_if_index);
+  memif_if_t *mif = pool_elt_at_index (apm->interfaces, hw->dev_instance);
+
+  /* Shut off redirection */
+  if (node_index == ~0)
+    {
+      mif->per_interface_next_index = node_index;
+      return;
+    }
+
+  mif->per_interface_next_index =
+    vlib_node_add_next (vlib_get_main (), memif_input_node.index, node_index);
+}
+
+static void
+memif_clear_hw_interface_counters (u32 instance)
+{
+  /* Nothing for now */
+}
+
+static clib_error_t *
+memif_interface_admin_up_down (vnet_main_t * vnm, u32 hw_if_index, u32 flags)
+{
+  memif_main_t *apm = &memif_main;
+  memif_msg_t msg;
+  vnet_hw_interface_t *hw = vnet_get_hw_interface (vnm, hw_if_index);
+  memif_if_t *mif = pool_elt_at_index (apm->interfaces, hw->dev_instance);
+
+  if (flags & VNET_SW_INTERFACE_FLAG_ADMIN_UP)
+    mif->flags |= MEMIF_IF_FLAG_ADMIN_UP;
+  else
+    {
+      mif->flags &= ~MEMIF_IF_FLAG_ADMIN_UP;
+      if (!(mif->flags & MEMIF_IF_FLAG_DELETING)
+	  && mif->connection.index != ~0)
+	{
+	  msg.version = MEMIF_VERSION;
+	  msg.type = MEMIF_MSG_TYPE_DISCONNECT;
+	  send (mif->connection.fd, &msg, sizeof (msg), 0);
+	}
+    }
+
+  return 0;
+}
+
+static clib_error_t *
+memif_subif_add_del_function (vnet_main_t * vnm,
+			      u32 hw_if_index,
+			      struct vnet_sw_interface_t *st, int is_add)
+{
+  /* Nothing for now */
+  return 0;
+}
+
+/* *INDENT-OFF* */
+VNET_DEVICE_CLASS (memif_device_class) = {
+  .name = "memif",
+  .tx_function = memif_interface_tx,
+  .format_device_name = format_memif_device_name,
+  .format_device = format_memif_device,
+  .format_tx_trace = format_memif_tx_trace,
+  .tx_function_n_errors = MEMIF_TX_N_ERROR,
+  .tx_function_error_strings = memif_tx_func_error_strings,
+  .rx_redirect_to_node = memif_set_interface_next_node,
+  .clear_counters = memif_clear_hw_interface_counters,
+  .admin_up_down_function = memif_interface_admin_up_down,
+  .subif_add_del_function = memif_subif_add_del_function,
+};
+
+VLIB_DEVICE_TX_FUNCTION_MULTIARCH(memif_device_class,
+				  memif_interface_tx)
+/* *INDENT-ON* */
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */

src/plugins/memif/memif.api
@@ -1,0 +1,127 @@
+/*
+ * Copyright (c) 2017 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/** \brief Create memory interface
+    @param client_index - opaque cookie to identify the sender
+    @param context - sender context, to match reply w/ request
+    @param role - role of the interface in the connection (master/slave)
+    @param key - 64bit integer used to authenticate and match opposite sides
+           of the connection
+    @param socket_filename - filename of the socket to be used for connection
+           establishment
+    @param ring_size - the number of entries of RX/TX rings
+    @param buffer_size - size of the buffer allocated for each ring entry
+    @param hw_addr - interface MAC address
+*/
+define memif_create
+{
+  u32 client_index;
+  u32 context;
+
+  u8 role; /* 0 = master, 1 = slave */
+  u64 key; /* optional, default is 0 */
+  u8 socket_filename[128]; /* optional, default is "/var/vpp/memif.sock"
+                              and can be changed in VPP startup config */
+
+  u32 ring_size; /* optional, default is 1024 entries, must be power of 2 */
+  u16 buffer_size; /* optional, default is 2048 bytes */
+  u8 hw_addr[6]; /* optional, randomly generated if not defined */
+};
+
+/** \brief Create memory interface response
+    @param context - sender context, to match reply w/ request
+    @param retval - return value for request
+    @param sw_if_index - software index of the newly created interface
+*/
+define memif_create_reply
+{
+  u32 context;
+  i32 retval;
+  u32 sw_if_index;
+};
+
+/** \brief Delete memory interface
+    @param client_index - opaque cookie to identify the sender
+    @param context - sender context, to match reply w/ request
+    @param sw_if_index - software index of the interface to delete
+*/
+define memif_delete
+{
+  u32 client_index;
+  u32 context;
+
+  u32 sw_if_index;
+};
+
+/** \brief Delete host-interface response
+    @param context - sender context, to match reply w/ request
+    @param retval - return value for request
+*/
+define memif_delete_reply
+{
+  u32 context;
+  i32 retval;
+};
+
+/** \brief Memory interface details structure
+    @param context - sender context, to match reply w/ request (memif_dump)
+    @param sw_if_index - index of the interface
+    @param if_name - name of the interface
+    @param hw_addr - interface MAC address
+    @param key - key associated with the interface
+    @param role - role of the interface in the connection (master/slave)
+    @param socket_filename - name of the socket used by this interface
+           to establish new connections
+    @param ring_size - the number of entries of RX/TX rings
+    @param buffer_size - size of the buffer allocated for each ring entry
+    @param admin_up_down - interface administrative status
+    @param link_up_down - interface link status
+
+*/
+define memif_details
+{
+  u32 context;
+
+  u32 sw_if_index;
+  u8 if_name[64];
+  u8 hw_addr[6];
+
+  /* memif specific parameters */
+  u64 key;
+  u8 role; /* 0 = master, 1 = slave */
+  u8 socket_filename[128];
+  u32 ring_size;
+  u16 buffer_size; /* optional, default is 2048 bytes */
+
+  /* 1 = up, 0 = down */
+  u8 admin_up_down;
+  u8 link_up_down;
+};
+
+/** \brief Dump all memory interfaces
+    @param client_index - opaque cookie to identify the sender
+    @param context - sender context, to match reply w/ request
+*/
+define memif_dump
+{
+  u32 client_index;
+  u32 context;
+};
+
+/*
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */

commit 75309e0e6c6d5d4e9e6313f321176e06aa27ad23

vppinfra: SFENCE requires SSE2 to be enabled
Change-Id: I0469bb91107cf0acced3cd19820db8d3712701c0
Type: fix
Fixes: 69fd874
Signed-off-by: Damjan Marion <damarion@cisco.com>

src/vppinfra/clib.h
@@ -181,7 +181,7 @@
 /* Full memory barrier (read and write). */
 #define CLIB_MEMORY_BARRIER() __sync_synchronize ()
 
-#if __x86_64__
+#if __SSE__
 #define CLIB_MEMORY_STORE_BARRIER() __builtin_ia32_sfence ()
 #else
 #define CLIB_MEMORY_STORE_BARRIER() __sync_synchronize ()