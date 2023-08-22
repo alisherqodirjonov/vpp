alisherqodirjonov/vpp

commit 22d6e2d1f8b12d7baf0e9e787b6c3a4017488c25

l2: change prefetch to prefetchw
L2 prefetches the vlib_buffer_t and then immediately writes to l2 split
horizon group. Changing to the prefetch to a prefetchw.

Type: improvement

Signed-off-by: Ray Kinsella <mdr@ashroe.eu>
Change-Id: If3b7996978ee80f99c55904692c7b3c905f537ce

src/vnet/l2/l2_input_node.c
@@ -252,10 +252,10 @@ l2input_node_inline (vlib_main_t * vm,
 	  /* Prefetch next iteration. */
 	  {
 	    /* Prefetch the buffer header and packet for the N+2 loop iteration */
-	    vlib_prefetch_buffer_header (b[4], LOAD);
-	    vlib_prefetch_buffer_header (b[5], LOAD);
-	    vlib_prefetch_buffer_header (b[6], LOAD);
-	    vlib_prefetch_buffer_header (b[7], LOAD);
+	    clib_prefetch_store (b + 4);
+	    clib_prefetch_store (b + 5);
+	    clib_prefetch_store (b + 6);
+	    clib_prefetch_store (b + 7);
 
 	    clib_prefetch_store (b[4]->data);
 	    clib_prefetch_store (b[5]->data);

commit f48d64dd98e51a377d5b6816ef6fc4864581b13f

l2: fix prefetch
This is a clone of Gerrit 35419.
(It is abandoned and I am not the owner so I cannot reopen.)

Ticket: CSIT-1816
Type: fix
Fixes: 22d6e2d1f8b12d7baf0e9e787b6c3a4017488c25

Change-Id: I2265cf38a9ce3155460a1025821c2749afca0add
Signed-off-by: Vratko Polak <vrpolak@cisco.com>

src/vnet/l2/l2_input_node.c
@@ -251,11 +251,11 @@ l2input_node_inline (vlib_main_t * vm,
 
 	  /* Prefetch next iteration. */
 	  {
-	    /* Prefetch the buffer header and packet for the N+2 loop iteration */
-	    clib_prefetch_store (b + 4);
-	    clib_prefetch_store (b + 5);
-	    clib_prefetch_store (b + 6);
-	    clib_prefetch_store (b + 7);
+	    /* Prefetch the buffer header for the N+2 loop iteration */
+	    clib_prefetch_store (b[4]);
+	    clib_prefetch_store (b[5]);
+	    clib_prefetch_store (b[6]);
+	    clib_prefetch_store (b[7]);
 
 	    clib_prefetch_store (b[4]->data);
 	    clib_prefetch_store (b[5]->data);