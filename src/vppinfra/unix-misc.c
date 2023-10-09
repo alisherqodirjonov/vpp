alisherqodirjonov/vpp

commit b9a2f57d19aa996221bc5545930f3d0e90bc829c

vppinfra: fix string termination in clib_file_get_resolved_basename
Type: fix
Fixes: aab6de0
Change-Id: Idf51462c8154663de23154f17a894b7245c9fbf0
Signed-off-by: Damjan Marion <damarion@cisco.com>

src/vppinfra/unix-misc.c
@@ -148,6 +148,7 @@ clib_file_get_resolved_basename (char *fmt, ...)
 
   r = readlink ((char *) link, buffer, sizeof (buffer) - 1);
   vec_free (link);
+  buffer[r] = 0;
 
   if (r < 1)
     return 0;
@@ -159,6 +160,7 @@ clib_file_get_resolved_basename (char *fmt, ...)
   while (p[0])
     vec_add1 (s, p++[0]);
 
+  vec_add1 (s, 0);
   return s;
 }
 

commit 22f907c4e2a3bea78b09fadfa97711738c015ec7

vppinfra: fix coverity issue CID 323952
Type: fix
Fixes: b9a2f57d1

Change-Id: I53ba0d96507b55ab7cd735073d6c4cf20a3cc948
Signed-off-by: Dave Wallace <dwallacelf@gmail.com>

src/vppinfra/unix-misc.c
@@ -148,11 +148,11 @@ clib_file_get_resolved_basename (char *fmt, ...)
 
   r = readlink ((char *) link, buffer, sizeof (buffer) - 1);
   vec_free (link);
-  buffer[r] = 0;
 
   if (r < 1)
     return 0;
 
+  buffer[r] = 0;
   p = buffer + r - 1;
   while (p > buffer && p[-1] != '/')
     p--;