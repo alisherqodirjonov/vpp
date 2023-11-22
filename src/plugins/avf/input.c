alisherqodirjonov/vpp

commit 76bdbf1533e622537a8f18e9dc2e6f7a77218ef1

avf: avoid eating our own tail
This happens in case we are not able to refill ring with new buffers.

Type: fix
Change-Id: I727e55c5b8482d9b8aeb4a9933a9a755fe21e55f
Signed-off-by: Damjan Marion <damarion@cisco.com>

src/plugins/avf/input.c
@@ -428,6 +428,9 @@ no_more_desc:
   rxq->next = next;
   rxq->n_enqueued -= n_rx_packets + n_tail_desc;
 
+  /* avoid eating our own tail */
+  rxq->descs[(next + rxq->n_enqueued) & mask].qword[1] = 0;
+
 #if defined(CLIB_HAVE_VEC256) || defined(CLIB_HAVE_VEC128)
   or_qw1 |= or_q1x4[0] | or_q1x4[1] | or_q1x4[2] | or_q1x4[3];
 #endif

commit cf662643cc69af92d3d928b00100bf657ce73726

avf: put sentinel at correct place
The previous fix was adding the sentinel before refilling rx,
which gave the NIC time to overwrite it with a new descriptor.

Ticket: VPP-2087
Type: fix
Fixes: 76bdbf1533e622537a8f18e9dc2e6f7a77218ef1

Change-Id: I32bde4a763a62fb66c5c3871d9f10af6066e2d47
Signed-off-by: Vratko Polak <vrpolak@cisco.com>

src/plugins/avf/input.c
@@ -125,6 +125,9 @@ avf_rxq_refill (vlib_main_t * vm, vlib_node_runtime_t * node, avf_rxq_t * rxq,
       n_alloc -= 8;
     }
 
+  /* RXQ can be smaller than 256 packets, especially if jumbo. */
+  rxq->descs[slot].qword[1] = 0;
+
   avf_tail_write (rxq->qrx_tail, slot);
 }
 
@@ -422,9 +425,6 @@ no_more_desc:
 
   rxq->next = next;
   rxq->n_enqueued -= n_rx_packets + n_tail_desc;
-
-  /* avoid eating our own tail */
-  rxq->descs[(next + rxq->n_enqueued) & mask].qword[1] = 0;
 
 #if defined(CLIB_HAVE_VEC256) || defined(CLIB_HAVE_VEC128)
   or_qw1 |= or_q1x4[0] | or_q1x4[1] | or_q1x4[2] | or_q1x4[3];

src/plugins/dev_iavf/rx_node.c
@@ -111,6 +111,9 @@ iavf_rxq_refill (vlib_main_t *vm, vlib_node_runtime_t *node,
       n_alloc -= 8;
     }
 
+  /* RXQ can be smaller than 256 packets, especially if jumbo. */
+  arq->descs[slot].qword[1] = 0;
+
   __atomic_store_n (arq->qrx_tail, slot, __ATOMIC_RELEASE);
 }
 
@@ -398,9 +401,6 @@ no_more_desc:
 
   arq->next = next;
   arq->n_enqueued -= n_rx_packets + n_tail_desc;
-
-  /* avoid eating our own tail */
-  arq->descs[(next + arq->n_enqueued) & mask].qword[1] = 0;
 
 #if defined(CLIB_HAVE_VEC256) || defined(CLIB_HAVE_VEC128)
   or_qw1 |= or_q1x4[0] | or_q1x4[1] | or_q1x4[2] | or_q1x4[3];