alisherqodirjonov/vpp

commit 11832ef1bc76fea801ab48bd5a35111ac2a44177

octeon: enable ethernet pause frame support
This patch adds support for MAC pause flow control.
By default, pause flow control is enabled in the device
configuration.

Type: feature

Signed-off-by: Alok Mishra <almishra@marvell.com>
Change-Id: I0f448479a38fae615d87af7e736c6053ada89cca

src/plugins/dev_octeon/port.c
@@ -53,6 +53,72 @@ oct_roc_err (vnet_dev_t *dev, int rv, char *fmt, ...)
 }
 
 vnet_dev_rv_t
+oct_port_pause_flow_control_init (vlib_main_t *vm, vnet_dev_port_t *port)
+{
+  vnet_dev_t *dev = port->dev;
+  oct_device_t *cd = vnet_dev_get_data (dev);
+  struct roc_nix *nix = cd->nix;
+  struct roc_nix_fc_cfg fc_cfg;
+  struct roc_nix_sq *sq;
+  struct roc_nix_cq *cq;
+  struct roc_nix_rq *rq;
+  int rrv;
+
+  if (roc_nix_is_sdp (nix) || roc_nix_is_lbk (nix))
+    return VNET_DEV_ERR_UNSUPPORTED_DEVICE;
+
+  fc_cfg.type = ROC_NIX_FC_RXCHAN_CFG;
+  fc_cfg.rxchan_cfg.enable = true;
+  rrv = roc_nix_fc_config_set (nix, &fc_cfg);
+  if (rrv)
+    return oct_roc_err (dev, rrv, "roc_nix_fc_config_set failed");
+
+  memset (&fc_cfg, 0, sizeof (struct roc_nix_fc_cfg));
+  fc_cfg.type = ROC_NIX_FC_RQ_CFG;
+  fc_cfg.rq_cfg.enable = true;
+  fc_cfg.rq_cfg.tc = 0;
+
+  foreach_vnet_dev_port_rx_queue (rxq, port)
+    {
+      oct_rxq_t *crq = vnet_dev_get_rx_queue_data (rxq);
+
+      rq = &crq->rq;
+      cq = &crq->cq;
+
+      fc_cfg.rq_cfg.rq = rq->qid;
+      fc_cfg.rq_cfg.cq_drop = cq->drop_thresh;
+
+      rrv = roc_nix_fc_config_set (nix, &fc_cfg);
+      if (rrv)
+	return oct_roc_err (dev, rrv, "roc_nix_fc_config_set failed");
+    }
+
+  memset (&fc_cfg, 0, sizeof (struct roc_nix_fc_cfg));
+  fc_cfg.type = ROC_NIX_FC_TM_CFG;
+  fc_cfg.tm_cfg.tc = 0;
+  fc_cfg.tm_cfg.enable = true;
+
+  foreach_vnet_dev_port_tx_queue (txq, port)
+    {
+      oct_txq_t *ctq = vnet_dev_get_tx_queue_data (txq);
+
+      sq = &ctq->sq;
+
+      fc_cfg.tm_cfg.sq = sq->qid;
+      rrv = roc_nix_fc_config_set (nix, &fc_cfg);
+      if (rrv)
+	return oct_roc_err (dev, rrv, "roc_nix_fc_config_set failed");
+    }
+
+  /* By default, enable pause flow control */
+  rrv = roc_nix_fc_mode_set (nix, ROC_NIX_FC_FULL);
+  if (rrv)
+    return oct_roc_err (dev, rrv, "roc_nix_fc_mode_set failed");
+
+  return VNET_DEV_OK;
+}
+
+vnet_dev_rv_t
 oct_port_init (vlib_main_t *vm, vnet_dev_port_t *port)
 {
   vnet_dev_t *dev = port->dev;
@@ -145,6 +211,13 @@ oct_port_init (vlib_main_t *vm, vnet_dev_port_t *port)
   if ((rrv = roc_nix_mac_mtu_set (nix, port->max_rx_frame_size)))
     {
       rv = oct_roc_err (dev, rrv, "roc_nix_mac_mtu_set() failed");
+      return rv;
+    }
+
+  /* Configure pause frame flow control*/
+  if ((rv = oct_port_pause_flow_control_init (vm, port)))
+    {
+      oct_port_deinit (vm, port);
       return rv;
     }
 

commit f1e96606c9c87ba7a17c5f8eb8c75571ea5f7499

octeon: fix pause flow control for lbk/sdp devices
Pause frame flow control is not supported for LBK (Loopback) and
SDP (System DPI Packet Interface Unit) devices.
This patch skips the pause flow configuration for these devices.

Type: fix
Fixes: 11832ef

Signed-off-by: Alok Mishra <almishra@marvell.com>
Change-Id: I3096fcef9df4ad59d64bfabb83f91f13813128a8

src/plugins/dev_octeon/port.c
@@ -64,8 +64,13 @@ oct_port_pause_flow_control_init (vlib_main_t *vm, vnet_dev_port_t *port)
   struct roc_nix_rq *rq;
   int rrv;
 
+  /* pause flow control is not supported on SDP/LBK devices */
   if (roc_nix_is_sdp (nix) || roc_nix_is_lbk (nix))
-    return VNET_DEV_ERR_UNSUPPORTED_DEVICE;
+    {
+      log_notice (dev,
+		  "pause flow control is not supported on SDP/LBK devices");
+      return VNET_DEV_OK;
+    }
 
   fc_cfg.type = ROC_NIX_FC_RXCHAN_CFG;
   fc_cfg.rxchan_cfg.enable = true;