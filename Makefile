alisherqodirjonov/vpp

commit d11d02e034e36d7f32b5e7bb7be5704dc2dbfd11

build: remove unused packages
Type: improvement
Change-Id: I324e34198b5332407759f3acff8b185f936d9090
Signed-off-by: Damjan Marion <damarion@cisco.com>

Makefile
@@ -87,12 +87,12 @@ endif
 
 # +libganglia1-dev if building the gmond plugin
 
-DEB_DEPENDS  = curl build-essential autoconf automake ccache
-DEB_DEPENDS += debhelper dkms git libtool libapr1-dev dh-python
-DEB_DEPENDS += libconfuse-dev git-review exuberant-ctags cscope pkg-config
-DEB_DEPENDS += clang gcovr lcov chrpath autoconf libnuma-dev
+DEB_DEPENDS  = curl build-essential ccache
+DEB_DEPENDS += debhelper git dh-python
+DEB_DEPENDS += git-review exuberant-ctags cscope
+DEB_DEPENDS += clang gcovr lcov chrpath
 DEB_DEPENDS += python3-all python3-setuptools check
-DEB_DEPENDS += libffi-dev python3-ply libunwind-dev
+DEB_DEPENDS += python3-ply libunwind-dev
 DEB_DEPENDS += cmake ninja-build python3-jsonschema python3-yaml
 DEB_DEPENDS += python3-venv  # ensurepip
 DEB_DEPENDS += python3-dev python3-pip
@@ -102,7 +102,6 @@ DEB_DEPENDS += python3-virtualenv
 DEB_DEPENDS += libssl-dev
 DEB_DEPENDS += libelf-dev libpcap-dev # for libxdp (af_xdp)
 DEB_DEPENDS += iperf3 # for 'make test TEST=vcl'
-DEB_DEPENDS += nasm
 DEB_DEPENDS += iperf ethtool  # for 'make test TEST=vm_vpp_interfaces'
 DEB_DEPENDS += libpcap-dev
 DEB_DEPENDS += tshark
@@ -124,41 +123,33 @@ else ifeq ($(OS_VERSION_ID),22.04)
 	# overwrite clang-format version to run `make checkstyle` successfully
 	# TODO: remove once ubuntu 20.04 is deprecated and extras/scripts/checkstyle.sh is upgraded to 15
 	export CLANG_FORMAT_VER=15
-	LIBFFI=libffi7
 	DEB_DEPENDS += enchant-2  # for docs
 else ifeq ($(OS_VERSION_ID),20.04)
 	DEB_DEPENDS += python3-virtualenv
 	DEB_DEPENDS += libssl-dev
 	DEB_DEPENDS += clang clang-format-11
-	LIBFFI=libffi7
 	DEB_DEPENDS += enchant-2  # for docs
 else ifeq ($(OS_ID)-$(OS_VERSION_ID),debian-10)
 	DEB_DEPENDS += virtualenv
 else ifeq ($(OS_ID)-$(OS_VERSION_ID),debian-11)
 	DEB_DEPENDS += virtualenv
 	DEB_DEPENDS += clang clang-format-11
-	LIBFFI=libffi7
 else ifeq ($(OS_ID)-$(OS_VERSION_ID),debian-12)
 	DEB_DEPENDS += virtualenv
 	DEB_DEPENDS += clang-14 clang-format-15
 	# for extras/scripts/checkstyle.sh
 	# TODO: remove once ubuntu 20.04 is deprecated and extras/scripts/checkstyle.sh is upgraded to -15
 	export CLANG_FORMAT_VER=15
-	LIBFFI=libffi8
 else ifeq ($(OS_ID)-$(OS_VERSION_ID),debian-13)
 	DEB_DEPENDS += virtualenv
 	DEB_DEPENDS += clang-19 clang-format-19
 	# for extras/scripts/checkstyle.sh
 	# TODO: remove once ubuntu 20.04 is deprecated and extras/scripts/checkstyle.sh is upgraded to -15
 	export CLANG_FORMAT_VER=15
-	LIBFFI=libffi8
 else
 	DEB_DEPENDS += clang-11 clang-format-11
-	LIBFFI=libffi7
 	DEB_DEPENDS += enchant-2  # for docs
 endif
-
-DEB_DEPENDS += $(LIBFFI)
 
 RPM_DEPENDS  = glibc-static
 RPM_DEPENDS += apr-devel

commit 468957dd5b5a38a5423160a70f4dc681d6b9e157

build: fix the make-ext-deps
After the spring cleaning there were a couple of packages missing,
which prevented the install of the external dependencies.
Also as per Damjan, dkms is no longer needed for dpdk.

Type: fix
Fixes: d11d02e034e36d7f32b5e7bb7be5704dc2dbfd11
Change-Id: I7f51577fee25283d64ddb898af87ae7520d6afb0
Signed-off-by: Andrew Yourtchenko <ayourtch@gmail.com>

Makefile
@@ -106,6 +106,7 @@ DEB_DEPENDS += iperf ethtool  # for 'make test TEST=vm_vpp_interfaces'
 DEB_DEPENDS += libpcap-dev
 DEB_DEPENDS += tshark
 DEB_DEPENDS += jq # for extracting test summary from .json report (hs-test)
+DEB_DEPENDS += nasm libnuma-dev # for make-ext-deps
 
 LIBFFI=libffi6 # works on all but 20.04 and debian-testing
 ifeq ($(OS_VERSION_ID),24.04)

build/external/deb/debian/control
@@ -2,7 +2,7 @@ Source: vpp-ext-deps
 Section: net
 Priority: extra
 Maintainer: vpp-dev@lists.fd.io
-Build-Depends: debhelper (>= 9), dkms
+Build-Depends: debhelper (>= 9)
 Standards-Version: 3.9.4
 
 Package: vpp-ext-deps

build/optional/deb/debian/control
@@ -2,7 +2,7 @@ Source: vpp-opt-deps
 Section: net
 Priority: extra
 Maintainer: vpp-dev@lists.fd.io
-Build-Depends: debhelper (>= 9), dkms
+Build-Depends: debhelper (>= 9)
 Standards-Version: 3.9.4
 
 Package: vpp-opt-deps